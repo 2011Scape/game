bd920d64db84038b46cbdec2cd08b968888cf640
Alycia <charliesalter@hotmail.com>
2022-12-22 22:55:57 -0800

    feat: add game filter button handling

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/inter/gameframe/game_frame.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/inter/gameframe/game_frame.plugin.kts
@@ -1,2 +1,12 @@
 package gg.rsmod.plugins.content.inter.gameframe
 
+val GAME_FILTER_VARBIT = 6161
+val CHAT_OPTIONS_INTERFACE = 751
+
+on_button(interfaceId = CHAT_OPTIONS_INTERFACE, component = 31) {
+    when(player.getInteractingOpcode()) {
+        52 -> {
+            player.toggleVarbit(GAME_FILTER_VARBIT)
+        }
+    }
+}
