74a204c72a41ce130dff0b9ba77ec6ae4c25cd35
Alycia <charliesalter@hotmail.com>
2022-12-28 21:15:58 -0800

    feat: create bob plugin/shop

---
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/lumbridge/bob.plugin.kts
@@ -0,0 +1,5 @@
+package gg.rsmod.plugins.content.areas.lumbridge
+
+on_npc_option(Npcs.BOB, "trade") {
+    player.openShop("Bob's Brilliant Axes")
+}
---
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/mechanics/shops/impl/bobs_axes.plugin.kts
@@ -0,0 +1,15 @@
+package gg.rsmod.plugins.content.mechanics.shops.impl
+
+import gg.rsmod.plugins.content.mechanics.shops.CoinCurrency
+
+create_shop("Bob's Brilliant Axes", currency = CoinCurrency(), purchasePolicy = PurchasePolicy.BUY_STOCK) {
+    sampleItems[0] = ShopItem(Items.BRONZE_PICKAXE, 1, resupplyCycles = 1000)
+    sampleItems[1] = ShopItem(Items.BRONZE_HATCHET, 1, resupplyCycles = 1000)
+    items[0] = ShopItem(Items.BRONZE_PICKAXE, 10)
+    items[1] = ShopItem(Items.BRONZE_HATCHET, 10)
+    items[2] = ShopItem(Items.IRON_HATCHET, 10)
+    items[3] = ShopItem(Items.STEEL_HATCHET, 10)
+    items[4] = ShopItem(Items.IRON_BATTLEAXE, 10)
+    items[5] = ShopItem(Items.STEEL_BATTLEAXE, 10)
+    items[6] = ShopItem(Items.MITHRIL_BATTLEAXE, 10)
+}
