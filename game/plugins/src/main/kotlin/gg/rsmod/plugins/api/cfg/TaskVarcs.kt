3ac4ea6202cae22c72408746dbcd53874d00fd79
Alycia <charliesalter@hotmail.com>
2023-01-12 19:07:47 -0800

    chore: submit start on task system varcs

---
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/api/cfg/TaskVarcs.kt
@@ -0,0 +1,29 @@
+package gg.rsmod.plugins.api.cfg
+
+object TaskVarcs {
+
+    const val A_GRAVE_CONSIDERATION = 61
+    const val DOOM = 62
+    const val SHRIMPIN_AINT_EASY = 63
+    const val MASTER_OF_ALL_I_SURVEY = 64
+    const val RAISE_THE_ROOF = 65
+    const val HAIL_TO_THE_DUKE_BABY = 66
+    const val WINDOW_SHOPPING = 67
+    const val GRINDING_MY_GEARS = 68
+    const val SAGE_ADVICE = 69
+    const val TINKLE_THE_IVORIES = 70
+    const val PASSING_OUT = 71
+    const val THE_RULES_OF_ENGAGEMENT = 72
+    const val TAKE_YOUR_PICK = 73
+    const val RING_MY_BELL = 74
+    const val IN_THE_COURTYARD = 75
+    const val TOWER_POWER = 76
+    const val BEWARE_OF_PIGZILLA = 77
+    const val WAIT_THATS_NOT_A_SHEEP = 78
+    const val THE_FRUIT_OF_THE_SEA = 79
+    const val CLAY_OF_CHAMPIONS = 80
+    const val JUST_ADD_WATER = 81
+    const val VERY_POTTER = 82
+    const val HOTPOT = 83
+
+}
