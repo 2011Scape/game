b0aa978452481d1020f90a72b101def748564a7a
Alycia <charliesalter@hotmail.com>
2023-01-12 19:07:08 -0800

    feat: spawns 12595 plugin

---
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/lumbridge/spawns_12595.plugin.kts
@@ -0,0 +1,14 @@
+package gg.rsmod.plugins.content.areas.lumbridge
+
+spawn_npc(npc = Npcs.CHICKEN, x = 3189, z = 3278, walkRadius = 4, direction = Direction.WEST)
+spawn_npc(npc = Npcs.CHICKEN, x = 3187, z = 3276, walkRadius = 4, direction = Direction.SOUTH_WEST)
+spawn_npc(npc = Npcs.CHICKEN, x = 3188, z = 3278, walkRadius = 4, direction = Direction.EAST)
+spawn_npc(npc = Npcs.FISHING_SPOT_326, x = 3169, z = 3266, direction = Direction.WEST)
+spawn_npc(npc = Npcs.FISHING_SPOT_326, x = 3173, z = 3274, direction = Direction.WEST)
+spawn_npc(npc = Npcs.FRED_THE_FARMER, x = 3189, z = 3273, walkRadius = 4, direction = Direction.EAST)
+spawn_npc(npc = Npcs.COW, x = 3176, z = 3317, walkRadius = 4, direction = Direction.WEST)
+spawn_npc(npc = Npcs.COW, x = 3169, z = 3319, walkRadius = 4, direction = Direction.NORTH)
+spawn_npc(npc = Npcs.COW_CALF, x = 3171, z = 3320, walkRadius = 4, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.CROW_2252, x = 3199, z = 3302, walkRadius = 4, direction = Direction.WEST)
+spawn_npc(npc = Npcs.GOBLIN_3267, x = 3145, z = 3266, walkRadius = 4, direction = Direction.EAST)
+spawn_npc(npc = Npcs.RAM_5168, x = 3197, z = 3268, walkRadius = 4, direction = Direction.NORTH)
