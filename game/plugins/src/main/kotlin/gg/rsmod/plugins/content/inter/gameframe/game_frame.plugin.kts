b31290800ead1e39e3924775a0df0bf9ae0b39f9
Alycia <charliesalter@hotmail.com>
2023-01-16 11:20:53 -0800

    feat: reset XP orb counter plugin

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/inter/gameframe/game_frame.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/inter/gameframe/game_frame.plugin.kts
@@ -1,6 +1,8 @@
 package gg.rsmod.plugins.content.inter.gameframe
 
 val GAME_FILTER_VARBIT = 6161
+val XP_ORB_COUNTER_VARP = 1801
+val RESIZABLE_GAME_FRAME = 746
 val CHAT_OPTIONS_INTERFACE = 751
 
 on_button(interfaceId = CHAT_OPTIONS_INTERFACE, component = 31) {
@@ -8,5 +10,11 @@ on_button(interfaceId = CHAT_OPTIONS_INTERFACE, component = 31) {
         52 -> {
             player.toggleVarbit(GAME_FILTER_VARBIT)
         }
+    }
+}
+
+on_button(interfaceId = RESIZABLE_GAME_FRAME, component = 229) {
+    when(player.getInteractingOpcode()) {
+        10 -> player.setVarp(XP_ORB_COUNTER_VARP,0)
     }
 }
