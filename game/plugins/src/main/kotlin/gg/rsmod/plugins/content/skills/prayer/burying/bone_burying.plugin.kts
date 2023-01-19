929a48baa9deacdc98660f83df5c1a8c859cc53c
Alycia <charliesalter@hotmail.com>
2023-01-18 23:30:23 -0800

    chore: refactor bone burying plugin

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/skills/prayer/burying/bone_burying.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/skills/prayer/burying/bone_burying.plugin.kts
@@ -8,14 +8,15 @@ val bones = boneData.map { it.bone }.toTypedArray()
 bones.forEach { bone ->
     on_item_option(bone, option = "bury") {
         player.queue {
+            player.filterableMessage("You dig a hole in the ground...")
             player.animate(827)
             player.lock()
-            player.filterableMessage("You dig a hole in the ground...")
             wait(2)
-            player.inventory.remove(bone, assureFullRemoval = true)
-            player.filterableMessage("You bury the bones.")
-            player.addXp(Skills.PRAYER, definitions[bone]!!.experience)
-            player.unlock()
+            if (player.inventory.remove(item = bone, beginSlot = player.getInteractingItemSlot()).hasSucceeded()) {
+                player.filterableMessage("You bury the bones.")
+                player.addXp(Skills.PRAYER, definitions[bone]!!.experience)
+                player.unlock()
+            }
         }
     }
 }
