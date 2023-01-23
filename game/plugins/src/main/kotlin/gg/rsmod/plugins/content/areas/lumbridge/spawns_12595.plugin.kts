348eb018633beedeeb942ac81410e749d02994b5
Alycia <charliesalter@hotmail.com>
2023-01-22 22:23:40 -0800

    feat: item spawns for regions 12851, 12595

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/lumbridge/spawns_12595.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/lumbridge/spawns_12595.plugin.kts
@@ -1,5 +1,11 @@
 package gg.rsmod.plugins.content.areas.lumbridge
 
+spawn_item(item = Items.SHEARS, amount = 1, x = 3192, z = 3272, respawnCycles = 160)
+spawn_item(item = Items.SHEARS, amount = 1, x = 3152, z = 3306, respawnCycles = 90)
+spawn_item(item = Items.EMPTY_POT, amount = 1, x = 3166, z = 3310, respawnCycles = 120)
+spawn_item(item = Items.EGG, amount = 1, x = 3191, z = 3276, respawnCycles = 55)
+
+
 spawn_npc(npc = Npcs.CHICKEN, x = 3189, z = 3278, walkRadius = 4, direction = Direction.WEST)
 spawn_npc(npc = Npcs.CHICKEN, x = 3187, z = 3276, walkRadius = 4, direction = Direction.SOUTH_WEST)
 spawn_npc(npc = Npcs.CHICKEN, x = 3188, z = 3278, walkRadius = 4, direction = Direction.EAST)
--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/lumbridge/spawns_12851.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/lumbridge/spawns_12851.plugin.kts
@@ -1,5 +1,10 @@
 package gg.rsmod.plugins.content.areas.lumbridge
 
+
+spawn_item(item = Items.BUCKET, amount = 1, x = 3225, z = 3294, respawnCycles = 100)
+spawn_item(item = Items.EGG, amount = 1, x = 3229, z = 3299, respawnCycles = 55)
+spawn_item(item = Items.EGG, amount = 1, x = 3226, z = 3301, respawnCycles = 55)
+
 spawn_npc(npc = Npcs.FARMER, x = 3226, z = 3290, walkRadius = 4, direction = Direction.WEST)
 spawn_npc(npc = Npcs.CHICKEN, x = 3234, z = 3297, walkRadius = 4, direction = Direction.WEST)
 spawn_npc(npc = Npcs.CHICKEN, x = 3230, z = 3296, walkRadius = 4, direction = Direction.SOUTH_WEST)
