b54cd36cab4bc0af46205684ff928d1b8b312f3d
Alycia <charliesalter@hotmail.com>
2023-01-22 22:23:48 -0800

    feat: onion picking plugin

---
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/objs/pick/pick_onion.plugin.kts
@@ -0,0 +1,34 @@
+package gg.rsmod.plugins.content.objs.pick
+
+val RESPAWN_DELAY = 100
+
+val ids = arrayOf(Objs.ONION_3366)
+
+ids.forEach {
+    on_obj_option(obj = it, option = "pick", lineOfSightDistance = 0) {
+        val obj = player.getInteractingGameObj()
+
+        if (obj.isSpawned(world)) {
+            player.queue {
+                val route = player.walkTo(this, obj.tile)
+                if (route.success) {
+                    if (player.inventory.isFull) {
+                        player.message("You don't have room for this onion.")
+                        return@queue
+                    }
+                    player.animate(827)
+                    val item = if (world.percentChance(5.0)) Items.ONION_SEED else Items.ONION
+                    wait(player.world.definitions.get(AnimDef::class.java, 827).cycleLength)
+                    player.inventory.add(item = item)
+                    world.remove(obj)
+                    world.queue {
+                        wait(RESPAWN_DELAY)
+                        world.spawn(DynamicObject(obj))
+                    }
+                } else {
+                    player.message(Entity.YOU_CANT_REACH_THAT)
+                }
+            }
+        }
+    }
+}
