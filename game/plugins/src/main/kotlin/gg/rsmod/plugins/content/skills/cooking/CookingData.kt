481164f3c19fffc245adb92cee75a525ba208d61
Alycia <charliesalter@hotmail.com>
2023-01-23 09:38:01 -0800

    chore: input more cooking data

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/skills/cooking/CookingData.kt
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/skills/cooking/CookingData.kt
@@ -5,9 +5,13 @@ import gg.rsmod.plugins.api.cfg.Items
 enum class CookingData(val raw: Int, val cooked: Int, val burnt: Int, val levelRequirement: Int, val experience: Double, val lowChance: Int, val highChance: Int) {
 
     SHRIMPS(raw = Items.RAW_SHRIMPS, cooked = Items.SHRIMPS, burnt = Items.BURNT_SHRIMP, levelRequirement = 1, experience = 30.0, lowChance = 128, highChance = 512),
+    ANCHOVIES(raw = Items.RAW_ANCHOVIES, cooked = Items.ANCHOVIES, burnt = Items.BURNT_FISH, levelRequirement = 1, experience = 30.0, lowChance = 128, highChance = 512),
+    CRAYFISH(raw = Items.RAW_CRAYFISH, cooked = Items.CRAYFISH, burnt = Items.BURNT_CRAYFISH, levelRequirement = 1, experience = 30.0, lowChance = 128, highChance = 512),
+    SARDINE(raw = Items.RAW_SARDINE, cooked = Items.SARDINE, burnt = Items.BURNT_FISH_369, levelRequirement = 1, experience = 40.0, lowChance = 118, highChance = 492),
 
 
-
+    RABBIT(raw = Items.RAW_RABBIT, cooked = Items.COOKED_RABBIT, burnt = Items.BURNT_RABBIT, levelRequirement = 1, experience = 30.0, lowChance = 128, highChance = 512),
+    CHICKEN(raw = Items.RAW_CHICKEN, cooked = Items.CHICKEN, burnt = Items.BURNT_CHICKEN, levelRequirement = 1, experience = 30.0, lowChance = 128, highChance = 512),
     BEEF(raw = Items.RAW_BEEF, cooked = Items.COOKED_MEAT, burnt = Items.BURNT_MEAT, levelRequirement = 1, experience = 30.0, lowChance = 128, highChance = 512),
     BEAR(raw = Items.RAW_BEAR_MEAT, cooked = Items.COOKED_MEAT, burnt = Items.BURNT_MEAT, levelRequirement = 1, experience = 30.0, lowChance = 128, highChance = 512),
     RAT(raw = Items.RAW_RAT_MEAT, cooked = Items.COOKED_MEAT, burnt = Items.BURNT_MEAT, levelRequirement = 1, experience = 30.0, lowChance = 128, highChance = 512),
