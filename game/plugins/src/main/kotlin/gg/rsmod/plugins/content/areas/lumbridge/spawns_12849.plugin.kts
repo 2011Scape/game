9ec88d1c71f812ab5dbc4dfd4dda87604daf32a7
Alycia <charliesalter@hotmail.com>
2023-01-30 00:03:37 -0800

    feat: spawns for region 12849

---
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/lumbridge/spawns_12849.plugin.kts
@@ -0,0 +1,20 @@
+package gg.rsmod.plugins.content.areas.lumbridge
+
+spawn_item(item = Items.SMALL_FISHING_NET, amount = 1, x = 3245, z = 3155, respawnCycles = 160)
+spawn_item(item = Items.SMALL_FISHING_NET, amount = 1, x = 3244, z = 3159, respawnCycles = 160)
+
+spawn_npc(npc = Npcs.GIANT_RAT, x = 3212, z = 3191, walkRadius = 4, direction = Direction.SOUTH)
+spawn_npc(npc = Npcs.GIANT_RAT, x = 3207, z = 3192, walkRadius = 4, direction = Direction.NORTH_EAST)
+spawn_npc(npc = Npcs.GIANT_RAT, x = 3216, z = 3184, walkRadius = 4, direction = Direction.NORTH_EAST)
+spawn_npc(npc = Npcs.GIANT_RAT, x = 3224, z = 3174, walkRadius = 4, direction = Direction.EAST)
+spawn_npc(npc = Npcs.GIANT_RAT, x = 3229, z = 3183, walkRadius = 4, direction = Direction.SOUTH_WEST)
+spawn_npc(npc = Npcs.GIANT_RAT, x = 3222, z = 3185, walkRadius = 4, direction = Direction.WEST)
+spawn_npc(npc = Npcs.GIANT_RAT, x = 3216, z = 3177, walkRadius = 4, direction = Direction.EAST)
+spawn_npc(npc = Npcs.GIANT_RAT, x = 3200, z = 3182, walkRadius = 4, direction = Direction.SOUTH_WEST)
+spawn_npc(npc = Npcs.GIANT_RAT, x = 3202, z = 3189, walkRadius = 4, direction = Direction.SOUTH)
+spawn_npc(npc = Npcs.FISHING_SPOT_NET_BAIT, x = 3248, z = 3161, direction = Direction.EAST)
+spawn_npc(npc = Npcs.FISHING_SPOT_NET_BAIT, x = 3246, z = 3156, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.FISHING_SPOT_NET_BAIT, x = 3245, z = 3152, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.FISHING_SPOT_NET_BAIT, x = 3242, z = 3148, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.CORDERO, x = 3231, z = 3197, walkRadius = 4, direction = Direction.SOUTH)
+spawn_npc(npc = Npcs.FINLAY, x = 3245, z = 3157, direction = Direction.SOUTH_EAST, static = true)
