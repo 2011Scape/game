7860958e96f1c4e8f0817baeb2a98791039cd461
Alycia <charliesalter@hotmail.com>
2023-02-05 13:01:51 -0800

    fix: update black hatchet level requirement

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/skills/woodcutting/AxeType.kt
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/skills/woodcutting/AxeType.kt
@@ -9,7 +9,7 @@ enum class AxeType(val item: Int, val level: Int, val animation: Int, val ratio:
     BRONZE(item = Items.BRONZE_HATCHET, level = 1, animation = 879, ratio = 1.0),
     IRON(item = Items.IRON_HATCHET, level = 1, animation = 877, ratio = 1.5),
     STEEL(item = Items.STEEL_HATCHET, level = 6, animation = 875, ratio = 2.0),
-    BLACK(item = Items.BLACK_HATCHET, level = 11, animation = 873, ratio = 2.25),
+    BLACK(item = Items.BLACK_HATCHET, level = 6, animation = 873, ratio = 2.25),
     MITHRIL(item = Items.MITHRIL_HATCHET, level = 21, animation = 871, ratio = 2.5),
     ADAMANT(item = Items.ADAMANT_HATCHET, level = 31, animation = 869, ratio = 3.0),
     RUNE(item = Items.RUNE_HATCHET, level = 41, animation = 867, ratio = 3.5),
