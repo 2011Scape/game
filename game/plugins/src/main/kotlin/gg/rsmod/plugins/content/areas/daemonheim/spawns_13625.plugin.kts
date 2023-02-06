a91b330f64171a45d00472b1ffc699db8d2db50d
Alycia <charliesalter@hotmail.com>
2023-02-05 21:21:13 -0800

    feat: daemonheim spawns

---
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/daemonheim/spawns_13625.plugin.kts
@@ -0,0 +1,7 @@
+package gg.rsmod.plugins.content.areas.daemonheim
+
+spawn_npc(npc = Npcs.REWARDS_TRADER, x = 3443, z = 3698, direction = Direction.SOUTH_WEST, static = true)
+spawn_npc(npc = Npcs.THOK_MASTER_OF_DUNGEONEERING, x = 3439, z = 3698, direction = Direction.SOUTH, static = true)
+spawn_npc(npc = Npcs.ESTRITH, x = 3441, z = 3694, direction = Direction.EAST, walkRadius = 4)
+spawn_npc(npc = Npcs.DUNGEONEERING_TUTOR, x = 3452, z = 3720, direction = Direction.SOUTH, static = true)
+spawn_npc(npc = Npcs.FREMENNIK_BANKER, x = 3448, z = 3720, direction = Direction.SOUTH_WEST, static = true)
---
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/daemonheim/spawns_13881.plugin.kts
@@ -0,0 +1,3 @@
+package gg.rsmod.plugins.content.areas.daemonheim
+
+spawn_npc(npc = Npcs.FREMENNIK_SHIPMASTER_9708, x = 3513, z = 3694, direction = Direction.WEST, static = true)
