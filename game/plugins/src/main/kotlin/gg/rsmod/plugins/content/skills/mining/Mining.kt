36981150b6ac8930ffefed4dfb3cc8682d160ade
Haydar <hjerew@student.unimelb.edu.au>
2023-02-15 15:26:25 +1100

    Updated mining ore success rate calculation, gems given, pickaxe functionality and added varrock armour functionality

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/skills/mining/Mining.kt
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/skills/mining/Mining.kt
@@ -1,7 +1,6 @@
 package gg.rsmod.plugins.content.skills.mining
 
 import gg.rsmod.game.fs.def.ItemDef
-import gg.rsmod.game.fs.def.ObjectDef
 import gg.rsmod.game.model.entity.DynamicObject
 import gg.rsmod.game.model.entity.GameObject
 import gg.rsmod.game.model.entity.Player
@@ -9,12 +8,18 @@ import gg.rsmod.game.model.queue.QueueTask
 import gg.rsmod.plugins.api.EquipmentType
 import gg.rsmod.plugins.api.Skills
 import gg.rsmod.plugins.api.cfg.Items
-import gg.rsmod.plugins.api.ext.*
+import gg.rsmod.plugins.api.ext.filterableMessage
+import gg.rsmod.plugins.api.ext.getEquipment
+import gg.rsmod.plugins.api.ext.hasEquipped
+import gg.rsmod.plugins.api.ext.interpolate
+import gg.rsmod.plugins.api.ext.messageBox
+import gg.rsmod.plugins.api.ext.player
+import gg.rsmod.plugins.api.ext.pluralSuffix
 import java.util.*
 
 object Mining {
-    data class Rock(val type: RockType, val obj: Int)
-    suspend fun mineRock(it: QueueTask, obj: GameObject, rock: RockType) {
+    data class Rock(val type: RockType, val obj: Int, val depletedRock: Int)
+    suspend fun mineRock(it: QueueTask, obj: GameObject, rock: RockType, depletedRockId: Int) {
         val p = it.player
         if (!canMine(it, p, obj, rock)) {
             return
@@ -33,42 +38,54 @@ object Mining {
                 break
             }
             val level = p.getSkills().getCurrentLevel(Skills.MINING)
-            if (level.interpolate(minChance = 60, maxChance = 190, minLvl = 1, maxLvl = 99, cap = 255)) {
-                p.filterableMessage("You manage to get some ${oreName.pluralSuffix(1).lowercase(Locale.getDefault())}.")
+            if (level.interpolate(rock.minChance,rock.maxChance,minLvl = 1, maxLvl = 99, cap = 255)) {
+                p.filterableMessage("You manage to get some ${oreName.pluralSuffix(2).lowercase(Locale.getDefault())}.")
 
                 if (p.hasEquipped(
                         EquipmentType.AMULET, Items.AMULET_OF_GLORY_1, Items.AMULET_OF_GLORY_2,
-                        Items.AMULET_OF_GLORY_3, Items.AMULET_OF_GLORY_4
+                        Items.AMULET_OF_GLORY_3, Items.AMULET_OF_GLORY_4, Items.AMULET_OF_GLORY_T,
+                        Items.AMULET_OF_GLORY_T1, Items.AMULET_OF_GLORY_T2, Items.AMULET_OF_GLORY_T3,
+                        Items.AMULET_OF_GLORY_T4, Items.AMULET_OF_GLORY_T_10719, Items.AMULET_OF_GLORY_8283
                     )
                 ) {
                     val chanceOfFindingGem = (1..86).random()
                     if (chanceOfFindingGem == 86) {
-                        p.inventory.add(Items.UNCUT_DIAMOND + ((0..7).random() * 2))
+                        p.inventory.add(Items.UNCUT_DIAMOND + ((0..3).random() * 2))
                     }
                 } else {
                     val chanceOfFindingGem = (1..256).random()
                     if (chanceOfFindingGem == 256) {
-                        p.inventory.add(Items.UNCUT_DIAMOND + ((0..7).random() * 2))
+                        p.inventory.add(Items.UNCUT_DIAMOND + ((0..3).random() * 2))
+                    }
+                }
+
+                if(p.hasEquipped(EquipmentType.CHEST,Items.VARROCK_ARMOUR_1, Items.VARROCK_ARMOUR_2,
+                        Items.VARROCK_ARMOUR_3,Items.VARROCK_ARMOUR_4)) {
+                    if((rock.varrockArmourAffected-(p.getEquipment(EquipmentType.CHEST)?.id ?: -1))>=0){
+                        p.inventory.add(rock.ore)
                     }
                 }
                 p.inventory.add(rock.ore)
                 p.addXp(Skills.MINING, rock.xp)
                 p.animate(-1)
-                val depletedRock = p.world.definitions.get(ObjectDef::class.java, obj.id).depleted
-                if (depletedRock != -1) {
+                if (depletedRockId != -1) {
                     val world = p.world
                     world.queue {
-                        val depletedOre = DynamicObject(obj, depletedRock)
+                        val depletedOre = DynamicObject(obj, depletedRockId)
                         world.remove(obj)
                         world.spawn(depletedOre)
-                        wait(rock.respawnTime.random())
+                        wait(rock.respawnTime) // need to add half timer here for mining guild runite ore
                         world.remove(depletedOre)
                         world.spawn(DynamicObject(obj))
                     }
                 }
                 break
             }
-            it.wait(2)
+            if ((pick.item != Items.DRAGON_PICKAXE)){
+                it.wait(pick.ticksBetweenRolls)
+            } else {
+                it.wait(pick.ticksBetweenRolls - if(((1..6).random() == 6)) {1} else {0})
+            }
         }
     }
     private suspend fun canMine(it: QueueTask, p: Player, obj: GameObject, rock: RockType): Boolean {
@@ -80,7 +97,7 @@ object Mining {
                 .getMaxLevel(Skills.MINING) >= it.level && (p.equipment.contains(it.item) || p.inventory.contains(it.item))
         }
         if (pick == null) {
-            it.messageBox("You need a pickaxe to mine this rock. You do not have a pickaxe<br><br>which you have the Mining level to use.")
+            it.messageBox("You need a pickaxe to mine this rock. You do not have a pickaxe which you have the Mining level to use.")
             return false
         }
         if (p.getSkills().getMaxLevel(Skills.MINING) < rock.level) {
--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/skills/mining/RockType.kt
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/skills/mining/RockType.kt
@@ -1,17 +1,28 @@
 package gg.rsmod.plugins.content.skills.mining
 
 import gg.rsmod.plugins.api.cfg.Items
-enum class RockType(val level: Int, val xp: Double, val ore: Int, val nonDepleteChance: Double, val respawnTime: IntRange) {
-    COPPER(level = 1, xp = 17.5, ore = Items.COPPER_ORE, nonDepleteChance = 0.0, respawnTime = 15..25),
-    TIN(level = 1, xp = 17.5, ore = Items.TIN_ORE, nonDepleteChance = 0.0, respawnTime = 15..25),
-    CLAY(level= 5, xp = 5.0, ore = Items.CLAY, nonDepleteChance = 0.0, respawnTime = 1..2 ),
-    IRON(level = 15, xp = 35.0, ore = Items.IRON_ORE, nonDepleteChance = 0.0, respawnTime = 15..25),
-    SILVER(level = 20, xp = 40.0, ore = Items.SILVER_ORE, nonDepleteChance = 50.0, respawnTime = 22..68),
-    COAL(level = 30, xp = 50.0, ore = Items.COAL, nonDepleteChance = 40.0, respawnTime = 22..68),
-    GOLD(level = 40, xp = 65.0, ore = Items.GOLD_ORE, nonDepleteChance = 33.333, respawnTime = 22..68),
-    MITHRIL(level = 55, xp = 80.0, ore = Items.MITHRIL_ORE, nonDepleteChance = 25.0, respawnTime = 22..68),
-    ADAMANTITE(level = 75, xp = 95.0, ore = Items.ADAMANTITE_ORE, nonDepleteChance = 16.66, respawnTime = 22..68),
-    RUNITE(level = 85, xp = 125.0, ore = Items.RUNITE_ORE, nonDepleteChance = 12.5, respawnTime = 22..68);
+enum class RockType(val level: Int, val xp: Double, val ore: Int, val respawnTime: Int, val varrockArmourAffected: Int,
+                    val minChance: Int, val maxChance: Int) {
+    CLAY(level= 5, xp = 5.0, ore = Items.CLAY, respawnTime = 2, varrockArmourAffected = Items.VARROCK_ARMOUR_1,
+        minChance=128, maxChance = 392),
+    COPPER(level = 1, xp = 17.5, ore = Items.COPPER_ORE,  respawnTime = 4, varrockArmourAffected = Items.VARROCK_ARMOUR_1,
+        minChance=102, maxChance = 379),
+    TIN(level = 1, xp = 17.5, ore = Items.TIN_ORE, respawnTime = 4, varrockArmourAffected = Items.VARROCK_ARMOUR_1,
+        minChance=102, maxChance = 379),
+    IRON(level = 15, xp = 35.0, ore = Items.IRON_ORE,  respawnTime = 9, varrockArmourAffected = Items.VARROCK_ARMOUR_1,
+        minChance=109, maxChance = 346),
+    SILVER(level = 20, xp = 40.0, ore = Items.SILVER_ORE,  respawnTime = 100, varrockArmourAffected = Items.VARROCK_ARMOUR_1,
+        minChance=24, maxChance = 200),
+    COAL(level = 30, xp = 50.0, ore = Items.COAL,  respawnTime = 50, varrockArmourAffected = Items.VARROCK_ARMOUR_1,
+        minChance=15, maxChance = 100),
+    GOLD(level = 40, xp = 65.0, ore = Items.GOLD_ORE,  respawnTime = 100, varrockArmourAffected = Items.VARROCK_ARMOUR_1,
+        minChance=6, maxChance = 75),
+    MITHRIL(level = 55, xp = 80.0, ore = Items.MITHRIL_ORE,  respawnTime = 200, varrockArmourAffected = Items.VARROCK_ARMOUR_2,
+        minChance=2, maxChance = 50),
+    ADAMANTITE(level = 75, xp = 95.0, ore = Items.ADAMANTITE_ORE,  respawnTime = 400, varrockArmourAffected = Items.VARROCK_ARMOUR_3,
+        minChance=-1, maxChance = 25),
+    RUNITE(level = 85, xp = 125.0, ore = Items.RUNITE_ORE,  respawnTime = 1200, varrockArmourAffected = Items.VARROCK_ARMOUR_4,
+        minChance=-1, maxChance = 18);
     companion object {
         val values = enumValues<RockType>()
     }
