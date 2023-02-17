13df37f910c71a654456e457e7f7185d20dc9dee
Alycia <charliesalter@hotmail.com>
2023-02-16 22:27:43 -0800

    chore: refactor mining plugin

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/skills/mining/Mining.kt
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/skills/mining/Mining.kt
@@ -9,65 +9,52 @@ import gg.rsmod.game.model.queue.QueueTask
 import gg.rsmod.plugins.api.EquipmentType
 import gg.rsmod.plugins.api.Skills
 import gg.rsmod.plugins.api.cfg.Items
-import gg.rsmod.plugins.api.ext.filterableMessage
-import gg.rsmod.plugins.api.ext.getEquipment
-import gg.rsmod.plugins.api.ext.hasEquipped
-import gg.rsmod.plugins.api.ext.interpolate
-import gg.rsmod.plugins.api.ext.messageBox
-import gg.rsmod.plugins.api.ext.player
-import gg.rsmod.plugins.api.ext.pluralSuffix
+import gg.rsmod.plugins.api.ext.*
 import java.util.*
 
 object Mining {
     suspend fun mineRock(it: QueueTask, obj: GameObject, rock: RockType) {
         val p = it.player
+
         if (!canMine(it, p, obj, rock)) {
             return
         }
+
         val oreName = p.world.definitions.get(ItemDef::class.java, rock.reward).name
+
         val pick = PickaxeType.values.firstOrNull {
             p.getSkills()
                 .getMaxLevel(Skills.MINING) >= it.level && (p.equipment.contains(it.item) || p.inventory.contains(it.item))
         }!!
+
         p.filterableMessage("You swing your pick at the rock.")
+
         while (true) {
-            p.animate(pick.animation)
-            it.wait(2)
             if (!canMine(it, p, obj, rock)) {
                 p.animate(-1)
                 break
             }
+            p.animate(pick.animation)
+            it.wait(pick.ticksBetweenRolls)
             val level = p.getSkills().getCurrentLevel(Skills.MINING)
-            if (level.interpolate(rock.lowChance, rock.highChance, minLvl = 1, maxLvl = 99, cap = 255)) {
+            if (interpolate(rock.lowChance, rock.highChance, level) > RANDOM.nextInt(255)) {
                 p.filterableMessage("You manage to get some ${oreName.pluralSuffix(2).lowercase(Locale.getDefault())}.")
 
-                if (p.hasEquipped(
-                        EquipmentType.AMULET, Items.AMULET_OF_GLORY_1, Items.AMULET_OF_GLORY_2,
-                        Items.AMULET_OF_GLORY_3, Items.AMULET_OF_GLORY_4, Items.AMULET_OF_GLORY_T,
-                        Items.AMULET_OF_GLORY_T1, Items.AMULET_OF_GLORY_T2, Items.AMULET_OF_GLORY_T3,
-                        Items.AMULET_OF_GLORY_T4, Items.AMULET_OF_GLORY_T_10719, Items.AMULET_OF_GLORY_8283
-                    )
-                ) {
-                    val chanceOfFindingGem = (1..86).random()
-                    if (chanceOfFindingGem == 86) {
-                        p.inventory.add(Items.UNCUT_DIAMOND + ((0..3).random() * 2))
-                    }
-                } else {
-                    val chanceOfFindingGem = (1..256).random()
-                    if (chanceOfFindingGem == 256) {
-                        p.inventory.add(Items.UNCUT_DIAMOND + ((0..3).random() * 2))
-                    }
+                var chanceOfGem = p.world.random(256)
+                if (p.hasEquipped(EquipmentType.AMULET, Items.AMULET_OF_GLORY_1, Items.AMULET_OF_GLORY_2, Items.AMULET_OF_GLORY_3, Items.AMULET_OF_GLORY_4, Items.AMULET_OF_GLORY_T, Items.AMULET_OF_GLORY_T1, Items.AMULET_OF_GLORY_T2, Items.AMULET_OF_GLORY_T3, Items.AMULET_OF_GLORY_T4, Items.AMULET_OF_GLORY_T_10719, Items.AMULET_OF_GLORY_8283)) {
+                    chanceOfGem = p.world.random(86)
                 }
 
-                if (p.hasEquipped(
-                        EquipmentType.CHEST, Items.VARROCK_ARMOUR_1, Items.VARROCK_ARMOUR_2,
-                        Items.VARROCK_ARMOUR_3, Items.VARROCK_ARMOUR_4
-                    )
-                ) {
+                if(chanceOfGem == 1) {
+                    p.inventory.add(Items.UNCUT_DIAMOND + (p.world.random(0..3) * 2))
+                }
+
+                if (p.hasEquipped(EquipmentType.CHEST, Items.VARROCK_ARMOUR_1, Items.VARROCK_ARMOUR_2, Items.VARROCK_ARMOUR_3, Items.VARROCK_ARMOUR_4)) {
                     if ((rock.varrockArmourAffected - (p.getEquipment(EquipmentType.CHEST)?.id ?: -1)) >= 0) {
                         p.inventory.add(rock.reward)
                     }
                 }
+
                 p.inventory.add(rock.reward)
                 p.addXp(Skills.MINING, rock.experience)
                 p.animate(-1)
@@ -78,23 +65,13 @@ object Mining {
                         val depletedOre = DynamicObject(obj, depletedRockId)
                         world.remove(obj)
                         world.spawn(depletedOre)
-                        wait(rock.respawnDelay) // need to add half timer here for mining guild runite ore
+                        // TODO: add support mining guild runite ore respawn timer
+                        wait(rock.respawnDelay)
                         world.remove(depletedOre)
                         world.spawn(DynamicObject(obj))
                     }
                 }
                 break
-            }
-            if ((pick.item != Items.DRAGON_PICKAXE)) {
-                it.wait(pick.ticksBetweenRolls)
-            } else {
-                it.wait(
-                    pick.ticksBetweenRolls - if (((1..6).random() == 6)) {
-                        1
-                    } else {
-                        0
-                    }
-                )
             }
         }
     }
