10abe9a4f9f66a2f59c58272217ad5f1e097b607
Alycia <charliesalter@hotmail.com>
2023-02-23 23:07:08 -0800

    feat: spawns for 12596, 12597, 12852, 12853

---
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/varrock/spawns_12596.plugin.kts
@@ -0,0 +1,20 @@
+package gg.rsmod.plugins.content.areas.varrock
+
+/**
+ * @author Alycia <https://github.com/alycii>
+ */
+
+spawn_npc(npc = Npcs.CHICKEN, x = 3197, z = 3356, walkRadius = 5, direction = Direction.EAST)
+spawn_npc(npc = Npcs.DUCK, x = 3179, z = 3348, walkRadius = 5, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.DUCK, x = 3178, z = 3353, walkRadius = 5, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.GIANT_SPIDER, x = 3146, z = 3347, walkRadius = 5, direction = Direction.WEST)
+spawn_npc(npc = Npcs.GUILDMASTER, x = 3190, z = 3358, walkRadius = 5, direction = Direction.NORTH)
+spawn_npc(npc = Npcs.VALAINE, x = 3192, z = 3359, height = 1, walkRadius = 5, direction = Direction.WEST)
+spawn_npc(npc = Npcs.SCAVVO, x = 3192, z = 3355, height = 1, walkRadius = 5, direction = Direction.SOUTH)
+spawn_npc(npc = Npcs.KATRINE, x = 3185, z = 3385, walkRadius = 5, direction = Direction.SOUTH)
+spawn_npc(npc = Npcs.CHICKEN_1017, x = 3198, z = 3359, walkRadius = 5, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.DREVEN, x = 3181, z = 3359, walkRadius = 5, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.TOOL_LEPRECHAUN, x = 3182, z = 3354, walkRadius = 5, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.THIEF_5926, x = 3184, z = 3386, walkRadius = 5, direction = Direction.EAST)
+spawn_npc(npc = Npcs.THIEF_5926, x = 3185, z = 3391, walkRadius = 5, direction = Direction.SOUTH_WEST)
+
---
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/varrock/spawns_12852.plugin.kts
@@ -0,0 +1,37 @@
+package gg.rsmod.plugins.content.areas.varrock
+
+/**
+ * @author Alycia <https://github.com/alycii>
+ */
+
+spawn_item(item = Items.PHOENIX_CROSSBOW, amount = 1, x = 3245, z = 3385, height = 1, respawnCycles = 70)
+spawn_item(item = Items.PHOENIX_CROSSBOW, amount = 1, x = 3243, z = 3383, height = 1, respawnCycles = 70)
+spawn_item(item = Items.LEATHER_GLOVES, amount = 1, x = 3242, z = 3385, height = 1, respawnCycles = 110)
+spawn_item(item = Items.LEATHER_BOOTS, amount = 1, x = 3244, z = 3386, height = 1, respawnCycles = 210)
+spawn_item(item = Items.IRON_DAGGER, amount = 1, x = 3242, z = 3383, height = 1, respawnCycles = 260)
+
+spawn_npc(npc = Npcs.SHEEP, x = 3237, z = 3346, walkRadius = 5, direction = Direction.NORTH)
+spawn_npc(npc = Npcs.SHEEP_43, x = 3241, z = 3354, walkRadius = 5, direction = Direction.WEST)
+spawn_npc(npc = Npcs.DARK_WIZARD, x = 3225, z = 3369, walkRadius = 5, direction = Direction.WEST)
+spawn_npc(npc = Npcs.DARK_WIZARD, x = 3226, z = 3371, walkRadius = 5, direction = Direction.SOUTH_EAST)
+spawn_npc(npc = Npcs.DARK_WIZARD, x = 3230, z = 3372, walkRadius = 5, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.DARK_WIZARD, x = 3230, z = 3370, walkRadius = 5, direction = Direction.WEST)
+spawn_npc(npc = Npcs.DARK_WIZARD, x = 3229, z = 3368, walkRadius = 5, direction = Direction.NORTH)
+spawn_npc(npc = Npcs.DARK_WIZARD, x = 3227, z = 3367, walkRadius = 5, direction = Direction.NORTH)
+spawn_npc(npc = Npcs.DARK_WIZARD_174, x = 3225, z = 3370, walkRadius = 5, direction = Direction.EAST)
+spawn_npc(npc = Npcs.DARK_WIZARD_174, x = 3230, z = 3370, walkRadius = 5, direction = Direction.WEST)
+spawn_npc(npc = Npcs.WEAPONSMASTER, x = 3246, z = 3383, height = 1, walkRadius = 5, direction = Direction.SOUTH_WEST)
+spawn_npc(npc = Npcs.IMP, x = 3208, z = 3357, walkRadius = 5, direction = Direction.WEST)
+spawn_npc(npc = Npcs.MASTER_FARMER_2235, x = 3241, z = 3345, walkRadius = 5, direction = Direction.EAST)
+spawn_npc(npc = Npcs.TARQUIN, x = 3203, z = 3344, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.SHEEP, x = 3229, z = 3347, walkRadius = 5, direction = Direction.NORTH)
+spawn_npc(npc = Npcs.SHEEP_5148, x = 3238, z = 3347, walkRadius = 5, direction = Direction.NORTH)
+spawn_npc(npc = Npcs.SHEEP_5149, x = 3233, z = 3350, walkRadius = 5, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.SHEEP_5160, x = 3249, z = 3345, walkRadius = 5, direction = Direction.SOUTH_WEST)
+spawn_npc(npc = Npcs.SHEEP_5161, x = 3233, z = 3341, walkRadius = 5, direction = Direction.SOUTH_WEST)
+spawn_npc(npc = Npcs.SHEEP_5161, x = 3241, z = 3344, walkRadius = 5, direction = Direction.WEST)
+spawn_npc(npc = Npcs.GUARD_5919, x = 3205, z = 3379, walkRadius = 5, direction = Direction.SOUTH)
+spawn_npc(npc = Npcs.GUARD_5919, x = 3211, z = 3381, walkRadius = 5, direction = Direction.WEST)
+spawn_npc(npc = Npcs.GUARD_5920, x = 3211, z = 3378, walkRadius = 5, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.SWAN, x = 3242, z = 3342, walkRadius = 5, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.RAT_BURGISS, x = 3271, z = 3335, walkRadius = 5, direction = Direction.NORTH_WEST)
