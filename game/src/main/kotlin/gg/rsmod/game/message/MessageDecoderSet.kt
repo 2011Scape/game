9724e637e71eef9cf843ab5635f9874a95c115b4
Alycia <charliesalter@hotmail.com>
2023-02-23 23:05:24 -0800

    chore: prev commit (OpNpc4 packet)

--- game/src/main/kotlin/gg/rsmod/game/message/MessageDecoderSet.kt
+++ game/src/main/kotlin/gg/rsmod/game/message/MessageDecoderSet.kt
@@ -48,6 +48,7 @@ class MessageDecoderSet {
         put(OpNpc1Message::class.java, OpNpc1Decoder(), OpNpc1Handler(), structures)
         put(OpNpc2Message::class.java, OpNpc2Decoder(), OpNpc2Handler(), structures)
         put(OpNpc3Message::class.java, OpNpc3Decoder(), OpNpc3Handler(), structures)
+        put(OpNpc4Message::class.java, OpNpc4Decoder(), OpNpc4Handler(), structures)
 
         put(OpHeldUMessage::class.java, OpHeldUDecoder(), OpHeldUHandler(), structures)
         put(OpLocUMessage::class.java, OpLocUDecoder(), OpLocUHandler(), structures)
