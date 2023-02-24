12d05ca1b0dda2055b639c5a4ff26af355aa8892
Kevin <ksenez94@gmail.com>
2023-02-24 01:10:35 -0500

    revert: npc directions changes fix: add new parameter to direction and use that to set npc facing instead

--- game/src/main/kotlin/gg/rsmod/game/sync/segment/AddLocalNpcSegment.kt
+++ game/src/main/kotlin/gg/rsmod/game/sync/segment/AddLocalNpcSegment.kt
@@ -35,7 +35,7 @@ class AddLocalNpcSegment(val player: Player, val npc: Npc, private val requiresB
         val facing = if (npc.lastFacingDirection != Direction.NONE) npc.lastFacingDirection else Direction.SOUTH
 
         buf.putBits(15, npc.index)
-        buf.putBits(3, facing.walkValue - 5)
+        buf.putBits(3, (facing.faceNpc) - 5)
         buf.putBits(1, if (requiresBlockUpdate) 1 else 0)
         buf.putBits(if (largeScene) 8 else 5, dz)
         buf.putBits(2, npc.tile.height)
