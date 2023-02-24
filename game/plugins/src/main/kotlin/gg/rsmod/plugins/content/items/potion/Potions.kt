bd1e71d9ee4920124991ffe7912bdf17ea40469f
rchesh <mrvgman@hotmail.com>
2023-02-24 13:16:32 -0700

    fix: potion mechanics
    - Corrected the sound played from the eating sound to the drinking sound.
    - Removed un-needed variables.
    - Corrected the timer to use potion delay so combo eating works.
    - 2011RS messages

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/items/potion/Potions.kt
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/items/potion/Potions.kt
@@ -2,10 +2,8 @@ package gg.rsmod.plugins.content.items.potion
 
 import gg.rsmod.game.fs.def.ItemDef
 import gg.rsmod.game.model.entity.Player
-import gg.rsmod.game.model.timer.COMBO_FOOD_DELAY
-import gg.rsmod.game.model.timer.FOOD_DELAY
+import gg.rsmod.game.model.timer.POTION_DELAY
 import gg.rsmod.plugins.api.EquipmentType
-import gg.rsmod.plugins.api.Skills
 import gg.rsmod.plugins.api.cfg.Items
 import gg.rsmod.plugins.api.ext.filterableMessage
 import gg.rsmod.plugins.api.ext.hasEquipped
@@ -18,28 +16,36 @@ object Potions {
 
     private const val DRINK_POTION_ANIM = 829
     private const val DRINK_POTION_ON_SLED_ANIM = 1469
-    private const val DRINK_POTION_SOUND = 2393
+    private const val DRINK_POTION_SOUND = 2401
 
-    fun canDrink(p: Player, potion: Potion): Boolean = !p.timers.has(if (potion.potionType.comboFood) COMBO_FOOD_DELAY else FOOD_DELAY)
+    fun canDrink(p: Player): Boolean = !p.timers.has(POTION_DELAY)
 
     fun drink(p: Player, potion: Potion) {
-        val delay = if (potion.potionType.comboFood) COMBO_FOOD_DELAY else FOOD_DELAY
+
         val anim = if (p.hasEquipped(EquipmentType.WEAPON, Items.SLED)) DRINK_POTION_ON_SLED_ANIM else DRINK_POTION_ANIM
-        val oldHp = p.getSkills().getCurrentLevel(Skills.HITPOINTS)
-        val potionName = p.world.definitions.get(ItemDef::class.java, potion.item).name
+
         p.animate(anim)
         p.playSound(DRINK_POTION_SOUND)
-        // Potion application strategy handled in PotionType
+
         potion.potionType.apply(p)
+
         p.resetFacePawn()
-        p.timers[delay] = potion.potionType.tickDelay
-        var message = "You drink the ${potionName.lowercase()}."
-        if(potion.potionType.message.isNotEmpty()) {
+        p.timers[POTION_DELAY] = potion.potionType.tickDelay
+
+        val potionName = p.world.definitions.get(ItemDef::class.java, potion.item).name
+        var message = "You drink some of your ${potionName.replace(Regex(" \\(([1234])\\)$"), "").lowercase()}."
+        if (potion.potionType.message.isNotEmpty()) {
             message = potion.potionType.message
         }
         p.filterableMessage(message)
-        if (p.getSkills().getCurrentLevel(Skills.HITPOINTS) > oldHp) {
-            p.filterableMessage("It heals some health.")
+        if (potion.replacement == Items.VIAL || potion.replacement == Items.BEER_GLASS) {
+            p.filterableMessage("You have finished your potion.")
+        } else {
+            val num = potionName.substringAfter("(").substringBefore(")").toInt() - 1
+            val dosesLeftMessage = "You have $num doses of potion left."
+            p.filterableMessage(dosesLeftMessage)
         }
+
     }
+
 }
