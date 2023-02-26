0264aad5c1e4c0d31bb4a887f2b4fa575814e3c6
Alycia <charliesalter@hotmail.com>
2023-02-26 14:56:36 -0800

    feat: spawns/shops for dwarven mine

---
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/falador/dwarven_mines/dwarven_shopping_store.plugin.kts
@@ -0,0 +1,41 @@
+package gg.rsmod.plugins.content.areas.falador.dwarven_mines
+
+import gg.rsmod.plugins.content.mechanics.shops.CoinCurrency
+
+/**
+ * @author Alycia <https://github.com/alycii>
+ */
+
+val shopkeepers = arrayOf(Npcs.DWARF_582)
+
+create_shop("Dwarven Shopping Store", CoinCurrency(), containsSamples = false) {
+    items[0] = ShopItem(Items.EMPTY_POT, 5)
+    items[1] = ShopItem(Items.JUG, 2)
+    items[2] = ShopItem(Items.SHEARS, 2)
+    items[3] = ShopItem(Items.BUCKET, 3)
+    items[4] = ShopItem(Items.BOWL, 2)
+    items[5] = ShopItem(Items.CAKE_TIN, 2)
+    items[6] = ShopItem(Items.TINDERBOX_590, 2)
+    items[7] = ShopItem(Items.CHISEL, 2)
+    items[8] = ShopItem(Items.HAMMER, 5)
+}
+
+shopkeepers.forEach {
+    on_npc_option(it, "trade") {
+        player.openShop("Dwarven Shopping Store")
+    }
+
+    on_npc_option(it, "talk-to") {
+        player.queue {
+            chatNpc("Can I help you at all?")
+            when(options("Yes please. What are you selling?", "No thanks.", title = "Select an Option")) {
+                1 -> {
+                    player.openShop("Dwarven Shopping Store")
+                }
+                2 -> {
+                    chatPlayer("No thanks.")
+                }
+            }
+        }
+    }
+}
---
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/falador/dwarven_mines/hura.plugin.kts
@@ -0,0 +1,51 @@
+package gg.rsmod.plugins.content.areas.falador.dwarven_mines
+
+import gg.rsmod.plugins.content.mechanics.shops.CoinCurrency
+
+/**
+ * @author Alycia <https://github.com/alycii>
+ */
+
+val shopkeepers = arrayOf(Npcs.HURA)
+
+create_shop("Hura's Crossbow Shop", CoinCurrency(), purchasePolicy = PurchasePolicy.BUY_STOCK, containsSamples = false) {
+    items[0] = ShopItem(Items.WOODEN_STOCK, 5)
+    items[1] = ShopItem(Items.OAK_STOCK, 5)
+    items[2] = ShopItem(Items.WILLOW_STOCK, 5)
+    items[3] = ShopItem(Items.TEAK_STOCK, 5)
+    items[4] = ShopItem(Items.MAPLE_STOCK, 5)
+    items[5] = ShopItem(Items.MAHOGANY_STOCK, 0)
+    items[6] = ShopItem(Items.YEW_STOCK, 0)
+    items[7] = ShopItem(Items.BRONZE_LIMBS, 5)
+    items[8] = ShopItem(Items.IRON_LIMBS, 5)
+    items[9] = ShopItem(Items.STEEL_LIMBS, 5)
+    items[10] = ShopItem(Items.MITHRIL_LIMBS, 5)
+    items[11] = ShopItem(Items.ADAMANTITE_LIMBS, 0)
+    items[12] = ShopItem(Items.RUNITE_LIMBS, 0)
+    items[13] = ShopItem(Items.BRONZE_CROSSBOW, 0)
+    items[14] = ShopItem(Items.IRON_CROSSBOW, 0)
+    items[15] = ShopItem(Items.STEEL_CROSSBOW, 0)
+    items[16] = ShopItem(Items.MITH_CROSSBOW, 0)
+    items[17] = ShopItem(Items.ADAMANT_CROSSBOW, 0)
+    items[18] = ShopItem(Items.RUNE_CROSSBOW, 0)
+}
+
+shopkeepers.forEach {
+    on_npc_option(it, "trade") {
+        player.openShop("Hura's Crossbow Shop")
+    }
+
+    on_npc_option(it, "talk-to") {
+        player.queue {
+            chatNpc("Can I help you at all?")
+            when(options("Yes please. What are you selling?", "No thanks.", title = "Select an Option")) {
+                1 -> {
+                    player.openShop("Hura's Crossbow Shop")
+                }
+                2 -> {
+                    chatPlayer("No thanks.")
+                }
+            }
+        }
+    }
+}
