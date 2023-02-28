9ad29239284cf7420a3929baa707dd31c456f46b
Kevin <ksenez94@gmail.com>
2023-02-27 23:59:01 -0500

    fix: object transform state checking v3

--- game/src/main/kotlin/gg/rsmod/game/model/entity/GameObject.kt
+++ game/src/main/kotlin/gg/rsmod/game/model/entity/GameObject.kt
@@ -72,7 +72,7 @@ abstract class GameObject : Entity {
         if (def.varp != -1) {
             val state = player.varps.getState(def.varp)
             if(state >= def.transforms!!.size) {
-                return id
+                return def.transforms!![state - 1]
             }
             return def.transforms!![state]
         }
