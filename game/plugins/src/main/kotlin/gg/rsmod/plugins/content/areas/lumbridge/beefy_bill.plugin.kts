a4d5003fd1b9d906faca11564f30c17c9754ba8a
DesecratedTree <52549792+DesecratedTree@users.noreply.github.com>
2023-02-27 23:57:08 -0600

    fixed some stuff

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/lumbridge/beefy_bill.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/lumbridge/beefy_bill.plugin.kts
@@ -3,8 +3,8 @@ package gg.rsmod.plugins.content.areas.lumbridge
 import gg.rsmod.plugins.content.mechanics.shops.CoinCurrency
 
 create_shop(
-		"Beefy Bill's Supplies", 
-		currency = CoinCurrency(), 
+		"Beefy Bill's Supplies",
+		currency = CoinCurrency(),
 		purchasePolicy = PurchasePolicy.BUY_STOCK) ,
 		 containsSamples = false
 		{
@@ -21,11 +21,11 @@ create_shop(
     items[10] = ShopItem(Items.SHEARS, 10)
     items[11] = ShopItem(Items.SHORTBOW, 10)
     items[12] = ShopItem(Items.SPINACH_ROLL, 10)
-    items[13] = ShopItem(Items.TINDERBOX, 10)    
+    items[13] = ShopItem(Items.TINDERBOX, 10)
 }
 
 on_npc_option(Npcs.BEEFY_BILL, "trade") {
-    player.openShop("Beefy Bill's Supplies")
+	player.openShop("Beefy Bill's Supplies")
 }
 
 on_npc_option(Npcs.BEEFY_BILL, option = "talk-to") {
@@ -33,16 +33,12 @@ on_npc_option(Npcs.BEEFY_BILL, option = "talk-to") {
         chatNpc("Beefy Bill at your service! I can bank your beef, your cowhides and your flour. I've also got other stuff for trade. What's it to be?.", facialExpression = FacialExpression.HAPPY_TALKING)
         when (options("Let's trade.", "I want you to bank things for me.", "Who are you?")) {
             1 -> {
-            	player.openShop("Beefy Bill's Supplies")
-                chatPlayer("Can I buy some leather then?", facialExpression = FacialExpression.CONFUSED)
-                chatNpc("I make leather from animal hides. Bring me some", "cowhides and one gold coin per hide, and I'll tan them", "into soft leather for you.", facialExpression = FacialExpression.HAPPY_TALKING)
+				player.openShop("Beefy Bill's Supplies")
             }
             2 -> {
                 chatPlayer("I want you to bank these things for me.", facialExpression = FacialExpression.HAPPY_TALKING)
                 chatNpc("Excellent. Just hand me the items, and I'll work out a price for you. I charge a 10% commission.", facialExpression = FacialExpression.TALKING_ALOT)
-                
                 //TODO CHECK FOR ITEMS IN INVENTORY and bank them
-                
                 chatNpc("Pleasure doing business with ya, mate!")
             }
             3 -> {
