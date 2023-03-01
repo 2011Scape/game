69002559e431e15efbdcd9436f407f470d8ddc3b
Alycia <charliesalter@hotmail.com>
2023-02-28 22:42:57 -0800

    fix: lumbridge spawns

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/lumbridge/spawns_12850.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/lumbridge/spawns_12850.plugin.kts
@@ -104,7 +104,7 @@ spawn_npc(npc = Npcs.DUCK_2693, x = 3238, z = 3246, walkRadius = 4, direction = 
 spawn_npc(npc = Npcs.DUCK_2693, x = 3239, z = 3235, walkRadius = 4, direction = Direction.NORTH_WEST)
 spawn_npc(npc = Npcs.GOBLIN_3267, x = 3222, z = 3238, walkRadius = 5, direction = Direction.NORTH)
 spawn_npc(npc = Npcs.BARFY_BILL, x = 3232, z = 3252, walkRadius = 2, direction = Direction.EAST)
-spawn_npc(npc = Npcs.DOOMSAYER, x = 3230, z = 3228, direction = Direction.NORTH_EAST, static = true)
+spawn_npc(npc = Npcs.DOOMSAYER, x = 3230, z = 3228, direction = Direction.EAST, static = true)
 spawn_npc(npc = Npcs.MAGIC_INSTRUCTOR, x = 3212, z = 3253, walkRadius = 2, direction = Direction.WEST)
 spawn_npc(npc = Npcs.CADMUS, x = 3210, z = 3213, height = 1, walkRadius = 4, direction = Direction.NORTH)
 spawn_npc(npc = Npcs.PRIEST_YAUCHOMI, x = 3244, z = 3214, walkRadius = 4, direction = Direction.NORTH)
