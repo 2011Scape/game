f50d8dc6893eca952f1eb9680d6b9279920d3bff
Kevin <ksenez94@gmail.com>
2023-03-02 23:38:57 -0500

    feat: Battlestaff Crafting

---
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/skills/crafting/weaponry/battlestaff_crafting.plugin.kts
@@ -0,0 +1,25 @@
+package gg.rsmod.plugins.content.skills.crafting.weaponry
+
+val battlestaffs = BattlestaffData.values
+val staffDefinitions = BattlestaffData.battlestaffDefinitions
+val orbIds = battlestaffs.map { staff -> staff.orbId }.toIntArray()
+
+orbIds.forEach {
+    val item = it
+
+    on_item_on_item(item1 = Items.BATTLESTAFF, item2 = item) {
+        val def = staffDefinitions[item] ?: return@on_item_on_item
+        if (player.inventory.getItemCount(item) == 1) {
+            attachOrb(player, def.resultItem, amount = 1)
+            return@on_item_on_item
+        }
+        player.queue {
+            produceItemBox(def.resultItem, option = SkillDialogueOption.MAKE, logic = ::attachOrb)
+        }
+    }
+}
+
+fun attachOrb(player: Player, item: Int, amount: Int) {
+    val def = battlestaffs.associateBy { it.resultItem }[item] ?: return
+    player.queue { BattlestaffAction.attach(this, def, amount) }
+}
