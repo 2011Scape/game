1d7a06ba817fa2f3b25e19df6db87c8bb336a38c
Kevin <ksenez94@gmail.com>
2023-03-02 21:50:13 -0500

    feat: Glassblowing

---
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/skills/crafting/glassblowing/GlassData.kt
@@ -0,0 +1,19 @@
+package gg.rsmod.plugins.content.skills.crafting.glassblowing
+
+import gg.rsmod.plugins.api.cfg.Items
+
+enum class GlassData(val id: Int, val levelRequired: Int, val experience: Double) {
+    BEER_GLASS(id = Items.BEER_GLASS, levelRequired = 1, experience = 17.5),
+    EMPTY_CANDLE_LANTERN(id = Items.CANDLE_LANTERN, levelRequired = 4, experience = 19.0),
+    EMPTY_OIL_LAMP(id = Items.OIL_LAMP_4525, levelRequired = 12, experience = 25.0),
+    EMPTY_VIAL(id = Items.VIAL, levelRequired = 33, experience = 35.0),
+    FISHBOWL(id = Items.FISHBOWL, levelRequired = 42, experience = 42.5),
+    UNPOWERED_ORB(id = Items.UNPOWERED_ORB, levelRequired = 46, experience = 52.5),
+    LANTERN_LENS(id = Items.LANTERN_LENS, levelRequired = 49, experience = 55.0),
+    EMPTY_LIGHT_ORB(id = Items.EMPTY_LIGHT_ORB, levelRequired = 87, experience = 70.0);
+
+    companion object {
+        val values = enumValues<GlassData>()
+        val glassDefinitions = values.associateBy { it.id }
+    }
+}
---
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/skills/crafting/glassblowing/GlassblowingAction.kt
@@ -0,0 +1,61 @@
+package gg.rsmod.plugins.content.skills.crafting.glassblowing
+
+import gg.rsmod.game.fs.def.ItemDef
+import gg.rsmod.game.model.queue.QueueTask
+import gg.rsmod.plugins.api.Skills
+import gg.rsmod.plugins.api.cfg.Items
+import gg.rsmod.plugins.api.ext.filterableMessage
+import gg.rsmod.plugins.api.ext.itemMessageBox
+import gg.rsmod.plugins.api.ext.message
+import gg.rsmod.plugins.api.ext.player
+import gg.rsmod.util.Misc
+import kotlin.math.min
+
+object GlassblowingAction {
+
+    suspend fun craft(task: QueueTask, data: GlassData, amount: Int) {
+        val player = task.player
+        val inventory = player.inventory
+
+        val maxCount = min(amount, inventory.getItemCount(Items.MOLTEN_GLASS))
+
+        repeat(maxCount) {
+            if (!canCraft(task, data)) {
+                player.animate(-1)
+                return@repeat
+            }
+            player.animate(884)
+            if (!inventory.remove(item = Items.MOLTEN_GLASS, assureFullRemoval = true).hasSucceeded()) {
+                return@repeat
+            }
+            inventory.add(data.id, assureFullInsertion = true)
+            player.addXp(Skills.CRAFTING, data.experience)
+            player.filterableMessage("You make ${Misc.formatWithIndefiniteArticle(player.world.definitions.get(ItemDef::class.java, data.id).name.lowercase())}.")
+            task.wait(3)
+        }
+
+    }
+
+    private suspend fun canCraft(task: QueueTask, data: GlassData) : Boolean {
+        val player = task.player
+        val inventory = player.inventory
+        val resultName = player.world.definitions.get(ItemDef::class.java, data.id).name.lowercase()
+
+        if (!inventory.contains(Items.GLASSBLOWING_PIPE)) {
+            player.message("You need a glassblowing pipe to make ${Misc.formatWithIndefiniteArticle(resultName)}.")
+            return false
+        }
+
+        if (!inventory.contains(Items.MOLTEN_GLASS)) {
+            player.message("You need some molten glass to make ${Misc.formatWithIndefiniteArticle(resultName)}.")
+            return false
+        }
+
+        if (player.getSkills().getCurrentLevel(Skills.CRAFTING) < data.levelRequired) {
+            task.itemMessageBox("You need a Crafting level of ${data.levelRequired} to<br>craft ${Misc.formatWithIndefiniteArticle(resultName)}.", item = data.id)
+            return false
+        }
+        return true
+    }
+
+}
