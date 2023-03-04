61e7cb12fefda5e6f76b046ccb251f88cef3a131
Alycia <charliesalter@hotmail.com>
2023-03-03 23:38:18 -0800

    fix: update dwarven mine staircase

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/falador/dwarven_mines_staircase.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/falador/dwarven_mines_staircase.plugin.kts
@@ -4,10 +4,10 @@ package gg.rsmod.plugins.content.areas.falador
  * @author Alycia <https://github.com/alycii>
  */
 
-on_obj_option(obj = Objs.STAIRS_30944, option = "climb-down") {
-    player.moveTo(Tile(3058, 9776, 0))
-}
 
-on_obj_option(obj = Objs.STAIRS_30943, option = "climb-up") {
-    player.moveTo(Tile(3061, 3376, 0))
+on_obj_option(obj = Objs.STAIRS_30943, option ="climb-up") {
+    player.moveTo(player.tile.x + 3, player.tile.z - 6400)
+}
+on_obj_option(obj = Objs.STAIRS_30944, option ="climb-down") {
+    player.moveTo(player.tile.x - 3, player.tile.z + 6400)
 }
