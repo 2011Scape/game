fa0768a5afa4d1ab0e93675adcb6ea8b84b01618
Alycia <charliesalter@hotmail.com>
2023-03-05 16:20:23 -0800

    feat: MapAnim packet

--- game/src/main/kotlin/gg/rsmod/game/model/Graphic.kt
+++ game/src/main/kotlin/gg/rsmod/game/model/Graphic.kt
@@ -14,7 +14,7 @@ data class Graphic(val id: Int, val height: Int, val delay: Int = 0)
  *
  * @author Tom <rspsmods@gmail.com>
  */
-class TileGraphic private constructor(val id: Int, val height: Int, val delay: Int = 0) : Entity() {
+class TileGraphic private constructor(val id: Int, val height: Int, val delay: Int = 0, val rotation: Int = 0) : Entity() {
 
     constructor(tile: Tile, id: Int, height: Int, delay: Int = 0) : this(id, height, delay) {
         this.tile = tile
