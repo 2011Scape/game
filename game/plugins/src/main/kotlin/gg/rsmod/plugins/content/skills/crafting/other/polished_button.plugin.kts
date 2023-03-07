6a5ccb624eef2b63dc62ab7c819090c0909381a0
Paleocene <sjveld91@gmail.com>
2023-03-07 08:35:57 +0100

    feat: polished buttons

---
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/skills/crafting/other/polished_button.plugin.kts
@@ -0,0 +1,19 @@
+package gg.rsmod.plugins.content.skills.crafting.other
+
+on_item_option(Items.BUTTONS, "polish") {
+    player.queue(TaskPriority.WEAK) {
+        if (player.getSkills().getCurrentLevel(Skills.AGILITY) < 3) {
+            player.message("You rub the buttons on your clothes but they aren't improved by the process.")
+        } else {
+            wait(1)
+            val slot = player.getInteractingItemSlot()
+            if (player.inventory[slot]?.id == Items.BUTTONS) {
+                if (player.inventory.remove(Items.BUTTONS, beginSlot = slot).hasSucceeded()) {
+                    player.inventory.add(Items.POLISHED_BUTTONS, beginSlot = slot)
+                    player.message("You rub the buttons on your clothes and they become more shiny.")
+                    player.addXp(Skills.CRAFTING, 5.0)
+                }
+            }
+        }
+    }
+}
