4a1b3e2e9caa3ac5fcb4a08965fbb4a0e67fa10b
Alycia <charliesalter@hotmail.com>
2023-03-07 18:08:01 -0800

    fix: dont lock on bone burying

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/skills/prayer/burying/bone_burying.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/skills/prayer/burying/bone_burying.plugin.kts
@@ -11,13 +11,11 @@ bones.forEach { bone ->
             player.filterableMessage("You dig a hole in the ground...")
             player.animate(827)
             player.playSound(2738)
-            player.lock()
             wait(2)
             if (player.inventory.remove(item = bone, beginSlot = player.getInteractingItemSlot()).hasSucceeded()) {
                 player.filterableMessage("You bury the bones.")
                 player.addXp(Skills.PRAYER, definitions[bone]!!.experience)
             }
-            player.unlock()
         }
     }
 }
