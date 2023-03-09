5ab0946dc9eecab0c70658031a44e207756effe7
vl1 <mrvgman@hotmail.com>
2023-03-09 14:45:29 -0700

    fix: cleanup potion action a little bit

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/items/potion/potionDrinking.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/items/potion/potionDrinking.plugin.kts
@@ -1,16 +1,9 @@
 package gg.rsmod.plugins.content.items.potion
 
-Potion.values.forEach { potion ->
+val potionValues = Potion.values()
+
+potionValues.forEach { potion ->
     on_item_option(item = potion.item, option = "Drink") {
-        if (!Potions.canDrink(player)) {
-            return@on_item_option
-        }
-        val inventorySlot = player.getInteractingItemSlot()
-        if (player.inventory.remove(item = potion.item, beginSlot = inventorySlot).hasSucceeded()) {
-            Potions.drink(player, potion)
-            if (potion.replacement != -1) {
-                player.inventory.add(item = potion.replacement, beginSlot = inventorySlot)
-            }
-        }
+        Potions.drink(player, potion)
     }
 }
