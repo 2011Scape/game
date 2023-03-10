b6131b7599e7b8b1926198d03a778f0f48df9031
Alycia <charliesalter@hotmail.com>
2023-03-09 21:02:53 -0800

    feat: spawns for 12342, 12441.. 42, 43, 12698

---
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/spawns/spawns_12441.plugin.kts
@@ -0,0 +1,18 @@
+package gg.rsmod.plugins.content.areas.spawns
+
+/**
+ * @author Alycia <https://github.com/alycii>
+ */
+
+spawn_npc(npc = Npcs.HILL_GIANT, x = 3118, z = 9845, walkRadius = 5, direction = Direction.EAST)
+spawn_npc(npc = Npcs.HILL_GIANT, x = 3111, z = 9844, walkRadius = 5, direction = Direction.SOUTH)
+spawn_npc(npc = Npcs.HILL_GIANT, x = 3123, z = 9845, walkRadius = 5, direction = Direction.WEST)
+spawn_npc(npc = Npcs.HILL_GIANT, x = 3114, z = 9833, walkRadius = 5, direction = Direction.WEST)
+spawn_npc(npc = Npcs.HILL_GIANT, x = 3110, z = 9841, walkRadius = 5, direction = Direction.NORTH)
+spawn_npc(npc = Npcs.HILL_GIANT, x = 3119, z = 9839, walkRadius = 5, direction = Direction.NORTH)
+spawn_npc(npc = Npcs.HILL_GIANT, x = 3097, z = 9832, walkRadius = 5, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.HILL_GIANT, x = 3101, z = 9832, walkRadius = 5, direction = Direction.NORTH)
+spawn_npc(npc = Npcs.HILL_GIANT, x = 3107, z = 9829, walkRadius = 5, direction = Direction.WEST)
+spawn_npc(npc = Npcs.HILL_GIANT, x = 3115, z = 9831, walkRadius = 5, direction = Direction.EAST)
+spawn_npc(npc = Npcs.HILL_GIANT, x = 3109, z = 9835, walkRadius = 5, direction = Direction.EAST)
+
