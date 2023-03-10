d742d5696f9cd9bf498c8b15097764df200a89a7
Alycia <charliesalter@hotmail.com>
2023-03-09 21:03:49 -0800

    feat: brass key door plugin

---
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/edgeville/brass_key_door.plugin.kts
@@ -0,0 +1,45 @@
+package gg.rsmod.plugins.content.areas.edgeville
+
+/**
+ * @author Alycia <https://github.com/alycii>
+ */
+val OPEN_DOOR_SFX = 62
+val CLOSE_DOOR_SFX = 60
+
+on_obj_option(obj = Objs.DOOR_1804, option = "open") {
+    if(!player.inventory.contains(Items.BRASS_KEY)) {
+        player.message("The door is locked.")
+        return@on_obj_option
+    }
+
+    handleDoor(player)
+}
+
+on_obj_option(obj = Objs.LADDER_12389, option = "Climb-Down") {
+    player.handleBasicLadder(player, climbUp = false)
+}
+
+on_obj_option(obj = Objs.LADDER_29358, option = "Climb-Down") {
+    player.handleBasicLadder(player, climbUp = false)
+}
+
+
+fun handleDoor(player: Player) {
+    val closedDoor = DynamicObject(id = 1804, type = 0, rot = 1, tile = Tile(x = 3115, z = 3449))
+    player.lock = LockState.DELAY_ACTIONS
+    world.remove(closedDoor)
+    val door = DynamicObject(id = 1803, type = 0, rot = 1, tile = Tile(x = 3115, z = 3449))
+    player.playSound(id = OPEN_DOOR_SFX)
+    world.spawn(door)
+
+    player.queue {
+        val x = 3115
+        val z = if (player.tile.z == 3450) 3449 else 3450
+        player.walkTo(tile = Tile(x = x, z = z), detectCollision = false)
+        wait(3)
+        world.remove(door)
+        player.lock = LockState.NONE
+        world.spawn(closedDoor)
+        player.playSound(CLOSE_DOOR_SFX)
+    }
+}
