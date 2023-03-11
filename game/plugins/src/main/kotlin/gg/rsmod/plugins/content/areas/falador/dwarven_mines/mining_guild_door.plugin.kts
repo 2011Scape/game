63724b048ea5a5b5909e1ba9ed78a5ea7b7cd090
raynna <andre.as.henrik.sson@hotmail.com>
2023-03-11 08:21:22 +0100

    Added mining guild door plugin Also added to show Rotation in console for debug objects.

---
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/falador/dwarven_mines/mining_guild_door.plugin.kts
@@ -0,0 +1,35 @@
+package gg.rsmod.plugins.content.areas.falador.dwarven_mines
+
+val OPEN_DOOR_SFX = 62
+val CLOSE_DOOR_SFX = 60
+val MINING_LEVEL_REQ = 60
+
+on_obj_option(obj = Objs.DOOR_2112, option = "open") {
+    if(player.tile.z > 9756 && player.getSkills().getMaxLevel(Skills.MINING) < MINING_LEVEL_REQ) {
+        player.queue {
+            messageBox("You need a Mining level of $MINING_LEVEL_REQ to access the Mining Guild.")
+        }
+        return@on_obj_option
+    }
+    handleDoor(player)
+}
+
+fun handleDoor(player: Player) {
+    val closedDoor = DynamicObject(id = 2112, type = 0, rot = 1, tile = Tile(x = 3046, z = 9756))
+    player.lock = LockState.DELAY_ACTIONS
+    world.remove(closedDoor)
+    val door = DynamicObject(id = 2112, type = 0, rot = 2, tile = Tile(x = 3046, z = 9757))
+    player.playSound(id = OPEN_DOOR_SFX)
+    world.spawn(door)
+
+    player.queue {
+        val x = 3046
+        val z = if (player.tile.z == 9756) 9757 else 9756
+        player.walkTo(tile = Tile(x = x, z = z), detectCollision = false)
+        wait(3)
+        world.remove(door)
+        player.lock = LockState.NONE
+        world.spawn(closedDoor)
+        player.playSound(CLOSE_DOOR_SFX)
+    }
+}
