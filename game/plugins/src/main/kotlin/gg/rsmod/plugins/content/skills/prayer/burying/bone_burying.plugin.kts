4b526f14a91a3c03000868c2b3689f6f9f0008a7
Alycia <charliesalter@hotmail.com>
2023-03-10 18:43:53 -0800

    chore: document bone-burying plugin

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/skills/prayer/burying/bone_burying.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/skills/prayer/burying/bone_burying.plugin.kts
@@ -8,16 +8,24 @@ val bones = boneData.map { it.bone }.toTypedArray()
 bones.forEach { bone ->
     on_item_option(bone, option = "bury") {
         player.queue {
+            // lock the player non-full to commit to the action
             player.lock = LockState.DELAY_ACTIONS
+
+            // send the message, animate and play the sound
             player.filterableMessage("You dig a hole in the ground...")
             player.animate(827)
             player.playSound(2738)
+
+            // wait 2 ticks for the animation to play
             wait(2)
+
+            // remove the bones, and add experience
             if (player.inventory.remove(item = bone, beginSlot = player.getInteractingItemSlot()).hasSucceeded()) {
                 player.filterableMessage("You bury the bones.")
                 player.addXp(Skills.PRAYER, definitions[bone]!!.experience)
-                player.unlock()
             }
+
+            // finally, unlock the player
             player.unlock()
         }
     }
