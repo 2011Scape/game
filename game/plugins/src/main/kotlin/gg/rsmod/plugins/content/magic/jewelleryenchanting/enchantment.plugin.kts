d09a30bfcedb99d0c40619a0c242e9db9918a70b
Alycia <charliesalter@hotmail.com>
2023-03-12 18:27:04 -0700

    chore: add documentation

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/magic/jewelleryenchanting/enchantment.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/magic/jewelleryenchanting/enchantment.plugin.kts
@@ -87,41 +87,50 @@ on_spell_on_item(fromInterface = 192, fromComponent = 88) {
 }
 
 /**
- * Performs a high or low alchemy on the item the player is interacting with, and adds coins and experience
- * to the player's inventory if successful.
+ * Performs an enchantment spell on an item.
  *
- * @param player The player performing the alchemy.
- * @param highAlchemy Whether to perform a high alchemy. If false, a low alchemy will be performed.
- * @return true if the alchemy was successful, false otherwise.
+ * @param player The player casting the spell.
+ * @param spell The enchantment spell being cast.
+ * @param experience The amount of experience to give to the player for casting the spell.
+ * @return True if the spell was successfully cast, false otherwise.
  */
 fun performEnchantment(player: Player, spell: SpellbookData, experience: Double): Boolean {
+    // Get the item the player is attempting to enchant
     val item = player.getInteractingItem()
-    val product = EnchantmentData.values().find {it.raw == item.id}
 
+    // Get the product of the enchantment spell
+    val product = EnchantmentData.values().find { it.raw == item.id }
+
+    // Switch to the spellbook tab
     player.runClientScript(115, GameframeTab.SPELLBOOK.id)
 
-    if(product == null || product.spell != spell) {
+    // Check if the item can be enchanted
+    if (product == null || product.spell != spell) {
         player.message("You can't use this spell on this item.")
         return false
     }
 
-    val animation = when(spell) {
+    // Determine the animation and graphic to play based on the spell being cast
+    val animation = when (spell) {
         SpellbookData.LVL_1_ENCHANT -> 719
         SpellbookData.LVL_2_ENCHANT -> 720
         else -> 721
     }
-
-    val graphic = when(spell) {
+    val graphic = when (spell) {
         SpellbookData.LVL_1_ENCHANT -> Graphic(114, 92)
         SpellbookData.LVL_2_ENCHANT -> Graphic(115, 92)
         else -> Graphic(116, 92)
     }
 
+    // Play the animation and graphic
     player.animate(animation)
     player.graphic(graphic)
+
+    // Remove the item from the player's inventory and add the enchanted product
     if (player.inventory.remove(Item(item.id, 1)).hasSucceeded()) {
         player.inventory.add(product.product)
         player.addXp(Skills.MAGIC, experience)
     }
+
     return true
 }
