3baf4163f7c38ba77cddb7e4fa040cdbd684c46f
Kevin <ksenez94@gmail.com>
2023-03-12 21:50:13 -0400

    fix: More of prev commit

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/skills/crafting/jewellery/JewelleryItem.kt
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/skills/crafting/jewellery/JewelleryItem.kt
@@ -1,3 +1,5 @@
 package gg.rsmod.plugins.content.skills.crafting.jewellery
 
-data class JewelleryItem(val mould: JewelleryMould, val resultItem: Int, val levelRequired: Int, val experience: Double, val modelComponent: Int, val optionComponent: Int)
+import gg.rsmod.plugins.content.skills.crafting.MouldItems
+
+data class JewelleryItem(val mould: MouldItems, val resultItem: Int, val levelRequired: Int, val experience: Double, val modelComponent: Int, val optionComponent: Int)
