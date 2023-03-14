a782a3cd2fe6b56da41566fbba173d7d649c5a8d
DesecratedTree <52549792+DesecratedTree@users.noreply.github.com>
2023-03-14 01:17:59 -0500

    feat: add druidic ritual quest. (#179)

---
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/objs/cauldron_of_thunder.plugin.kts
@@ -0,0 +1,22 @@
+package gg.rsmod.plugins.content.objs
+
+on_item_on_obj(obj = Objs.CAULDRON_OF_THUNDER, item = Items.RAW_BEEF) { //adds interaction between meats and cauldron in tav dungeon
+    player.animate(883) //animation for dipping meat into cauldron
+    player.inventory.remove(Items.RAW_BEEF)
+    player.inventory.add(Items.ENCHANTED_BEEF)
+}
+on_item_on_obj(obj = Objs.CAULDRON_OF_THUNDER, item = Items.RAW_CHICKEN) {
+    player.animate(883)
+    player.inventory.remove(Items.RAW_CHICKEN)
+    player.inventory.add(Items.ENCHANTED_CHICKEN)
+}
+on_item_on_obj(obj = Objs.CAULDRON_OF_THUNDER, item = Items.RAW_BEAR_MEAT) {
+    player.animate(883)
+    player.inventory.remove(Items.RAW_BEAR_MEAT)
+    player.inventory.add(Items.ENCHANTED_BEAR_MEAT)
+}
+on_item_on_obj(obj = Objs.CAULDRON_OF_THUNDER, item = Items.RAW_RAT_MEAT) {
+    player.animate(883)
+    player.inventory.remove(Items.RAW_RAT_MEAT)
+    player.inventory.add(Items.ENCHANTED_RAT_MEAT)
+}
---
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/quests/impl/DruidicRitual.kt
@@ -0,0 +1,75 @@
+package gg.rsmod.plugins.content.quests.impl
+
+import gg.rsmod.game.model.entity.Player
+import gg.rsmod.game.model.quest.Quest
+import gg.rsmod.game.model.quest.QuestStage
+import gg.rsmod.plugins.api.Skills
+import gg.rsmod.plugins.api.cfg.Items
+import gg.rsmod.plugins.api.ext.getVarp
+import gg.rsmod.plugins.api.ext.setVarp
+import gg.rsmod.plugins.content.quests.QUEST_POINT_VARP
+import gg.rsmod.plugins.content.quests.advanceToNextStage
+import gg.rsmod.plugins.content.quests.buildQuestFinish
+
+object DruidicRitual : Quest( //sets up the quest info.
+    name = "Druidic Ritual",
+    startPoint = "Speak to Kaqemeex in the stone circle north of Taverley",
+    requirements = emptyList(),
+    requiredItems = "Raw bear meat, raw rat meat, raw beef, raw chicken.",
+    combat = "None.",
+    rewards = "4 Quest Points, introduction to the Herblore skill and 250 Herblore XP.",
+    pointReward = 4,
+    varbit = 80,
+    spriteId = 4432,
+    slot = 33,
+    stages = listOf(
+        QuestStage( //quest journal stage 1
+            objectives = listOf(
+                "I told ${red("Kaqemeex")} I would help them prepare their ceremony.",
+                "I should speak to ${red("Sanfew")} in the village to the south."
+            ),
+            value = 1
+        ),
+        QuestStage( //quest journal stage 2
+            objectives = listOf(
+                "I told ${red("Kaqemeex")} I would help them prepare their ceremony.",
+                "${red("Sanfew")} told me for the ritual they would need me to place",
+                "raw bear meat, raw chicken, raw rat meat, and raw beef in",
+                "the ${red("Cauldron of Thunder")} in the dungeon South of ${red("Taverley")}."
+            ),
+            value = 2
+        ),
+        QuestStage( //quest journal stage 3
+            objectives = listOf(
+                "I told ${red("Kaqemeex")} I would help them prepare their ceremony.",
+                "The ceremony required various meats being placed in the",
+                "${red("Cauldron of Thunder")}. I did this and gave them to ${red("Sanfew")}.",
+                "I should speak to ${red("Kaqemeex")} again and claim my reward.",
+            ),
+            value = 3
+        ),
+        QuestStage(
+            objectives = listOf( //finished quest text
+                striked(text ="I told ${red("Kaqemeex")} I would help them prepare their ceremony."),
+                striked("The ceremony required various meats being placed in the"),
+                striked("${red("Cauldron of Thunder")}. I did this and gave them to ${red("Sanfew")}."),
+                striked("${red("Kaqemeex")} then taught me the basics of the skill Herblore."),
+                questCompleteText
+            ),
+            value = 4
+        ),
+    ),
+) {
+
+    init { //inits the quest in the server
+        addQuest(this)
+    }
+
+    override fun finishQuest(player: Player) { //what happens when you finish the quest
+        player.advanceToNextStage(this)
+        player.setVarp(QUEST_POINT_VARP, player.getVarp(QUEST_POINT_VARP).plus(pointReward)) //adds quest points
+        player.addXp(Skills.HERBLORE, 250.0) //adds xp for herblore
+        player.buildQuestFinish(this, item = Items.CLEAN_GUAM, rewards = arrayOf("4 Quest Points", "250 Herblore XP", "Access to Herblore skill")) //shows the finish quest dialogue.
+    }
+
+}
