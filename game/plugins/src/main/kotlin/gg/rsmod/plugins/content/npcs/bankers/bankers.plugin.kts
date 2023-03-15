0644823b03d11f2298cc6a56f0d6d302a0690497
SkalFate <20464880+skalfate@users.noreply.github.com>
2023-03-15 08:40:31 -0600

    Added Functionality for Varrock East Female Bankers, Falador Bankers, and Grand Exchange Bankers

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/npcs/bankers/bankers.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/npcs/bankers/bankers.plugin.kts
@@ -2,7 +2,17 @@ package gg.rsmod.plugins.content.npcs.bankers
 
 import gg.rsmod.plugins.content.inter.bank.openBank
 
-val bankers = arrayOf(Npcs.BANKER, Npcs.BANKER_45, Npcs.BANKER_494, Npcs.BANKER_495, Npcs.BANKER_4907)
+val bankers = arrayOf(
+        Npcs.BANKER,
+        Npcs.BANKER_45, // Various Female Bankers
+        Npcs.BANKER_494, // Various Male Bankers
+        Npcs.BANKER_495, // Various Female Bankers
+        Npcs.BANKER_2759, // Varrock East Female Bankers
+        Npcs.BANKER_3293, // GrandExchange Female Bankers
+        Npcs.BANKER_3416, // GrandExchange Male Bankers
+        Npcs.BANKER_4907, // Lumbridge Castle Banker
+        Npcs.BANKER_6200, // Falador Bankers All Share the Same ID
+)
 
 bankers.forEach {
     on_npc_option(it, option = "Bank", lineOfSightDistance = 2) {
