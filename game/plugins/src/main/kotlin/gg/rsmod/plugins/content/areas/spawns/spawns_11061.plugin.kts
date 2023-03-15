e7213b20beecb6ed73029189c3a35a0575ee1a8f
Alycia <charliesalter@hotmail.com>
2023-03-14 22:37:33 -0700

    feat: spawns for 11061, 11317, 11318, 11572, 11573, 11574

---
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/spawns/spawns_11061.plugin.kts
@@ -0,0 +1,30 @@
+package gg.rsmod.plugins.content.areas.spawns
+
+/**
+ * @author Alycia <https://github.com/alycii>
+ */
+spawn_npc(npc = Npcs.MAN, x = 2804, z = 3427, walkRadius = 5, direction = Direction.NORTH_EAST)
+spawn_npc(npc = Npcs.GIANT_BAT, x = 2759, z = 3402, walkRadius = 5, direction = Direction.NORTH_EAST)
+spawn_npc(npc = Npcs.GIANT_BAT, x = 2756, z = 3400, walkRadius = 5, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.RENEGADE_KNIGHT, x = 2769, z = 3403, walkRadius = 5, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.RENEGADE_KNIGHT, x = 2766, z = 3402, walkRadius = 5, direction = Direction.WEST)
+spawn_npc(npc = Npcs.RENEGADE_KNIGHT, x = 2770, z = 3402, walkRadius = 5, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.RENEGADE_KNIGHT, x = 2772, z = 3404, walkRadius = 5, direction = Direction.NORTH)
+spawn_npc(npc = Npcs.RENEGADE_KNIGHT, x = 2767, z = 3405, height = 1, walkRadius = 5, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.RENEGADE_KNIGHT, x = 2771, z = 3403, height = 1, walkRadius = 5, direction = Direction.SOUTH)
+spawn_npc(npc = Npcs.RENEGADE_KNIGHT, x = 2767, z = 3401, height = 1, walkRadius = 5, direction = Direction.SOUTH_EAST)
+spawn_npc(npc = Npcs.SIR_MORDRED, x = 2769, z = 3402, height = 2, walkRadius = 5, direction = Direction.SOUTH_WEST)
+spawn_npc(npc = Npcs.ERNEST, x = 2770, z = 3403, height = 2, walkRadius = 5, direction = Direction.WEST)
+spawn_npc(npc = Npcs.BAT, x = 2760, z = 3402, walkRadius = 5, direction = Direction.EAST)
+spawn_npc(npc = Npcs.BAT, x = 2759, z = 3400, walkRadius = 5, direction = Direction.NORTH)
+spawn_npc(npc = Npcs.BAT, x = 2756, z = 3403, walkRadius = 5, direction = Direction.NORTH)
+spawn_npc(npc = Npcs.BAT, x = 2759, z = 3400, walkRadius = 5, direction = Direction.WEST)
+spawn_npc(npc = Npcs.BAT, x = 2755, z = 3402, walkRadius = 5, direction = Direction.NORTH)
+spawn_npc(npc = Npcs.BANKER, x = 2807, z = 3443, direction = Direction.SOUTH)
+spawn_npc(npc = Npcs.BANKER, x = 2810, z = 3443, direction = Direction.SOUTH)
+spawn_npc(npc = Npcs.BANKER_495, x = 2809, z = 3443, direction = Direction.SOUTH)
+spawn_npc(npc = Npcs.BANKER_495, x = 2811, z = 3443, direction = Direction.SOUTH)
+spawn_npc(npc = Npcs.CANDLEMAKER, x = 2799, z = 3438, walkRadius = 2, direction = Direction.NORTH)
+spawn_npc(npc = Npcs.ARHEIN, x = 2803, z = 3430, walkRadius = 2, direction = Direction.SOUTH)
+spawn_npc(npc = Npcs.TRADER_CREWMEMBER_4653, x = 2794, z = 3414, walkRadius = 2, direction = Direction.SOUTH)
+spawn_npc(npc = Npcs.TRADER_CREWMEMBER_4656, x = 2795, z = 3414, walkRadius = 2, direction = Direction.SOUTH)
