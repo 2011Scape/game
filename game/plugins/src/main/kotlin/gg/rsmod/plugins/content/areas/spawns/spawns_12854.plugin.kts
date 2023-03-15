9868364bf682c7864f4c3b2ae30194473a45e1ea
Alycia <charliesalter@hotmail.com>
2023-03-14 23:05:02 -0700

    feat: spawns for 12854

---
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/spawns/spawns_12854.plugin.kts
@@ -0,0 +1,31 @@
+package gg.rsmod.plugins.content.areas.spawns
+
+/**
+ * @author Alycia <https://github.com/alycii>
+ */
+
+spawn_item(item = Items.WOODEN_SHIELD, amount = 1, x = 3217, z = 3514, respawnCycles = 45)
+spawn_item(item = Items.BUCKET, amount = 1, x = 3221, z = 3497, height = 1, respawnCycles = 100)
+spawn_item(item = Items.BUCKET, amount = 1, x = 3222, z = 3491, height = 1, respawnCycles = 100)
+spawn_item(item = Items.PIE_DISH, amount = 1, x = 3222, z = 3494, respawnCycles = 120)
+
+spawn_npc(npc = Npcs.FATHER_LAWRENCE, x = 3252, z = 3485, walkRadius = 5, direction = Direction.SOUTH)
+spawn_npc(npc = Npcs.KING_ROALD, x = 3220, z = 3471, walkRadius = 3, direction = Direction.SOUTH)
+spawn_npc(npc = Npcs.SIR_PRYSIN, x = 3204, z = 3470, walkRadius = 5, direction = Direction.WEST)
+spawn_npc(npc = Npcs.CAPTAIN_ROVIN, x = 3204, z = 3495, height = 2, walkRadius = 5, direction = Direction.SOUTH)
+spawn_npc(npc = Npcs.MONK_OF_ZAMORAK_1045, x = 3213, z = 3476, walkRadius = 5, direction = Direction.WEST)
+spawn_npc(npc = Npcs.GRIZZLY_BEAR_1195, x = 3228, z = 3501, walkRadius = 5, direction = Direction.SOUTH_WEST)
+spawn_npc(npc = Npcs.TREZNOR, x = 3229, z = 3456, walkRadius = 5, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.RELDO, x = 3208, z = 3496, walkRadius = 5, direction = Direction.WEST)
+spawn_npc(npc = Npcs.TOOL_LEPRECHAUN, x = 3232, z = 3459, walkRadius = 5, direction = Direction.WEST)
+spawn_npc(npc = Npcs.ESTATE_AGENT, x = 3240, z = 3474, walkRadius = 3, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.AEONISIG_RAISPHER, x = 3222, z = 3475, direction = Direction.SOUTH_WEST)
+spawn_npc(npc = Npcs.SUROK_MAGIS, x = 3208, z = 3496, walkRadius = 5, direction = Direction.SOUTH)
+spawn_npc(npc = Npcs.GUARD_5920, x = 3220, z = 3461, walkRadius = 5, direction = Direction.WEST)
+spawn_npc(npc = Npcs.GUARD_5920, x = 3221, z = 3464, walkRadius = 5, direction = Direction.EAST)
+spawn_npc(npc = Npcs.GUARD_5920, x = 3213, z = 3466, walkRadius = 5, direction = Direction.NORTH)
+spawn_npc(npc = Npcs.GUARD_5920, x = 3214, z = 3461, walkRadius = 5, direction = Direction.SOUTH_WEST)
+spawn_npc(npc = Npcs.GUARD_5920, x = 3215, z = 3464, walkRadius = 5, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.GUARD_5920, x = 3208, z = 3467, walkRadius = 5, direction = Direction.SOUTH)
+spawn_npc(npc = Npcs.GUARD_5920, x = 3209, z = 3460, walkRadius = 5, direction = Direction.EAST)
+spawn_npc(npc = Npcs.GUARD_5920, x = 3205, z = 3463, walkRadius = 5, direction = Direction.EAST)
