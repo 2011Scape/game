83967d576c64b61eb0d968660cf9faaed96c2ebc
Alycia <charliesalter@hotmail.com>
2023-03-19 21:46:21 -0700

    Revert "feat: Pestle and Mortar loop action"
    This reverts commit a7ebf8c15e5c5173367847e517109d48216290ce.

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/items/pestle_and_mortar/PestleAndMortarData.kt
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/items/pestle_and_mortar/PestleAndMortarData.kt
@@ -14,10 +14,5 @@ enum class PestleAndMortarData(val source: Int, val result: Int) {
     Ashes(Items.ASHES, Items.GROUND_ASHES),
     PoisonKarambwan(Items.POISON_KARAMBWAN, Items.KARAMBWAN_PASTE),
     FishingBait(Items.FISHING_BAIT, Items.GROUND_FISHING_BAIT),
-    SeaWeed(Items.SEAWEED, Items.GROUND_SEAWEED);
-
-    companion object {
-        val values = enumValues<PestleAndMortarData>()
-        val definitions = values.associateBy { it.source }
-    }
+    SeaWeed(Items.SEAWEED, Items.GROUND_SEAWEED),
 }
