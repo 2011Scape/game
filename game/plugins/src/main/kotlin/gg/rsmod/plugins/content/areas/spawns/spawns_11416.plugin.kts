ac2ccd8b94b45592daf882fffb1f3d6c90062a79
Alycia <charliesalter@hotmail.com>
2023-03-23 23:49:04 -0700

    feat: taverly dungeon spawns

---
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/spawns/spawns_11416.plugin.kts
@@ -0,0 +1,31 @@
+package gg.rsmod.plugins.content.areas.spawns
+
+/**
+ * @author Alycia <https://github.com/alycii>
+ */
+
+spawn_item(item = Items.BONES, amount = 1, x = 2831, z = 9766, respawnCycles = 120)
+spawn_item(item = Items.BONES, amount = 1, x = 2829, z = 9764, respawnCycles = 120)
+
+spawn_npc(npc = Npcs.ZOMBIE_74, x = 2843, z = 9767, walkRadius = 5, direction = Direction.WEST)
+spawn_npc(npc = Npcs.ZOMBIE_74, x = 2845, z = 9762, walkRadius = 5, direction = Direction.SOUTH_EAST)
+spawn_npc(npc = Npcs.ZOMBIE_74, x = 2842, z = 9759, walkRadius = 5, direction = Direction.SOUTH)
+spawn_npc(npc = Npcs.ZOMBIE_75, x = 2839, z = 9774, walkRadius = 5, direction = Direction.EAST)
+spawn_npc(npc = Npcs.ZOMBIE_76, x = 2843, z = 9774, walkRadius = 5, direction = Direction.SOUTH)
+spawn_npc(npc = Npcs.ZOMBIE_76, x = 2842, z = 9769, walkRadius = 5, direction = Direction.EAST)
+spawn_npc(npc = Npcs.ZOMBIE_76, x = 2842, z = 9764, walkRadius = 5, direction = Direction.SOUTH_EAST)
+spawn_npc(npc = Npcs.ZOMBIE_76, x = 2845, z = 9754, walkRadius = 5, direction = Direction.EAST)
+spawn_npc(npc = Npcs.ZOMBIE_76, x = 2849, z = 9751, walkRadius = 5, direction = Direction.NORTH)
+spawn_npc(npc = Npcs.ZOMBIE_76, x = 2850, z = 9747, walkRadius = 5, direction = Direction.WEST)
+spawn_npc(npc = Npcs.ZOMBIE_76, x = 2844, z = 9758, walkRadius = 5, direction = Direction.SOUTH_EAST)
+spawn_npc(npc = Npcs.BLACK_DEMON, x = 2861, z = 9766, walkRadius = 5, direction = Direction.SOUTH_EAST)
+spawn_npc(npc = Npcs.BLACK_DEMON, x = 2875, z = 9776, walkRadius = 5, direction = Direction.WEST)
+spawn_npc(npc = Npcs.BLACK_DEMON, x = 2869, z = 9782, walkRadius = 5, direction = Direction.WEST)
+spawn_npc(npc = Npcs.BLACK_DEMON, x = 2856, z = 9779, walkRadius = 5, direction = Direction.WEST)
+spawn_npc(npc = Npcs.BLACK_DEMON, x = 2867, z = 9773, walkRadius = 5, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.BLACK_DEMON, x = 2863, z = 9780, walkRadius = 5, direction = Direction.EAST)
+spawn_npc(npc = Npcs.BLACK_DEMON, x = 2858, z = 9775, walkRadius = 5, direction = Direction.EAST)
+spawn_npc(npc = Npcs.GREATER_DEMON_4698, x = 2861, z = 9752, walkRadius = 5, direction = Direction.EAST)
+spawn_npc(npc = Npcs.GREATER_DEMON_4700, x = 2862, z = 9750, walkRadius = 5, direction = Direction.SOUTH_EAST)
+
+
---
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/spawns/spawns_11928.plugin.kts
@@ -0,0 +1,16 @@
+package gg.rsmod.plugins.content.areas.spawns
+
+/**
+ * @author Alycia <https://github.com/alycii>
+ */
+
+spawn_item(item = Items.BONES, amount = 1, x = 2966, z = 9772, respawnCycles = 60)
+spawn_item(item = Items.BONES, amount = 1, x = 2968, z = 9771, respawnCycles = 60)
+
+spawn_npc(npc = Npcs.MAGIC_AXE, x = 2955, z = 9795, walkRadius = 5, direction = Direction.SOUTH)
+spawn_npc(npc = Npcs.MAGIC_AXE, x = 2956, z = 9791, walkRadius = 5, direction = Direction.SOUTH)
+spawn_npc(npc = Npcs.MAGIC_AXE, x = 2962, z = 9792, walkRadius = 5, direction = Direction.SOUTH)
+spawn_npc(npc = Npcs.MAGIC_AXE, x = 2966, z = 9788, walkRadius = 5, direction = Direction.SOUTH)
+spawn_npc(npc = Npcs.MAGIC_AXE, x = 2959, z = 9783, walkRadius = 5, direction = Direction.SOUTH)
+spawn_npc(npc = Npcs.MAGIC_AXE, x = 2964, z = 9775, walkRadius = 5, direction = Direction.SOUTH)
+spawn_npc(npc = Npcs.MAGIC_AXE, x = 2954, z = 9776, walkRadius = 5, direction = Direction.SOUTH)
