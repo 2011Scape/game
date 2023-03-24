823aa62356f1aa562840bd3174765a43f3da3f8b
DesecratedTree <52549792+DesecratedTree@users.noreply.github.com>
2023-03-24 07:24:59 -0500

    lumbridge content

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/lumbridge/melee_instructor.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/lumbridge/melee_instructor.plugin.kts
@@ -1,91 +1,39 @@
 package gg.rsmod.plugins.content.areas.lumbridge
 
+import gg.rsmod.plugins.content.skills.Skillcapes
+
 on_npc_option(npc = Npcs.MELEE_INSTRUCTOR, option = "talk-to") {
     player.queue {
         this.chatNpc("Greetings adventurer, I am the Melee combat tutor. Is", "there anything I can do for you?")
         mainChat(this)
     }
 }
-suspend fun buySkillcape(it: QueueTask) {
-    it.chatPlayer("May I buy a Skillcape of Defence, please?")
-    it.chatNpc("You wish to join the elite defenders of this world? ", "I'm afraid such things do not come cheaply - " ,"in fact they cost 99000 coins, to be precise!")
-    when (it.options("99000 coins? That's much too expensive.", "I think I have the money right here, actually.")){
-        1 -> {
-            it.chatPlayer("99000 coins? That's much too expensive.")
-            it.chatNpc("Not at all; there are many other adventurers who ", "would love the opportunity to purchase such a ", "prestigious item! You can find me here if you change ", "your mind.")
-        }
-        2 -> {
-            it.chatPlayer("I think I have the money right here, actually.")
-            if(it.player.inventory.freeSlotCount < 2) {
-                it.chatNpc("You don't have enough free space in your inventory ", "for me to sell you a Skillcape of defence.")
-                it.chatNpc("Come back to me when you've cleared up some space.")
-            }
-            if(it.player.hasItem(Items.DEFENCE_HOOD) || it.player.hasItem(Items.DEFENCE_CAPE)) {
-                it.chatNpc("You already have a Skillcape of defence.")
-            }
-            else{
-                if (it.player.inventory.getItemCount(Items.COINS_995) >= 99000){
-                    it.player.inventory.remove(item = Item(Items.COINS_995, amount = 99000), assureFullRemoval = true)
-                    it.doubleItemMessageBox("Harlan gives you a Defence skillcape and hood.", item1 = Items.DEFENCE_CAPE, item2 = Items.DEFENCE_HOOD)
-                    //TODO check for previous skillcape earned, give trimmed cape if earned, regular cape if not.
-                    it.player.inventory.add(Items.DEFENCE_CAPE)
-                    it.player.inventory.add(Items.DEFENCE_HOOD)
-                    it.chatNpc("Excellent! Wear that cape with pride my friend.")
-                }
-                else{
-                    it.chatPlayer("But, unfortunately, I was mistaken.")
-                    it.chatNpc("Well, come back and see me when you do.")
-                }
-            }
-        }
-    }
-}
 
 suspend fun mainChat(it: QueueTask) {
-    when (it.options("Tell me about melee combat.", "Tell me about different weapon types I can use.", "Tell me about skillcapes.", "I'd like a training sword and shield.", "Goodbye.")) {
-        1 -> {
-            it.chatPlayer("Tell me about melee combat.")
-            it.chatNpc("Well adventurer, the first thing you will need is a", "sword and shield appropriate for your level.")
-            it.player.runClientScript(115, 5)
-            it.chatNpc("Make sure to equip your sword and shield. Click on", "them in your inventory, they will disappear from your", "inventory and move to your worn items. You can see", "your worn items in the worn items tab here.")
-            it.player.runClientScript(115, 0)
-            it.chatNpc("When you are wielding your sword you will then be", "able to see the correct options in the combat interface.")
-            it.chatNpc("There are four different melee styles. Accurate,", "aggressive, defensive, and controlled. Not all weapons will", "have all four styles though.")
-            it.chatPlayer("Interesting, what does each style do?")
-            it.chatNpc("Well I am glad you asked. The accurate style will give", "you experience points in your Attack skill, you will also", "find you will deal damage more frequently as a result", "of being well, more accurate.")
-            it.chatNpc("Next we have the aggressive style. This style will give", "you experience in your Strength skill. When", "using this style you will notice that your attacks will hit", "a little harder.")
-            it.chatNpc("Now for the defensive style, this style will give you", "experience points in your Defensive skill. When using", "this style you will notice that you get hit less often.")
-            it.chatNpc("Finally, we have the controlled style. This style will give", "you the same amount of experience as the other styles", "would but shared accross all three of the combat skills.")
-            it.chatNpc("If you were using the training sword for example, there", "are four different attack types. Stab, lunge, slash and", "block.")
-            it.chatNpc("Each type uses one of the attack styles. Stab uses", "accurate , lunge and slash use aggressive and block uses", "defensive.")
-            it.chatNpc("To find out which style an attack type uses, hover your", "mouse cursor over the style button.")
-            it.chatNpc("Is there anything else you would like to know?")
-            it.terminateAction
-            it.player.queue { mainChat(this) }
+    when (it.options(
+        "Who are you?",
+        "What is that cape you're wearing?",
+        title = "What would you like to talk about?"
+    )) {
+        FIRST_OPTION -> {
+            it.chatPlayer("Who are you?")
+            it.chatNpc("My name is Harlan, a master of defence!")
         }
-        2 -> {
-            it.chatPlayer("Tell me about different weapon types I can use.")
-            it.chatNpc("Well let me see now...There are stabbing type weapons ", "such as daggers, then you have swords which are ", "slashing, maces that have great crushing abilities, ", "battle axes which are powerful.")
-            it.chatNpc("It depends a lot on how you want to fight. Experiment ", "and find out what is best for you. Never be scared to ", "try out a new weapon; you never know, you might like it!")
-            it.chatNpc("While I tried all of them for a while, ", "I settled on this rather good sword.")
-            it.chatNpc("You might also find that different weapon types ", "are more accurate against different monsters.")
-            it.chatNpc("Is there anything else you would like to know?")
-            it.terminateAction
-            it.player.queue { mainChat(this) }
-        }
-        3 -> {
-            it.chatPlayer("Tell me about skillcapes.")
+        SECOND_OPTION -> {
+            it.chatPlayer("What is that cape you're wearing?")
+            it.chatNpc(
+                "Ah, this is a skillcape of Defence. I have mastered the",
+                "art of defence and wear it proudly to show others.")
+            it.chatPlayer("Hmm, interesting.")
             if (it.player.getSkills().getCurrentLevel(Skills.DEFENCE) >= 99){
                 it.chatNpc("Ah, I can see you're already a master in the fine ", "art of Defence. Perhaps you have come to me to ", "purchase a Skillcape of Defence, and thus join the ", "elite few who have mastered this exacting skill?")
-                when (it.options("May I buy a Skillcape of Defence, please?", "Can I ask about something else?")){
-                    1 -> {
+                when (it.options("Yes, please sell me a Skillcape of Defence", "No, thanks.")){
+                    FIRST_OPTION -> {
                         it.terminateAction
                         it.player.queue { buySkillcape(this) }
                     }
-                    2 -> {
-                        it.chatPlayer("Can I ask about something else?")
-                        it.terminateAction
-                        it.player.queue { mainChat(this) }
+                    SECOND_OPTION -> {
+                        it.chatPlayer("No, thanks.")
                     }
                 }
             }else{
@@ -95,25 +43,48 @@ suspend fun mainChat(it: QueueTask) {
                 it.player.queue { mainChat(this) }
             }
         }
-        4 -> {
-            it.chatPlayer("I'd like a training sword and shield.")
-            if(it.player.hasItem(Items.TRAINING_SWORD) && it.player.hasItem(Items.TRAINING_SHIELD)) {
-                it.chatNpc("You already have a training sword and shield. Save", "some for the other adventurers.")
-            } else {
-                if(it.player.inventory.freeSlotCount >= 2) {
-                    it.doubleItemMessageBox("Harlan gives you a Training sword and shield.", item1 = Items.TRAINING_SWORD, item2 = Items.TRAINING_SHIELD)
-                    it.player.inventory.add(Items.TRAINING_SWORD)
-                    it.player.inventory.add(Items.TRAINING_SHIELD)
-                    it.chatNpc("There you go, use it well.")
-                } else{
-                    it.chatNpc("You don't have enough space for me to give you a", "training sword, nor a shield.")
-                }
+    }
+}
+
+suspend fun buySkillcape(it: QueueTask) {
+    val npc = it.player.getInteractingNpc()
+
+    // get the skillcape data
+    val data = Skillcapes.values().firstOrNull { it.npcId == npc.id } ?: return
+
+    it.chatPlayer("May I buy a Skillcape of Defence, please?")
+    it.chatNpc(
+        "You wish to join the elite defenders of this world?",
+        "I'm afraid such things do not come cheaply -",
+        "in fact they cost 99000 coins, to be precise!"
+    )
+    when (it.options("99000 coins? That's much too expensive.", "I think I have the money right here, actually.")) {
+        FIRST_OPTION -> {
+            it.chatPlayer("99000 coins? That's much too expensive.")
+            it.chatNpc(
+                "Not at all; there are many other adventurers who",
+                "would love the opportunity to purchase such a",
+                "prestigious item! You can find me here if you change",
+                "your mind."
+            )
+        }
+        SECOND_OPTION -> {
+            it.chatPlayer("I think I have the money right here, actually.")
+            if (it.player.inventory.freeSlotCount < 2) {
+                it.chatNpc(
+                    "You don't have enough free space in your inventory ",
+                    "for me to sell you a Skillcape of Defence."
+                )
+                it.chatNpc("Come back to me when you've cleared up some space.")
+                return
             }
-
+            if (it.player.inventory.getItemCount(Items.COINS_995) >= 99000) {
+                Skills.purchaseSkillcape(it.player, data = Skillcapes.DEFENCE)
+                it.chatNpc("Excellent! Wear that cape with pride my friend.")
+            } else{
+                it.chatPlayer("But, unfortunately, I was mistaken.")
+                it.chatNpc("Well, come back and see me when you do.")
+            }
         }
-        5 -> {
-            it.chatPlayer("Goodbye.")
-        }
-
     }
 }
--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/lumbridge/ranged_instructor.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/lumbridge/ranged_instructor.plugin.kts
@@ -8,12 +8,11 @@ on_npc_option(npc = Npcs.RANGED_INSTRUCTOR, option = "talk-to") {
 }
 
 suspend fun mainChat(it: QueueTask) {
-    when (it.options("How can I train my Ranged?", "How do I create a bow and arrows?", "I'd like some arrows and a training bow.", "Goodbye.")) {
-        1 -> {
+    when (it.options("How can I train my Ranged?", "How do I create a bow and arrows?", "Goodbye.")) {
+        FIRST_OPTION -> {
             it.chatPlayer("How can I train my Ranged?")
             it.chatNpc("To start with you'll need a bow and arrows, you were ", "given a Shortbow and some arrows when you arrived here.") //from Tutorial island.")
-            it.chatNpc("Alternatively, you can claim a training bow and some ", "arrows from me.")
-            it.chatNpc("The Magic Combat tutor and I both give out ", "items every 30 minutes, however you must choose ", "whether you want runes or ranged equipment.")
+            it.chatNpc("Alternatively, you can buy a training bow and some ", "arrows from Beefy Bill, just north of the mill.")
             it.player.runClientScript(115, 2)
             it.chatNpc("Not all bows can use every type of arrow, most bows ", "have a limit. You can find out your bows limit by ", "checking the Ranged skill guide.")
             it.chatNpc("If you do decide to use the Training bow, you will only ", "be able to use the Training arrows with it. Remember", "to pick up your arrows, re-use them and come back", "when you need more.")
@@ -26,7 +25,7 @@ suspend fun mainChat(it: QueueTask) {
             it.terminateAction
             it.player.queue { mainChat(this) }
         }
-        2 -> {
+        SECOND_OPTION -> {
             it.chatPlayer("How do I create a bow and arrows?")
             it.chatNpc("Ahh the art of fletching. Fletching is used to create ", "your own bow and arrows.")
             it.chatNpc("It's quite simple really. You'll need an axe to cut some ", "logs from trees and a knife. Knives can be found in ", "and around the Lumbridge castle and in the Varrock", "General store upstairs.")
@@ -41,34 +40,8 @@ suspend fun mainChat(it: QueueTask) {
             it.terminateAction
             it.player.queue { mainChat(this) }
         }
-        3 -> {
-            it.chatPlayer("I'd like some arrows and a training bow.")
-            it.terminateAction
-            it.player.queue { claimBow(this) }
-        }
-        4 -> {
+        THIRD_OPTION -> {
             it.chatPlayer("Goodbye.")
-        }
-    }
-}
-
-suspend fun claimBow(it: QueueTask) {
-    val tutorConsumablesTimer = TimerKey(persistenceKey = "tutor_consumables_timer", tickOffline = true)
-    val tutorConsumablesDelay = 3000
-    if (it.player.timers.has(tutorConsumablesTimer)) {
-        it.chatNpc("I work with the Magic tutor to give out consumable ", "items that you may need for combat such as arrows ", "and runes. However we have had some cheeky people ", "try to take both!")
-        it.chatNpc("So, every half an hour, you may come back and claim ", "either arrows OR runes, but not both. Come back in a ", "while for arrows, or simply make your own.")
-    } else {
-        if(it.player.hasItem(Items.TRAINING_BOW) && it.player.hasItem(Items.TRAINING_ARROWS)) {
-            it.chatNpc("You already have a training bow and some training arrows.")
-        }else if(it.player.inventory.freeSlotCount >= 2) {
-            it.doubleItemMessageBox("The instructor gives you a Training bow and arrows.", item1 = Items.TRAINING_BOW, item2 = Items.TRAINING_ARROWS)
-            it.player.inventory.add(Items.TRAINING_BOW)
-            it.player.inventory.add(Items.TRAINING_ARROWS, amount = 25)
-            it.player.timers[tutorConsumablesTimer] = tutorConsumablesDelay
-            it.chatNpc("There you go, use them well.")
-        }else{
-            it.chatNpc("You don't have enough space for me to give you a", "training bow, nor arrows.")
         }
     }
 }
