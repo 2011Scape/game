ef6a11445fd2f72d15001496f53e751636151c48
Alycia <charliesalter@hotmail.com>
2023-04-02 23:22:12 -0700

    feat: loyalty point store

---
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/loyalty/loyalty_points.plugin.kts
@@ -0,0 +1,29 @@
+package gg.rsmod.plugins.content.loyalty
+
+import gg.rsmod.game.model.attr.LOYALTY_POINTS
+import gg.rsmod.plugins.content.mechanics.shops.LoyaltyPointsCurrency
+
+/**
+ * @author Alycia <https://github.com/alycii>
+ */
+
+create_shop(
+    "Loyalty Rewards",
+    currency = LoyaltyPointsCurrency(),
+    purchasePolicy = PurchasePolicy.BUY_NONE,
+    containsSamples = false
+) {
+    items[0] = ShopItem(Items.CHRISTMAS_CRACKER, amount = 1, sellPrice = 1000)
+    items[1] = ShopItem(Items.SANTA_HAT, amount = 1, sellPrice = 7_000)
+    items[2] = ShopItem(Items.RED_HWEEN_MASK, amount = 1, sellPrice = 4_000)
+    items[3] = ShopItem(Items.GREEN_HWEEN_MASK, amount = 1, sellPrice = 4_000)
+    items[4] = ShopItem(Items.BLUE_HWEEN_MASK, amount = 1, sellPrice = 4_000)
+    items[5] = ShopItem(Items.EASTER_EGG, amount = 1, sellPrice = 2_000)
+    items[6] = ShopItem(Items.PUMPKIN, amount = 1, sellPrice = 2_000)
+    items[7] = ShopItem(Items.DISK_OF_RETURNING, amount = 1, sellPrice = 100_000)
+}
+
+on_npc_option(npc = Npcs.XUAN, option = "open-shop") {
+    player.openShop("Loyalty Rewards")
+    player.setComponentText(interfaceId = 620, component = 24, text = "You currently have ${player.attr[LOYALTY_POINTS]!!.format()} loyalty points.")
+}
