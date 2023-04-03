d68aa88d7e7c34d405bb0639b3c6e2bd778810bc
Alycia <charliesalter@hotmail.com>
2023-04-02 23:22:21 -0700

    chore: changes to daily timer

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/daily/daily.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/daily/daily.plugin.kts
@@ -1,10 +1,13 @@
 package gg.rsmod.plugins.content.daily
 
+import gg.rsmod.game.model.attr.LOYALTY_POINTS
 import gg.rsmod.game.model.timer.DAILY_TIMER
 
 /**
  * @author Alycia <https://github.com/alycii>
  */
+
+val time = 144000
 
 on_timer(DAILY_TIMER) {
 
@@ -12,5 +15,9 @@ on_timer(DAILY_TIMER) {
     player.addLoyalty(amount = 500)
 
     // Reset the daily timer
-    player.timers[DAILY_TIMER] = 144000
+    player.timers[DAILY_TIMER] = time
+
+    // Send the message
+    player.message("Your daily timer has been reset, and you have been awarded 500 loyalty points.")
+    player.message("You now have a total of ${player.attr[LOYALTY_POINTS]!!.format()} loyalty points.")
 }
