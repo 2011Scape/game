8b9d4fbcc68b8cd169145dbba4f8b2791a1963a8
Alycia <charliesalter@hotmail.com>
2023-04-03 16:48:54 -0700

    feat: spawns for karamja, brimhaven, tzhaar city

---
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/spawns/spawns_10801.plugin.kts
@@ -0,0 +1,23 @@
+package gg.rsmod.plugins.content.areas.spawns
+
+/**
+ * @author Alycia <https://github.com/alycii>
+ */
+
+
+spawn_npc(npc = Npcs.SNAKE, x = 2741, z = 3173, walkRadius = 5, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.SNAKE, x = 2746, z = 3167, walkRadius = 5, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.SNAKE, x = 2746, z = 3147, walkRadius = 5, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.SNAKE, x = 2733, z = 3162, walkRadius = 5, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.SNAKE, x = 2730, z = 3146, walkRadius = 5, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.MONKEY, x = 2749, z = 3173, walkRadius = 5, direction = Direction.WEST)
+spawn_npc(npc = Npcs.TRIBESMAN, x = 2730, z = 3161, walkRadius = 5, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.TRIBESMAN, x = 2729, z = 3159, walkRadius = 5, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.TRIBESMAN, x = 2727, z = 3163, walkRadius = 5, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.TRIBESMAN, x = 2725, z = 3157, walkRadius = 5, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.TRIBESMAN, x = 2726, z = 3161, walkRadius = 5, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.TRIBESMAN, x = 2725, z = 3161, walkRadius = 5, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.TRIBESMAN, x = 2723, z = 3155, walkRadius = 5, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.TRIBESMAN, x = 2723, z = 3156, walkRadius = 5, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.TRIBESMAN, x = 2725, z = 3162, walkRadius = 5, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.SANIBOCH, x = 2744, z = 3152, walkRadius = 5, direction = Direction.SOUTH_EAST)
---
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/spawns/spawns_10802.plugin.kts
@@ -0,0 +1,26 @@
+package gg.rsmod.plugins.content.areas.spawns
+
+/**
+ * @author Alycia <https://github.com/alycii>
+ */
+
+
+spawn_npc(npc = Npcs.POISON_SCORPION, x = 2715, z = 3218, walkRadius = 5, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.POISON_SCORPION, x = 2719, z = 3223, walkRadius = 5, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.POISON_SCORPION, x = 2720, z = 3212, walkRadius = 5, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.POISON_SCORPION, x = 2722, z = 3220, walkRadius = 5, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.POISON_SCORPION, x = 2724, z = 3215, walkRadius = 5, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.POISON_SCORPION, x = 2729, z = 3224, walkRadius = 5, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.POISON_SCORPION, x = 2733, z = 3225, walkRadius = 5, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.POISON_SCORPION, x = 2726, z = 3217, walkRadius = 5, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.POISON_SCORPION, x = 2728, z = 3218, walkRadius = 5, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.POISON_SCORPION, x = 2735, z = 3219, walkRadius = 5, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.POISON_SCORPION, x = 2723, z = 3212, walkRadius = 5, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.POISON_SCORPION, x = 2721, z = 3216, walkRadius = 5, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.POISON_SCORPION, x = 2716, z = 3219, walkRadius = 5, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.POISON_SCORPION, x = 2725, z = 3227, walkRadius = 5, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.MOSS_GIANT, x = 2699, z = 3206, walkRadius = 5, direction = Direction.NORTH)
+spawn_npc(npc = Npcs.MOSS_GIANT_1587, x = 2692, z = 3202, walkRadius = 5, direction = Direction.EAST)
+spawn_npc(npc = Npcs.MOSS_GIANT_1587, x = 2695, z = 3216, walkRadius = 5, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.MOSS_GIANT_1588, x = 2691, z = 3215, walkRadius = 5, direction = Direction.WEST)
+spawn_npc(npc = Npcs.MOSS_GIANT_4688, x = 2700, z = 3212, walkRadius = 5, direction = Direction.SOUTH)
