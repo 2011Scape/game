afeb37c2f02bbef0980d099ed7d75a24434d5ab2
RSDEANEO <deane1991@live.co.uk>
2023-04-05 22:23:03 +0100

    feat: spawn for 16729 (glacor cave dungeon)

---
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/spawns/spawn_16729.plugin.kts
@@ -0,0 +1,16 @@
+package gg.rsmod.plugins.content.areas.spawns
+
+spawn_npc(npc = Npcs.MOVARIO, x = 4196, z = 5748, walkRadius = 5, direction = Direction.EAST)
+
+spawn_npc(npc = Npcs.GLACOR, x = 4176, z = 5709, direction = Direction.SOUTH)
+spawn_npc(npc = Npcs.GLACOR, x = 4173, z = 5721, direction = Direction.SOUTH)
+spawn_npc(npc = Npcs.GLACOR, x = 4182, z = 5722, direction = Direction.SOUTH)
+spawn_npc(npc = Npcs.GLACOR, x = 4193, z = 5719, direction = Direction.SOUTH)
+spawn_npc(npc = Npcs.GLACOR, x = 4205, z = 5718, direction = Direction.SOUTH)
+spawn_npc(npc = Npcs.GLACOR, x = 4211, z = 5713, direction = Direction.SOUTH)
+spawn_npc(npc = Npcs.GLACOR, x = 4202, z = 5708, direction = Direction.SOUTH)
+spawn_npc(npc = Npcs.GLACOR, x = 4194, z = 5706, direction = Direction.SOUTH)
+spawn_npc(npc = Npcs.GLACOR, x = 4187, z = 5705, direction = Direction.SOUTH)
+spawn_npc(npc = Npcs.GLACOR, x = 4185, z = 5713, direction = Direction.SOUTH)
+spawn_npc(npc = Npcs.GLACOR, x = 4168, z = 5727, direction = Direction.SOUTH)
+spawn_npc(npc = Npcs.GLACOR, x = 4179, z = 5703, direction = Direction.SOUTH)
