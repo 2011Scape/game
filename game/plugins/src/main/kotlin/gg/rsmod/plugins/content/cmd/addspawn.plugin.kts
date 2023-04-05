1d94d2bbb69da2f9b9ae7dcd92595cd61d76319f
Kevin <ksenez94@gmail.com>
2023-04-05 00:40:36 -0400

    fix: Some adjustments to the add_spawn command.

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/cmd/addspawn.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/cmd/addspawn.plugin.kts
@@ -6,16 +6,23 @@ import java.io.BufferedWriter
 import java.io.File
 import java.io.FileWriter
 import java.lang.reflect.Modifier
+import java.nio.file.Files
+import java.nio.file.Paths
 
 on_command("add_spawn", Privilege.ADMIN_POWER) {
     val args = player.getCommandArgs()
-    tryWithUsage(player, args, "Invalid format! Example of proper command <col=42C66C>::p 0</col>") { values ->
+    tryWithUsage(player, args, "Invalid format! Example of proper command <col=42C66C>::add_spawn 0</col>") { values ->
         val id = values[0].toInt()
         addSpawn(player, id)
         world.spawn(Npc(id = id, tile = player.tile, world = world))
     }
 }
 
+/**
+ * Grabs the [Npcs] file val name for the corresponding [id]
+ * @param id The npcs id.
+ * @return if [id] is found in [Npcs] as a value, return the value name. If not, null
+ */
 fun getName(id: Int): String? {
     val constVals = Npcs::class.java.declaredFields.filter {
         it.isAccessible = true
@@ -39,14 +46,24 @@ fun tryWithUsage(player: Player, args: Array<String>, failMessage: String, tryUn
     }
 }
 
+/**
+ * Adds the spawn to the proper region file corresponding to the players location.
+ */
 fun addSpawn(player: Player, id: Int) {
-    val string = "spawn_npc(npc = Npcs.${getName(id)}, x = ${player.tile.x}, z = ${player.tile.z}, walkRadius = 5, direction = Direction.${player.faceDirection})"
+    if (getName(id) == null)
+        player.message("ERROR: val not found corresponding with given id; reverting to using supplied id instead.")
+    val string = "spawn_npc(npc = ${if (getName(id) == null) id else "Npcs.${getName(id)}"}, x = ${player.tile.x}, z = ${player.tile.z}, walkRadius = 5, direction = Direction.${player.faceDirection})"
     try {
         val file = File("./game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/spawns/spawns_${player.tile.regionId}.plugin.kts")
         if (!file.exists())
             createSpawnFile(file)
         val writer = BufferedWriter(FileWriter(file, true))
-        writer.write("$string //Added by command by user: ${Misc.formatForDisplay(player.username)}")
+
+        val lines = Files.readAllLines(Paths.get(file.path))
+        if (lines.last().isNotEmpty() || lines.last().isNotBlank())
+            writer.newLine()
+
+        writer.write(string)
         writer.newLine()
         writer.close()
         player.message("Added Spawn for NPC=<col=FFFF00>$id</col> [Region=<col=FFFF00>${player.tile.regionId}</col> - Coords=<col=FFFF00>${player.tile.x}, ${player.tile.z}, ${player.tile.height}</col>]", type = ChatMessageType.CONSOLE)
