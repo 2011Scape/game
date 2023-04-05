56d9cb1d2c880a6a63142e672a63f91be56bec1e
Kevin <ksenez94@gmail.com>
2023-04-05 00:41:50 -0400

    feat: Add more spawns to asgarnia ice dungeon (Hobgoblins and Pirates)

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/spawns/spawns_11925.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/spawns/spawns_11925.plugin.kts
@@ -1,6 +1,22 @@
 package gg.rsmod.plugins.content.areas.spawns
 
+/**
+ * @author Kevin Senez <ksenez94@gmail.com>
+ */
 spawn_npc(npc = Npcs.MUGGER, x = 2994, z = 9549, walkRadius = 5, direction = Direction.SOUTH)
 spawn_npc(npc = Npcs.MUGGER, x = 2993, z = 9551, walkRadius = 5, direction = Direction.SOUTH)
 spawn_npc(npc = Npcs.MUGGER, x = 2995, z = 9555, walkRadius = 5, direction = Direction.SOUTH)
 spawn_npc(npc = Npcs.MUGGER, x = 2999, z = 9548, walkRadius = 5, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.PIRATE, x = 2993, z = 9568, walkRadius = 5, direction = Direction.SOUTH)
+spawn_npc(npc = Npcs.PIRATE, x = 2991, z = 9574, walkRadius = 5, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.PIRATE, x = 2989, z = 9578, walkRadius = 5, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.PIRATE, x = 2991, z = 9583, walkRadius = 5, direction = Direction.NORTH_EAST)
+spawn_npc(npc = Npcs.PIRATE, x = 2996, z = 9583, walkRadius = 5, direction = Direction.EAST)
+spawn_npc(npc = Npcs.PIRATE, x = 2999, z = 9580, walkRadius = 5, direction = Direction.SOUTH_EAST)
+spawn_npc(npc = Npcs.PIRATE, x = 2996, z = 9576, walkRadius = 5, direction = Direction.SOUTH_WEST)
+spawn_npc(npc = Npcs.PIRATE, x = 2999, z = 9573, walkRadius = 5, direction = Direction.SOUTH_EAST)
+spawn_npc(npc = Npcs.PIRATE, x = 2994, z = 9570, walkRadius = 5, direction = Direction.SOUTH_WEST)
+spawn_npc(npc = Npcs.HOBGOBLIN_2688, x = 3006, z = 9580, walkRadius = 5, direction = Direction.SOUTH)
+spawn_npc(npc = Npcs.HOBGOBLIN_2688, x = 2999, z = 9595, walkRadius = 5, direction = Direction.NORTH_EAST)
+spawn_npc(npc = Npcs.HOBGOBLIN_2688, x = 3002, z = 9593, walkRadius = 5, direction = Direction.SOUTH_EAST)
+spawn_npc(npc = Npcs.HOBGOBLIN_2688, x = 3006, z = 9595, walkRadius = 5, direction = Direction.EAST)
