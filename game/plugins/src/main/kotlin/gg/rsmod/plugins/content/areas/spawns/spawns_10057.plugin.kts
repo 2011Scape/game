dbddfc546a8bc368ddbd04e580d59d3c147e1324
Harley C. Gilpin <75695035+HarleyGilpin@users.noreply.github.com>
2023-04-05 07:21:58 -0700

    Update spawns_10057.plugin.kts
    fix: removed redundant value for NPC walkRadius parameter.

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/spawns/spawns_10057.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/spawns/spawns_10057.plugin.kts
@@ -4,4 +4,4 @@
  */
 spawn_npc(npc = Npcs.KOLODION, x = 2541, z = 4716, walkRadius = 3, direction = Direction.WEST)
 spawn_npc(npc = Npcs.GUNDAI, x = 2534, z = 4713, walkRadius = 3, direction = Direction.WEST)
-spawn_npc(npc = Npcs.LUNDAIL, x = 2535, z = 4719, walkRadius = 0, direction = Direction.SOUTH)
+spawn_npc(npc = Npcs.LUNDAIL, x = 2535, z = 4719, direction = Direction.SOUTH)
