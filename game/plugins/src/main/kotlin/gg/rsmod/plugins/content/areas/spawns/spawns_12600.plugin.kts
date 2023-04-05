250bd5fa95dd7dda5669378dbb175779376e7af8
Harley C. Gilpin <75695035+HarleyGilpin@users.noreply.github.com>
2023-04-05 07:23:28 -0700

    Update spawns_12600.plugin.kts
    fix: removed redundant value for NPC walkRadius parameter.

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/spawns/spawns_12600.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/spawns/spawns_12600.plugin.kts
@@ -2,4 +2,4 @@
  * WILDERNESS NPC & ITEM SPAWNS
  * @author Harley Gilpin <https://github.com/HarleyGilpin>
  */
-spawn_npc(npc = Npcs.GOBLIN_MUSICIAN, x = 3140, z = 3642, walkRadius = 0, direction = Direction.SOUTH)
+spawn_npc(npc = Npcs.GOBLIN_MUSICIAN, x = 3140, z = 3642, direction = Direction.SOUTH)
