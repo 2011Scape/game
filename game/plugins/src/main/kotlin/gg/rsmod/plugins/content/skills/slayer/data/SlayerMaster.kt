7722520e0d2b87f43c5b9cb516f839f366764f79
Alycia <charliesalter@hotmail.com>
2023-04-06 00:45:00 -0700

    feat: add cave bug/slime to turael

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/skills/slayer/data/SlayerMaster.kt
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/skills/slayer/data/SlayerMaster.kt
@@ -1,8 +1,10 @@
 package gg.rsmod.plugins.content.skills.slayer.data
 
 import gg.rsmod.game.model.combat.SlayerAssignment
+import gg.rsmod.plugins.api.Skills
 import gg.rsmod.plugins.api.cfg.Npcs
 import gg.rsmod.plugins.api.cfg.Requirement
+import gg.rsmod.plugins.api.cfg.SkillRequirement
 
 /**
  * @author Alycia <https://github.com/alycii>
@@ -13,7 +15,11 @@ class SlayerData(private val assignmentsByMaster: Map<SlayerMaster, List<Assignm
     }
 }
 
-data class Assignment(val assignment: SlayerAssignment, val amount: IntRange = 0..0, val requirement: List<Requirement> = emptyList())
+data class Assignment(
+    val assignment: SlayerAssignment,
+    val amount: IntRange = 0..0,
+    val requirement: List<Requirement> = emptyList()
+)
 
 enum class SlayerMaster(val id: Int, val identifier: String, val defaultAmount: IntRange) {
     TURAEL(Npcs.TURAEL, "Turael", 15..50),
@@ -28,8 +34,18 @@ val slayerData = SlayerData(
             Assignment(assignment = SlayerAssignment.BAT),
             Assignment(assignment = SlayerAssignment.BIRD),
             Assignment(assignment = SlayerAssignment.BEAR),
-            // Assignment(assignment = SlayerAssignment.CAVE_BUG),
-            // Assignment(assignment = SlayerAssignment.CAVE_SLIME),
+            Assignment(
+                assignment = SlayerAssignment.CAVE_BUG,
+                requirement = listOf(
+                    SkillRequirement(skill = Skills.SLAYER, level = 7)
+                )
+            ),
+            Assignment(
+                assignment = SlayerAssignment.CAVE_SLIME,
+                requirement = listOf(
+                    SkillRequirement(skill = Skills.SLAYER, level = 17)
+                )
+            ),
             Assignment(assignment = SlayerAssignment.COW),
             // Assignment(assignment = SlayerAssignment.CRAWLING_HAND),
             // Assignment(assignment = SlayerAssignment.DESERT_LIZARD),
