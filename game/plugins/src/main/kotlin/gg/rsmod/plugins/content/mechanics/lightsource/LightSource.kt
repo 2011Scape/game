363ddddcc65366246f20e920417b1e1a75f01808
Alycia <charliesalter@hotmail.com>
2023-04-06 00:42:38 -0700

    feat: light sources

---
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/mechanics/lightsource/LightSource.kt
@@ -0,0 +1,22 @@
+package gg.rsmod.plugins.content.mechanics.lightsource
+
+import gg.rsmod.game.model.entity.Player
+import gg.rsmod.plugins.api.cfg.Items
+
+/**
+ * @author Alycia <https://github.com/alycii>
+ */
+enum class LightSource(val raw: Int, val product: Int, val levelRequired: Int, val enclosed: Boolean, val interfaceId: Int) {
+    CANDLE(raw = Items.CANDLE, product = Items.LIT_CANDLE, levelRequired = 1, enclosed = false, interfaceId = 98);
+
+    companion object {
+        fun getActiveLightSource(player: Player): LightSource? {
+            for (source in values()) {
+                if (player.inventory.contains(source.product)) {
+                    return source
+                }
+            }
+            return null
+        }
+    }
+}
