9b47865c4dab4501c5f5e2285934e8bf4c5e9a94
Kevin <ksenez94@gmail.com>
2023-04-07 03:27:52 -0400

    feat: Add Ice warrior level 57 definitions & drop table

---
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/npcs/definitions/other/ice_warrior_level_57.plugin.kts
@@ -0,0 +1,86 @@
+package gg.rsmod.plugins.content.npcs.definitions.other
+
+import gg.rsmod.game.model.combat.SlayerAssignment
+import gg.rsmod.plugins.content.drops.DropTableFactory
+import gg.rsmod.plugins.content.drops.global.Gems
+import gg.rsmod.plugins.content.drops.global.Herbs
+import gg.rsmod.plugins.content.drops.global.Seeds
+
+val ids = intArrayOf(Npcs.ICE_WARRIOR, Npcs.ICE_WARRIOR_145, Npcs.ICE_WARRIOR_3073)
+
+val table = DropTableFactory
+val ice_warrior = table.build {
+    main {
+        total(1024)
+        obj(Items.IRON_BATTLEAXE, slots = 24)
+        obj(Items.MITHRIL_MACE, slots = 8)
+
+        obj(Items.NATURE_RUNE, quantity = 4, slots = 80)
+        obj(Items.CHAOS_RUNE, quantity = 3, slots = 64)
+        obj(Items.LAW_RUNE, quantity = 2, slots = 56)
+        obj(Items.COSMIC_RUNE, quantity = 2, slots = 40)
+        obj(Items.MITHRIL_ARROW, quantity = 3, slots = 40)
+        obj(Items.ADAMANT_ARROW, quantity = 2, slots = 16)
+        obj(Items.DEATH_RUNE, quantity = 2, slots = 24)
+        obj(Items.BLOOD_RUNE, quantity = 2, slots = 8)
+
+        table(Herbs.minorHerbTable, slots = 75)
+
+        table(Seeds.uncommonSeedtable, slots = 125)
+
+        obj(Items.COINS_995, quantity = 15, slots = 224)
+        obj(Items.COINS_995, quantity = 5, slots = 104)
+        obj(Items.COINS_995, quantity = 10, slots = 62)
+
+        nothing(50)
+
+        table(Gems.gemTable, slots = 24)
+    }
+
+    table("Tertiary") {
+        total(128)
+        obj(Items.CLUE_SCROLL_MEDIUM, slots = 1)
+        nothing(127)
+    }
+}
+
+table.register(ice_warrior, *ids)
+
+on_npc_death(*ids) {
+    table.getDrop(world, npc.damageMap.getMostDamage()!! as Player, npc.id, npc.tile)
+}
+
+ids.forEach {
+    set_combat_def(it) {
+        configs {
+            attackSpeed = 4
+            respawnDelay = 30
+        }
+        stats {
+            hitpoints = 590
+            attack = 47
+            strength = 47
+            defence = 47
+        }
+        bonuses {
+            defenceStab = 30
+            defenceSlash = 40
+            defenceCrush = 20
+            defenceMagic = 10
+            defenceRanged = 30
+        }
+        anims {
+            attack = 391
+            block = 389
+            death = 843
+        }
+        aggro {
+            radius = 5
+        }
+        slayerData {
+            slayerAssignment = SlayerAssignment.ICE_WARRIOR
+            levelRequirement = 1
+            xp = 59.0
+        }
+    }
+}
