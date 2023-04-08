e30b2a9aaf2f5135e1719888614f8d70971cde38
Harley C. Gilpin <75695035+HarleyGilpin@users.noreply.github.com>
2023-04-08 13:53:40 -0700

    bug<anti-cheat>: Fix didn't work reverting back changes...

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/util/anticheat/anti_cheat.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/util/anticheat/anti_cheat.plugin.kts
@@ -1,11 +1,6 @@
 package gg.rsmod.plugins.util.anticheat
 
 import gg.rsmod.plugins.content.combat.isBeingAttacked
-import java.util.*
-import gg.rsmod.plugins.content.drops.DropTableFactory
-import gg.rsmod.plugins.content.drops.DropTableType
-import gg.rsmod.plugins.content.drops.global.Rare
-import gg.rsmod.util.Misc
 
 /**
  * @author Alycia <https://github.com/alycii>
@@ -21,45 +16,36 @@ on_login {
     }
 }
 
-// Create a synchronized map using the `synchronizedMap` method
-val timers: MutableMap<TimerKey, Int> = Collections.synchronizedMap(HashMap())
-
-// The `on_timer` function is called every time the `TIMER` timer fires
 on_timer(TIMER) {
-    // Create a copy of the `timers` collection using the `synchronized` block to ensure thread safety
-    synchronized(timers) {
-        if(player.isBeingAttacked() || player.isLocked() || player.isDead() || player.interfaces.currentModal != -1) {
-            timers[TIMER] = 10
-            return@on_timer
-        }
-        player.interruptQueues()
-        player.stopMovement()
-        player.animate(-1)
-        timers[LOGOUT_TIMER] = 200
-        player.lockingQueue(TaskPriority.STRONG, lockState = LockState.FULL_WITH_DAMAGE_IMMUNITY) {
-            val randomNumber = world.random(100)
-            val amount = inputInt("Please answer with the following number: $randomNumber")
-            if (amount == randomNumber) {
-                if (!player.inventory.hasSpace) {
-                    itemMessageBox("Thank you for solving this random event, a gift has been added to your bank.", item = Items.MYSTERY_BOX)
-                    player.bank.add(Item(Items.MYSTERY_BOX))
-                } else {
-                    itemMessageBox("Thank you for solving this random event, a gift has been added to your inventory.", item = Items.MYSTERY_BOX)
-                    player.inventory.add(Item(Items.MYSTERY_BOX))
-                }
-                player.addLoyalty(world.random(1..30))
-                timers.remove(LOGOUT_TIMER)
-                timers[TIMER] = world.random(range)
-            } else {
-                timers[TIMER] = 1
+    if(player.isBeingAttacked() || player.isLocked() || player.isDead() || player.interfaces.currentModal != -1) {
+        player.timers[TIMER] = 10
+        return@on_timer
+    }
+    player.interruptQueues()
+    player.stopMovement()
+    player.animate(-1)
+    player.timers[LOGOUT_TIMER] = 200
+    player.lockingQueue(TaskPriority.STRONG, lockState = LockState.FULL_WITH_DAMAGE_IMMUNITY) {
+        val randomNumber = world.random(100)
+        val amount = inputInt("Please answer with the following number: $randomNumber")
+        if (amount == randomNumber) {
+            if (!player.inventory.hasSpace) {
+                itemMessageBox("Thank you for solving this random event, a gift has been added to your bank.", item = Items.MYSTERY_BOX)
+                player.bank.add(Item(Items.MYSTERY_BOX))
             }
+            else {
+                itemMessageBox("Thank you for solving this random event, a gift has been added to your inventory.", item = Items.MYSTERY_BOX)
+                player.inventory.add(Item(Items.MYSTERY_BOX))
+            }
+            player.addLoyalty(world.random(1..30))
+            player.timers.remove(LOGOUT_TIMER)
+            player.timers[TIMER] = world.random(range)
+        } else {
+            player.timers[TIMER] = 1
         }
     }
 }
 
 on_timer(LOGOUT_TIMER) {
-    // Use the `synchronized` block to ensure thread safety while calling `handleLogout` on the player
-    synchronized(timers) {
-        player.handleLogout()
-    }
+    player.handleLogout()
 }
