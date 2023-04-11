93240c7ed5780e6bb0932c71cefa0ca9d0698f67
RSDEANEO <deane1991@live.co.uk>
2023-04-11 11:26:11 +0100

    fix: spawn for 12441 (hill giants in edgeville dungeon)

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/spawns/spawns_12441.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/spawns/spawns_12441.plugin.kts
@@ -4,15 +4,19 @@ package gg.rsmod.plugins.content.areas.spawns
  * @author Alycia <https://github.com/alycii>
  */
 
-spawn_npc(npc = Npcs.HILL_GIANT, x = 3118, z = 9845, walkRadius = 5, direction = Direction.EAST)
-spawn_npc(npc = Npcs.HILL_GIANT, x = 3111, z = 9844, walkRadius = 5, direction = Direction.SOUTH)
-spawn_npc(npc = Npcs.HILL_GIANT, x = 3123, z = 9845, walkRadius = 5, direction = Direction.WEST)
-spawn_npc(npc = Npcs.HILL_GIANT, x = 3114, z = 9833, walkRadius = 5, direction = Direction.WEST)
-spawn_npc(npc = Npcs.HILL_GIANT, x = 3110, z = 9841, walkRadius = 5, direction = Direction.NORTH)
-spawn_npc(npc = Npcs.HILL_GIANT, x = 3119, z = 9839, walkRadius = 5, direction = Direction.NORTH)
-spawn_npc(npc = Npcs.HILL_GIANT, x = 3097, z = 9832, walkRadius = 5, direction = Direction.NORTH_WEST)
-spawn_npc(npc = Npcs.HILL_GIANT, x = 3101, z = 9832, walkRadius = 5, direction = Direction.NORTH)
-spawn_npc(npc = Npcs.HILL_GIANT, x = 3107, z = 9829, walkRadius = 5, direction = Direction.WEST)
-spawn_npc(npc = Npcs.HILL_GIANT, x = 3115, z = 9831, walkRadius = 5, direction = Direction.EAST)
-spawn_npc(npc = Npcs.HILL_GIANT, x = 3109, z = 9835, walkRadius = 5, direction = Direction.EAST)
+spawn_npc(npc = Npcs.HILL_GIANT, x = 3118, z = 9849, walkRadius = 5, direction = Direction.EAST)
+spawn_npc(npc = Npcs.HILL_GIANT, x = 3120, z = 9845, walkRadius = 5, direction = Direction.SOUTH)
+spawn_npc(npc = Npcs.HILL_GIANT, x = 3116, z = 9845, walkRadius = 5, direction = Direction.WEST)
+spawn_npc(npc = Npcs.HILL_GIANT, x = 3110, z = 9843, walkRadius = 5, direction = Direction.WEST)
+spawn_npc(npc = Npcs.HILL_GIANT, x = 3111, z = 9840, walkRadius = 5, direction = Direction.NORTH)
+spawn_npc(npc = Npcs.HILL_GIANT, x = 3117, z = 9835, walkRadius = 5, direction = Direction.NORTH)
+spawn_npc(npc = Npcs.HILL_GIANT, x = 3116, z = 9832, walkRadius = 5, direction = Direction.NORTH_WEST)
+spawn_npc(npc = Npcs.HILL_GIANT, x = 3112, z = 9832, walkRadius = 5, direction = Direction.NORTH)
+spawn_npc(npc = Npcs.HILL_GIANT, x = 3108, z = 9833, walkRadius = 5, direction = Direction.WEST)
+spawn_npc(npc = Npcs.HILL_GIANT, x = 3106, z = 9828, walkRadius = 5, direction = Direction.EAST)
+spawn_npc(npc = Npcs.HILL_GIANT, x = 3102, z = 9831, walkRadius = 5, direction = Direction.EAST)
+spawn_npc(npc = Npcs.HILL_GIANT, x = 3100, z = 9834, walkRadius = 5, direction = Direction.NORTH)
+
+
+
 
