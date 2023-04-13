939f25535bcebfabeb8a9c81c6f1e32d2257a7f1
Harley C. Gilpin <75695035+HarleyGilpin@users.noreply.github.com>
2023-04-13 07:57:44 -0700

    feat: added drill demon minigame spawns.

---
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/spawns/spawns_12619.plugin.kts
@@ -0,0 +1,8 @@
+package gg.rsmod.plugins.content.areas.spawns
+
+spawn_obj(obj = Objs.SITUP_SIGN, x = 3160, z = 4821, height = 0, type = 10, rot = 0)
+spawn_obj(obj = Objs.PUSHUP_SIGN, x = 3162, z = 4821, height = 0, type = 10, rot = 0)
+spawn_obj(obj = Objs.STARJUMP_SIGN, x = 3164, z = 4821, height = 0, type = 10, rot = 0)
+spawn_obj(obj = Objs.RUNNINGMAN_SIGN, x = 3166, z = 4821, height = 0, type = 10, rot = 0)
+
+spawn_npc(npc = Npcs.SERGEANT_DAMIEN, x = 3164, z = 4822, direction = Direction.SOUTH)
