989443116834c2014f9968b40ef045f225203722
RSDEANEO <deane1991@live.co.uk>
2023-04-28 22:51:55 +0100

    feat: added missing fishing spots for 10807

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/spawns/spawns_10807.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/spawns/spawns_10807.plugin.kts
@@ -4,7 +4,9 @@ package gg.rsmod.plugins.content.areas.spawns
  * @author Alycia <https://github.com/alycii>
  */
 
-spawn_npc(npc = Npcs.FISHING_SPOT_LURE_BAIT, x = 2716, z = 3530, direction = Direction.NORTH_WEST, static = true)
-
+spawn_npc(npc = Npcs.FISHING_SPOT_315, x = 2714, z = 3532, direction = Direction.NORTH, static = true)
+spawn_npc(npc = Npcs.FISHING_SPOT_315, x = 2714, z = 3533, direction = Direction.NORTH, static = true)
+spawn_npc(npc = Npcs.FISHING_SPOT_315, x = 2716, z = 3530, direction = Direction.NORTH, static = true)
+spawn_npc(npc = Npcs.FISHING_SPOT_315, x = 2718, z = 3529, direction = Direction.NORTH, static = true)
 
 
