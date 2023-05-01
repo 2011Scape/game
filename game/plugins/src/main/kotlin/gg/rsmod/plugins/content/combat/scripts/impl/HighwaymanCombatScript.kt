b31e65d8c0abc045de0f145ff7b42072b9244baf
Harley C. Gilpin <75695035+HarleyGilpin@users.noreply.github.com>
2023-05-01 09:13:55 -0700

    feat: added highwayman combat definition

---
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/combat/scripts/impl/HighwaymanCombatScript.kt
@@ -0,0 +1,46 @@
+package gg.rsmod.plugins.content.combat.scripts.impl
+
+import gg.rsmod.game.model.Tile
+import gg.rsmod.game.model.TileGraphic
+import gg.rsmod.game.model.combat.CombatClass
+import gg.rsmod.game.model.combat.CombatScript
+import gg.rsmod.game.model.combat.StyleType
+import gg.rsmod.game.model.combat.WeaponStyle
+import gg.rsmod.game.model.queue.QueueTask
+import gg.rsmod.plugins.api.HitType
+import gg.rsmod.plugins.api.cfg.Npcs
+import gg.rsmod.plugins.api.ext.npc
+import gg.rsmod.plugins.api.ext.prepareAttack
+import gg.rsmod.plugins.content.combat.*
+import gg.rsmod.plugins.content.combat.formula.MeleeCombatFormula
+import java.util.*
+
+object HighwaymanCombatScript : CombatScript() {
+
+    override val ids = intArrayOf(Npcs.HIGHWAYMAN, Npcs.HIGHWAYMAN_2677, Npcs.HIGHWAYMAN_7443)
+
+    override suspend fun handleSpecialCombat(it: QueueTask) {
+        val npc = it.npc
+        var target = npc.getCombatTarget() ?: return
+        var chatExecuted = false
+
+        while (npc.canEngageCombat(target)) {
+            npc.facePawn(target)
+            if (npc.moveToAttackRange(it, target, distance = 1, projectile = false) && npc.isAttackDelayReady()) {
+                    if (!chatExecuted) {
+                        npc.forceChat("Stand and deliver!")
+                        chatExecuted = true
+                    }
+                    npc.prepareAttack(CombatClass.MELEE, StyleType.SLASH, WeaponStyle.ACCURATE)
+                    npc.animate(npc.combatDef.attackAnimation)
+                    npc.dealHit(target = target, formula = MeleeCombatFormula, delay = 1, type = HitType.MELEE)
+                    npc.postAttackLogic(target)
+            }
+            it.wait(4)
+            target = npc.getCombatTarget() ?: break
+        }
+
+        npc.resetFacePawn()
+        npc.removeCombatTarget()
+    }
+}
