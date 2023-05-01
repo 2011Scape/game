a8d5be23dadc76717f0310dff6a7510da94b4832
RSDEANEO <deane1991@live.co.uk>
2023-05-01 23:12:37 +0100

    feat: added fishing spawns for 13877

---
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/spawns/spawn_13877.plugin.kts
@@ -0,0 +1,8 @@
+package gg.rsmod.plugins.content.areas.spawns
+
+spawn_npc(npc = Npcs.FISHING_SPOT_1236, x = 3479, z = 3434, direction = Direction.NORTH, static = true)
+spawn_npc(npc = Npcs.FISHING_SPOT_1236, x = 3481, z = 3437, direction = Direction.NORTH, static = true)
+spawn_npc(npc = Npcs.FISHING_SPOT_1236, x = 3482, z = 3430, direction = Direction.NORTH, static = true)
+spawn_npc(npc = Npcs.FISHING_SPOT_1236, x = 3483, z = 3449, direction = Direction.NORTH, static = true)
+spawn_npc(npc = Npcs.FISHING_SPOT_1236, x = 3490, z = 3444, direction = Direction.NORTH, static = true)
+
