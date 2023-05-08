3b46570b41d4460249123edaf5e852c87db879f7
Harley C. Gilpin <75695035+HarleyGilpin@users.noreply.github.com>
2023-05-08 16:37:36 -0700

    bug: added missing citizens.

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/canifis/canifis_citizen.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/canifis/canifis_citizen.plugin.kts
@@ -18,39 +18,21 @@ val citizens = listOf(
     Npcs.SVETLANA,
     Npcs.ZOJA,
     Npcs.YADVIGA,
-    Npcs.NIKITA
-)
-
-val male_citizens = listOf(
-    Npcs.EDUARD,
-    Npcs.LEV,
-    Npcs.YURI,
-    Npcs.BORIS,
-    Npcs.GEORGY,
-    Npcs.JOSEPH,
-    Npcs.NIKOLAI,
-    Npcs.IMRE
-)
-
-val female_citizens = listOf(
-    Npcs.VERA,
-    Npcs.MILLA,
-    Npcs.SOFIYA,
-    Npcs.IRINA,
-    Npcs.SVETLANA,
-    Npcs.ZOJA,
-    Npcs.YADVIGA,
-    Npcs.NIKITA
+    Npcs.NIKITA,
+    Npcs.LILIYA,
+    Npcs.ALEXIS,
+    Npcs.KSENIA,
+    Npcs.GALINA
 )
 
 citizens.forEach {
     on_npc_option(it, option = "talk-to") {
-        player.queue { maleCanifisCitizenChat(this) }
+        player.queue { CanifisCitizenChat(this) }
     }
 }
 
-fun maleCanifisCitizenChat(maleCanifisCitizenChat: QueueTask) {
-    maleCanifisCitizenChat.player.queue {
+fun CanifisCitizenChat(canifisCitizenChat: QueueTask) {
+    canifisCitizenChat.player.queue {
         when (Random.nextInt(1, 10)) {
             1 -> chatNpc("If I were as ugly as you I would not dare", "to show my face in public!")
             2 -> chatNpc("Out of my way, punk.")
