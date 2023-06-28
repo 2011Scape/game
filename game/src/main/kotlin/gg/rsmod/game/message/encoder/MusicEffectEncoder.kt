3ba4a66539e6811b9a5a1aac73b7ee6152b3dbfb
Harley C. Gilpin <75695035+HarleyGilpin@users.noreply.github.com>
2023-06-28 14:39:16 -0700

    feat: added MusicEffectEncoder

---
+++ game/src/main/kotlin/gg/rsmod/game/message/encoder/MusicEffectEncoder.kt
@@ -0,0 +1,19 @@
+package gg.rsmod.game.message.encoder
+
+import gg.rsmod.game.message.MessageEncoder
+import gg.rsmod.game.message.impl.MusicEffectMessage
+
+/**
+ * @author Harley <https://github.com/HarleyGilpin>
+ */
+class MusicEffectEncoder : MessageEncoder<MusicEffectMessage>() {
+
+    override fun extract(message: MusicEffectMessage, key: String): Number = when (key) {
+        "id" -> message.id
+        "unknown_byte" -> message.unknownByte
+        "volume" -> message.volume
+        else -> throw Exception("Unhandled value key.")
+    }
+
+    override fun extractBytes(message: MusicEffectMessage, key: String): ByteArray = throw Exception("Unhandled value key.")
+}
---
+++ game/src/main/kotlin/gg/rsmod/game/message/impl/MusicEffectMessage.kt
@@ -0,0 +1,8 @@
+package gg.rsmod.game.message.impl
+
+import gg.rsmod.game.message.Message
+
+/**
+ * @author Harley <github.com/HarleyGilpin>
+ */
+data class MusicEffectMessage(val id: Int, val unknownByte: Int = 0, val volume: Int = 255) : Message
