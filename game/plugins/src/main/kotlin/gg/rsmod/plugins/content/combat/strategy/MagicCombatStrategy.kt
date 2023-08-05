a05c97beee234342179abdad9fe5d944704e6d0a
Harley C. Gilpin <75695035+HarleyGilpin@users.noreply.github.com>
2023-08-05 14:37:17 -0700

    feat: added graphics rotation functions for magic

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/combat/strategy/MagicCombatStrategy.kt
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/combat/strategy/MagicCombatStrategy.kt
@@ -1,5 +1,7 @@
 package gg.rsmod.plugins.content.combat.strategy
 
+import gg.rsmod.game.Server.Companion.logger
+import gg.rsmod.game.model.Direction
 import gg.rsmod.game.model.Graphic
 import gg.rsmod.game.model.Tile
 import gg.rsmod.game.model.entity.Npc
@@ -33,13 +35,42 @@ object MagicCombatStrategy : CombatStrategy {
         return true
     }
 
+    fun convertToRotation(value: Int): Int {
+        return when(value) {
+            0 -> 1
+            1 -> 0
+            2 -> 7
+            3 -> 2
+            4 -> 6
+            5 -> 3
+            6 -> 4
+            7 -> 5
+            else -> throw IllegalArgumentException("Invalid value for direction: $value")
+        }
+    }
+
+    fun combinedDirection(attackDirection: Direction, targetDirection: Direction): Int {
+        val lookupTable = arrayOf(
+            intArrayOf(1, 2, 4, 0, 7, 3, 5, 6), // Player Facing NORTH_WEST
+            intArrayOf(0, 1, 2, 3, 4, 5, 6, 7), // Player Facing NORTH
+            intArrayOf(3, 0, 1, 5, 2, 6, 7, 4), // Player Facing NORTH_EAST
+            intArrayOf(2, 4, 7, 1, 6, 0, 3, 5), // Player Facing WEST
+            intArrayOf(5, 3, 0, 6, 1, 7, 4, 2), // Player Facing EAST
+            intArrayOf(4, 7, 6, 2, 5, 1, 0, 3), // Player Facing SOUTH_WEST
+            intArrayOf(7, 6, 5, 4, 3, 2, 1, 0), // Player Facing SOUTH
+            intArrayOf(6, 5, 3, 7, 0, 4, 2, 1)  // Player Facing SOUTH_EAST
+        )
+        return lookupTable[targetDirection.orientationValue][attackDirection.orientationValue]
+    }
     override fun attack(pawn: Pawn, target: Pawn) {
         val world = pawn.world
 
         val spell = pawn.attr[Combat.CASTING_SPELL] ?: return
-
+        val attackDirection = Direction.calculateAttackDirection(target.tile, pawn.tile)
+        val targetDirection = target.faceDirection
+        val impactDirection: Int = combinedDirection(attackDirection, targetDirection)
+        val impactGfxRotation: Int = convertToRotation(impactDirection)
         val projectile = pawn.createProjectile(target, gfx = spell.projectile, type = ProjectileType.MAGIC)
-
         pawn.stopMovement()
         spell.castGfx?.let { gfx -> pawn.graphic(gfx) }
         var animation = spell.castAnimation[0]
@@ -50,7 +81,11 @@ object MagicCombatStrategy : CombatStrategy {
             animation = spell.castAnimation.getOrNull(2) ?: spell.castAnimation[0]
         }
         pawn.animate(animation)
-        spell.impactGfx?.let { gfx -> target.graphic(Graphic(gfx.id, gfx.height, projectile.lifespan)) }
+        if (pawn is Player) {
+            logger.info("Hit Direction: ${impactDirection}, Hit Rotation: ${impactGfxRotation}, Attack Direction: ${attackDirection}:${attackDirection.orientationValue}, Target Face:${targetDirection}:${targetDirection.orientationValue}")
+
+        }
+        spell.impactGfx?.let { gfx -> target.graphic(Graphic(gfx.id, gfx.height, projectile.lifespan, impactGfxRotation)) }
         if (spell.projectile > -1) {
             world.spawn(pawn.createProjectile(target, gfx = spell.projectile, type = ProjectileType.MAGIC))
         }
