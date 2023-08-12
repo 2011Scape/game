e344e1cd470b95e77104352499a5fe505621b5ab
Harley C. Gilpin <75695035+HarleyGilpin@users.noreply.github.com>
2023-08-12 08:21:43 -0700

    feat: added player attributes for barbarian agility

--- game/src/main/kotlin/gg/rsmod/game/model/attr/Attributes.kt
+++ game/src/main/kotlin/gg/rsmod/game/model/attr/Attributes.kt
@@ -45,12 +45,22 @@ val DISPLAY_MODE_CHANGE_ATTR = AttributeKey<Int>()
 val FACING_PAWN_ATTR = AttributeKey<WeakReference<Pawn>>()
 
 /**
+ * The barbarian agility stage
+ */
+val BARBARIAN_AGILITY_STAGE = AttributeKey<Int>()
+
+/**
+ * The advanced barbarian agility stage
+ */
+val ADVANCED_BARBARIAN_AGILITY_STAGE = AttributeKey<Int>()
+
+/**
  * The gnome agility stage
  */
 val GNOME_AGILITY_STAGE = AttributeKey<Int>()
 
 /**
- * The gnome agility stage
+ * The advanced gnome agility stage
  */
 val ADVANCED_GNOME_AGILITY_STAGE = AttributeKey<Int>()
 
