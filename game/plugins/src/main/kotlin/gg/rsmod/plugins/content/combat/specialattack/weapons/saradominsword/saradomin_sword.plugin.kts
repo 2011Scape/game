62e67635ef0fcd7e86eb021f1147dcfd75af7ecd
William1Gray <wgray5093@gmail.com>
2023-09-14 22:01:12 -0300

    rebalance: make the saradomin sword special attack better reflect what it was in 2011

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/combat/specialattack/weapons/saradominsword/saradomin_sword.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/combat/specialattack/weapons/saradominsword/saradomin_sword.plugin.kts
@@ -8,30 +8,28 @@ val SPECIAL_REQUIREMENT = 100
 val SARASWORD_SPEC_ANIMATION = 11993
 val SARASWORD_SPEC_PLAYER_GFX = 2115
 val SARASWORD_SPEC_TARGET_GFX = 1194
-val MAGIC_DAMAGE_MAX_HIT = 16.0
+val MAGIC_DAMAGE_MAX_HIT = 20.0
+val MAGIC_DAMAGE_MIN_HIT = 5.0
 val SARASWORD_SPEC_SFX_ID1 = 3887 // According to https://www.runelister.com/forum/topic/osrs-sound-effect-list/
 val SARASWORD_SPEC_SFX_ID2 = 3869
 /**
- * From the OSRS wiki:
- * The Saradomin sword has a special attack, Saradomin's Lightning, that deals 10% more melee damage and 1-16 extra Magic damage.
- * This special attack consumes 100% of the wielder's special attack energy.
- *
- * The special attack rolls against the opponent's Slash defence bonus using the attack bonus of the stance the player is currently in.
- * If the melee attack misses, then the Magic attack will also fail. However, if the melee hit is a successful hit but rolls a 0, the Magic damage will still be applied.
- * Players receive 2 Magic experience for each point of damage caused by the extra Magic damage.
- *
- * The magical part of Saradomin's Lightning will always splash when directed at players using Protect from Magic.
- *
- * From sal's realm
- * Its special attack, Saradomin's Lightning, will deal 5-15 extra Magic damage as lightning strikes, using 100% of the special attack bar.
+ * From the wiki in 2011 (see https://runescape.wiki/w/Saradomin_sword?oldid=4832090):
+ * Its special attack, Saradomin's Lightning, adds 50-200 extra magic-based melee damage to the standard melee attack
+ * and raises the maximum inflicted damage by 10% for the melee hit. Players receive magic experience for using this
+ * special, but only for the magical hit. Essentially, the special deals two attacks: the melee attack that the
+ * player would normally have hit (with its maximum inflicted damage raised by 10% however) without the special,
+ * and an extra magical attack that can deal 50-200(or 90-240 with a ferocious ring) damage. The bonus from a
+ * hexcrest or full slayer helm on a slayer task is applied to this magical attack. The special attack drains 100% of
+ * the special attack bar.
  */
 
 SpecialAttacks.register(SPECIAL_REQUIREMENT, Items.SARADOMIN_SWORD) {
+    //First normal attack
     val maxHit = MeleeCombatFormula.getMaxHit(player, target, specialAttackMultiplier = 1.10)
     val accuracy = MeleeCombatFormula.getAccuracy(player, target)
     val landHit = accuracy >= world.randomDouble()
     val delay = 1
-    val firstHit = player.dealHit(
+    player.dealHit(
         target = target,
         maxHit = maxHit,
         landHit = landHit,
@@ -39,24 +37,22 @@ SpecialAttacks.register(SPECIAL_REQUIREMENT, Items.SARADOMIN_SWORD) {
         hitType = HitType.MELEE
     )
 
-    if (firstHit.landed) {
-        world.spawn(AreaSound(tile = player.tile, id = SARASWORD_SPEC_SFX_ID1, radius = 10, volume = 1))
-        world.spawn(AreaSound(tile = player.tile, id = SARASWORD_SPEC_SFX_ID2, radius = 10, volume = 1))
+    //Magic special attack
+    world.spawn(AreaSound(tile = player.tile, id = SARASWORD_SPEC_SFX_ID1, radius = 10, volume = 1))
+    world.spawn(AreaSound(tile = player.tile, id = SARASWORD_SPEC_SFX_ID2, radius = 10, volume = 1))
 
-        player.animate(id = SARASWORD_SPEC_ANIMATION)
-        player.graphic(SARASWORD_SPEC_PLAYER_GFX)
-        target.graphic(SARASWORD_SPEC_TARGET_GFX)
+    player.animate(id = SARASWORD_SPEC_ANIMATION)
+    player.graphic(SARASWORD_SPEC_PLAYER_GFX)
+    target.graphic(SARASWORD_SPEC_TARGET_GFX)
 
-        val landSecondHit = target.prayerIcon != PrayerIcon.PROTECT_FROM_MAGIC.id // Should the Pawn.dealHit function handle this instead of being added here?
-        val magicDamage = world.random(5..15)
-        player.addXp(Skills.MAGIC, 2.0*magicDamage, modifiers = false)
+    val totalMagicDamage = player.dealHit(
+        target = target,
+        maxHit = MAGIC_DAMAGE_MAX_HIT,
+        minHit = MAGIC_DAMAGE_MIN_HIT,
+        landHit = true,
+        delay = 1,
+        hitType = HitType.MAGIC
+    ).hit.hitmarks.sumOf { x -> x.damage }
 
-        player.dealHit(
-            target = target,
-            maxHit = MAGIC_DAMAGE_MAX_HIT,
-            landHit = landSecondHit,
-            delay = 1,
-            hitType = HitType.MAGIC
-        )
-    }
+    player.addXp(Skills.MAGIC, 0.2*totalMagicDamage, modifiers = false)
 }
