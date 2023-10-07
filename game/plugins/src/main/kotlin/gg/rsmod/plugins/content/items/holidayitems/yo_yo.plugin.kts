b581a9caeeb3eec72e7452d089a036900a142ecb
Harley <hg@harleygilpin.com>
2023-10-07 11:59:10 -0700

    Fix: Fixed sound references for yo-yo.

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/items/holidayitems/yo_yo.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/items/holidayitems/yo_yo.plugin.kts
@@ -4,22 +4,22 @@ val YO_YO = intArrayOf(Items.YOYO, Items.YOYO_10733)
 
 YO_YO.forEach {
     on_item_option(item = it, option = "play") {
-        player.playSound(Sfx.YOYO_PLAY)
+        player.playSound(Sfx.YOYO1)
         player.animate(1457)
     }
 
     on_item_option(item = it, option = "loop") {
-        player.playSound(Sfx.YOYO_LOOP)
+        player.playSound(Sfx.YOYO2)
         player.animate(1458)
     }
 
     on_item_option(item = it, option = "walk") {
-        player.playSound(Sfx.YOYO_WALK)
+        player.playSound(Sfx.YOYO3)
         player.animate(1459)
     }
 
     on_item_option(item = it, option = "crazy") {
-        player.playSound(Sfx.YOYO_CRAZY)
+        player.playSound(Sfx.YOYO4)
         player.animate(1460)
     }
 }
