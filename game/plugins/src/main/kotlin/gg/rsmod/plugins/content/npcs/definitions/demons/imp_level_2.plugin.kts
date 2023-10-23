a5133b05a71d4c616842c36c89014a728cee24f0
Harley <hg@harleygilpin.com>
2023-10-23 08:01:07 -0700

    bug: fixed imp teleport causing game crashes.

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/npcs/definitions/demons/imp_level_2.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/npcs/definitions/demons/imp_level_2.plugin.kts
@@ -2,8 +2,81 @@ package gg.rsmod.plugins.content.npcs.definitions.demons
 
 import gg.rsmod.game.model.combat.StyleType
 import gg.rsmod.plugins.content.drops.DropTableFactory
+import kotlin.random.Random
+import kotlin.random.nextInt
 
 val ids = intArrayOf(Npcs.IMP, Npcs.IMP_709)
+
+// Constants
+val TELEPORT_TIMER = TimerKey()
+val DELAY = 50..100
+val GRAPHIC_ID = 74
+val MAX_TILE_ATTEMPTS = 3
+val TILE_RANGE = -10..10
+
+// Event handler for when a global NPC spawns.
+on_global_npc_spawn {
+    // Check if the spawned NPC is an IMP
+    if (npc.id == Npcs.IMP || npc.id == Npcs.IMP_709) {
+        // If it is, set a teleport timer with a random delay for this NPC.
+        npc.timers[TELEPORT_TIMER] = world.random(DELAY)
+    }
+}
+
+// Event handler for when the teleport timer triggers.
+on_timer(TELEPORT_TIMER) {
+    // If the NPC is not alive, exit the function.
+    if (!npc.isAlive()) return@on_timer
+
+    // Retrieve the region ID of the NPC's current tile.
+    val npcRegion = npc.tile.regionId
+
+    // Try to find a valid tile within the same region for teleportation.
+    val randomTile = findValidTile(npc, npcRegion)
+
+    // If a valid tile is found and the NPC is not locked, proceed with teleportation.
+    if (randomTile != null && npc.lock.canMove()) {
+        npc.queue {
+            // Spawn a graphic effect at the NPC's current position.
+            world.spawn(TileGraphic(tile = npc.tile, id = GRAPHIC_ID, height = 0))
+
+            // Teleport the NPC to the new valid tile.
+            npc.teleportNpc(randomTile)
+
+            // Wait for one game tick.
+            wait(1)
+
+            // Spawn a graphic effect at the NPC's new position post-teleportation.
+            world.spawn(TileGraphic(tile = npc.tile, id = GRAPHIC_ID, height = 0))
+        }
+    } else {
+        // If teleportation failed, handle the failure (logging or other actions).
+    }
+
+    // Reset the teleport timer with a new random delay.
+    npc.timers[TELEPORT_TIMER] = DELAY.random()
+}
+
+// Function to find a valid tile for NPC teleportation, ensuring it's within the same region and not clipped.
+fun findValidTile(npc: Npc, npcRegion: Int): Tile? {
+    // Attempt to find a valid tile up to the maximum number of defined attempts.
+    repeat(MAX_TILE_ATTEMPTS) {
+        // Generate a random tile within the defined range.
+        val randomTile = npc.tile.transform(
+            x = Random.nextInt(TILE_RANGE),
+            z = Random.nextInt(TILE_RANGE)
+        )
+        // Check if the tile is valid (not clipped and within the same region).
+        if (!world.collision.isClipped(randomTile) && randomTile.regionId == npcRegion) {
+            return randomTile // Return the valid tile.
+        }
+    }
+    return null // Return null if no valid tile was found within the attempts.
+}
+
+// Function to handle cases when teleportation fails.
+fun handleTeleportFailure(npc: Npc, tile: Tile?) {
+}
 
 val table = DropTableFactory
 val imp = table.build {
