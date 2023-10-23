501d5d68bf761f740e5bb9b4af482935ad72d458
Harley <hg@harleygilpin.com>
2023-10-23 07:59:19 -0700

    feat: improved abyssal demon teleport.

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/combat/scripts/impl/AbyssalDemonCombatScript.kt
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/combat/scripts/impl/AbyssalDemonCombatScript.kt
@@ -31,39 +31,33 @@ object AbyssalDemonCombatScript : CombatScript() {
         while (npc.canEngageCombat(target)) {
             npc.facePawn(target)
             if (npc.moveToAttackRange(it, target, distance = 1, projectile = false) && npc.isAttackDelayReady()) {
-                if (world.random(2) == 1) {
-                    world.spawn(TileGraphic(tile = npc.tile, id = 409, height = 0))
-                    it.wait(1)
-                    var randomNPCTile = getRandomTile(npc.tile)
-                    var randomPlayerTile = getRandomTile(target.tile)
-                    while (world.collision.isClipped(randomPlayerTile)) {
-                        randomPlayerTile = getRandomTile(target.tile)
-                    }
-                    while (world.collision.isClipped(randomNPCTile)) {
-                        randomNPCTile = getRandomTile(npc.tile)
-                    }
-                    // Teleport NPC or Player based on a random decision
-                    if (Random.nextBoolean() && npc.lock.canMove()) {
-                        target.fullInterruption(interactions = true)
-                        npc.invisible = true
-                        npc.walkMask = npc.def.walkMask
-                        npc.walkTo(randomPlayerTile, MovementQueue.StepType.FORCED_WALK) // Teleport NPC
-                        it.wait(2)
-                        npc.invisible = false
-                        npc.prepareAttack(CombatClass.MELEE, StyleType.STAB, WeaponStyle.NONE)
-                        npc.animate(npc.combatDef.attackAnimation)
-                        npc.dealHit(target = target, formula = MeleeCombatFormula, delay = 1, type = HitType.MELEE)
-                    } else {
-                        target.teleportTo(randomNPCTile) // Teleport Player
-                    }
-                } else {
-                    npc.prepareAttack(CombatClass.MELEE, StyleType.STAB, WeaponStyle.NONE)
-                    npc.animate(npc.combatDef.attackAnimation)
-                    npc.dealHit(target = target, formula = MeleeCombatFormula, delay = 1, type = HitType.MELEE)
-                }
+                // Perform the attack
+                npc.prepareAttack(CombatClass.MELEE, StyleType.STAB, WeaponStyle.NONE)
+                npc.animate(npc.combatDef.attackAnimation)
+                npc.dealHit(target = target, formula = MeleeCombatFormula, delay = 1, type = HitType.MELEE)
                 npc.postAttackLogic(target)
+
+                // Check for teleportation after a successful hit
+                if (world.random(6) == 0) { // 1/6 chance to teleport away
+                    val randomNPCTile = getRandomTileWithinArea()
+                    if (npc.lock.canMove() && !world.collision.isClipped(randomNPCTile)) {
+                        world.spawn(TileGraphic(tile = randomNPCTile, id = 409, height = 0))
+                        npc.teleportNpc(randomNPCTile)
+                    }
+                } else if (world.random(3) == 0) { // 1/3 chance to teleport the player if the demon didn't teleport
+                    val randomPlayerTile = getRandomTileWithinArea()
+                    if (!world.collision.isClipped(randomPlayerTile)) {
+                        world.spawn(TileGraphic(tile = randomPlayerTile, id = 409, height = 0))
+                        target.teleportTo(randomPlayerTile)
+                        target.fullInterruption(interactions = true) // Player stops attacking
+                        // If Auto Retaliate is on, the player will automatically start attacking again
+                    }
+                }
+
+                it.wait(4)
+            } else {
+                it.wait(1) // Wait for a tick before re-evaluating conditions
             }
-            it.wait(4)
             target = npc.getCombatTarget() ?: break
         }
 
@@ -71,13 +65,38 @@ object AbyssalDemonCombatScript : CombatScript() {
         npc.removeCombatTarget()
     }
 
-    private fun getRandomTile(tile: Tile) : Tile {
-        var deltaX: Int
-        var deltaZ: Int
-        do {
-            deltaX = Random.nextInt(-1, 2)
-            deltaZ = Random.nextInt(-1, 2)
-        } while (deltaX == 0 && deltaZ == 0)
-        return tile.transform(x = deltaX, z = deltaZ)
+    private val validTiles: List<Tile> by lazy { generateValidTiles() }
+
+    private fun generateValidTiles(): List<Tile> {
+        // Define the boundaries of the teleportation area
+        val minX = 3409
+        val maxX = 3430
+        val minY = 3555
+        val maxY = 3576
+        val plane = 2
+
+        // Define the boundaries of the exclusion area
+        val excludeMinX = 3421
+        val excludeMaxX = 3430
+        val excludeMinY = 3555
+        val excludeMaxY = 3561
+
+        val tiles = mutableListOf<Tile>()
+
+        for (x in minX..maxX) {
+            for (y in minY..maxY) {
+                // Check if the coordinates are not within the exclusion zone
+                if (x !in excludeMinX..excludeMaxX || y !in excludeMinY..excludeMaxY) {
+                    tiles.add(Tile(x, y, plane))
+                }
+            }
+        }
+
+        return tiles
+    }
+
+    private fun getRandomTileWithinArea(): Tile {
+        // Select a random tile from the precomputed list of valid tiles
+        return validTiles.random()
     }
 }
