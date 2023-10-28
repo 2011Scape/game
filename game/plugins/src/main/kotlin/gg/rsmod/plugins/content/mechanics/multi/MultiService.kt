8e667c8196844365be09ce168ea4184cef0a0a3d
Harley <hg@harleygilpin.com>
2023-10-28 11:40:44 -0700

    feat<wip>: updated to yml to add inline comments.

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/mechanics/multi/MultiService.kt
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/mechanics/multi/MultiService.kt
@@ -1,32 +1,39 @@
-package gg.rsmod.plugins.content.mechanics.multi
+package gg.rsmod.plugins.content.mechanics.multi;
 
-import com.google.gson.Gson
-import gg.rsmod.game.Server
-import gg.rsmod.game.model.World
-import gg.rsmod.game.model.region.ChunkCoords
-import gg.rsmod.game.service.Service
-import gg.rsmod.plugins.api.ext.appendToString
-import gg.rsmod.util.ServerProperties
-import mu.KLogging
-import java.nio.file.Files
-import java.nio.file.Paths
+import gg.rsmod.game.Server;
+import gg.rsmod.game.model.World;
+import gg.rsmod.game.model.region.ChunkCoords;
+import gg.rsmod.game.service.Service;
+import gg.rsmod.plugins.api.ext.appendToString;
+import gg.rsmod.util.ServerProperties;
+import mu.KLogging;
+import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.constructor.Constructor;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
+
 class MultiService : Service {
 
     val multiRegions = mutableListOf<Int>()
-
     val multiChunks = mutableListOf<ChunkCoords>()
 
     override fun init(server: Server, world: World, serviceProperties: ServerProperties) {
-        val file = Paths.get(serviceProperties.get("areas") ?: "./data/cfg/areas/multi.json")
+        val file = Paths.get(serviceProperties.get("areas") ?: "./data/cfg/areas/multi.yml")
         Files.newBufferedReader(file).use { reader ->
-            val multiAreaData = Gson().fromJson(reader, MultiAreaData::class.java)
+            val yaml = Yaml(Constructor(Map::class.java))
+            val multiAreaData = yaml.load(reader) as Map<String, Any>
 
-            multiAreaData.regions.forEach { region ->
+            @Suppress("UNCHECKED_CAST")
+            (multiAreaData["regions"] as List<Int>).forEach { region ->
                 setMultiCombatRegion(region)
             }
 
-            multiAreaData.chunks.forEach { (x, z) ->
-                setMultiCombatChunks(ChunkCoords(x, z))
+            @Suppress("UNCHECKED_CAST")
+            (multiAreaData["chunks"] as List<Map<String, Int>>).forEach { chunkMap ->
+                setMultiCombatChunks(ChunkCoords(chunkMap["x"]!!, chunkMap["z"]!!))
             }
         }
 
