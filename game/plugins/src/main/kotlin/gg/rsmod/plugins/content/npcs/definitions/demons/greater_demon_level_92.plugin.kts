7a7f86fe444a4b2bb2d83e5e3b48c8213d6ea934
RSDEANEO <deane1991@live.co.uk>
2024-01-09 13:12:16 +0000

    feat: updated drops

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/npcs/definitions/demons/greater_demon_level_92.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/npcs/definitions/demons/greater_demon_level_92.plugin.kts
@@ -4,56 +4,50 @@ import gg.rsmod.game.model.combat.SlayerAssignment
 import gg.rsmod.game.model.combat.StyleType
 import gg.rsmod.plugins.content.drops.DropTableFactory
 import gg.rsmod.plugins.content.drops.global.Gems
-import gg.rsmod.plugins.content.drops.global.Herbs
-import gg.rsmod.plugins.content.drops.global.Rare
+
 
 val ids = intArrayOf(Npcs.GREATER_DEMON, Npcs.GREATER_DEMON_4698, Npcs.GREATER_DEMON_4699, Npcs.GREATER_DEMON_4700, Npcs.GREATER_DEMON_4701)
 
 val table = DropTableFactory
 val greaterdemon = table.build {
     guaranteed {
-        obj(Items.INFERNAL_ASHES)
+        obj(Items.ACCURSED_ASHES, quantity = 1)
     }
+    /** Running total: 246 **/
     main {
-        total(182) // Total number of drops available
-
-        obj(Items.STEEL_2H_SWORD, quantity = 1, slots = 4)
-        obj(Items.STEEL_HATCHET, quantity = 1, slots = 3)
-        obj(Items.STEEL_BATTLEAXE, quantity = 1, slots = 3)
-        obj(Items.MITHRIL_KITESHIELD, quantity = 1, slots = 1)
-        obj(Items.ADAMANT_PLATELEGS, quantity = 1, slots = 1)
-        obj(Items.RUNE_FULL_HELM, quantity = 1, slots = 1)
-
-        obj(Items.MITHRIL_KITESHIELD, quantity = 1, slots = 16)
-        obj(Items.RUNE_CHAINBODY, quantity = 1, slots = 8)
-        obj(Items.RUNE_MED_HELM, quantity = 1, slots = 8)
-        obj(Items.RUNE_SQ_SHIELD, quantity = 1, slots = 8)
-        obj(Items.DRAGON_MED_HELM, quantity = 1, slots = 16)
-
-        obj(Items.FIRE_RUNE, quantity = 75, slots = 8)
-        obj(Items.CHAOS_RUNE, quantity = 15, slots = 3)
-        obj(Items.DEATH_RUNE, quantity = 5, slots = 3)
-        obj(Items.FIRE_RUNE, quantity = 37, slots = 1)
-
-        obj(Items.COINS_995, quantity = 132, slots = 40)
-        obj(Items.COINS_995, quantity = 44, slots = 29)
-        obj(Items.COINS_995, quantity = 220, slots = 10)
-        obj(Items.COINS_995, quantity = 11, slots = 7)
-        obj(Items.COINS_995, quantity = 460, slots = 1)
-
-        obj(Items.TUNA, quantity = 1, slots = 3)
-        obj(Items.GOLD_BAR, quantity = 1, slots = 2)
-        obj(Items.THREAD, quantity = 10, slots = 1)
-
-        table(Gems.gemTable, slots = 5)
+        total(246)
+        //WEAPONS AND ARMOUR
+        obj(Items.STEEL_2H_SWORD, quantity = 1, slots = 8)
+        obj(Items.STEEL_HATCHET, quantity = 1, slots = 6)
+        obj(Items.STEEL_BATTLEAXE, quantity = 1, slots = 6)
+        obj(Items.MITHRIL_KITESHIELD, quantity = 1, slots = 2)
+        obj(Items.ADAMANT_PLATELEGS, quantity = 1, slots = 2)
+        obj(Items.RUNE_FULL_HELM, quantity = 1, slots = 2)
+        //RUNES
+        obj(Items.FIRE_RUNE, quantity = 75, slots = 16)
+        obj(Items.CHAOS_RUNE, quantity = 15, slots = 6)
+        obj(Items.DEATH_RUNE, quantity = 5, slots = 6)
+        obj(Items.FIRE_RUNE, quantity = 37, slots = 2)
+        //COINS
+        obj(Items.COINS_995, quantity = 132, slots = 80)
+        obj(Items.COINS_995, quantity = 44, slots = 58)
+        obj(Items.COINS_995, quantity = 220, slots = 20)
+        obj(Items.COINS_995, quantity = 11, slots = 14)
+        obj(Items.COINS_995, quantity = 460, slots = 2)
+        //OTHER
+        obj(Items.TUNA, quantity = 1, slots = 6)
+        obj(Items.GOLD_BAR, quantity = 1, slots = 4)
+        obj(Items.THREAD, quantity = 10, slots = 4)
+        //TABLES
+        table(Gems.gemTable, slots = 10)
     }
     table("Charms") {
         total(1000)
-        obj(Items.GOLD_CHARM, quantity = 1, slots = 20)
-        obj(Items.GREEN_CHARM, quantity = 1, slots = 8)
-        obj(Items.CRIMSON_CHARM, quantity = 1, slots = 30)
-        obj(Items.BLUE_CHARM, quantity = 1, slots = 7)
-        nothing(slots = 935)
+        obj(Items.GOLD_CHARM, quantity = 1, slots = 70) // 7% drop rate
+        obj(Items.GREEN_CHARM, quantity = 1, slots = 40) // 4% drop rate
+        obj(Items.CRIMSON_CHARM, quantity = 1, slots = 260) // 26% drop rate
+        obj(Items.BLUE_CHARM, quantity = 1, slots = 10) // 1% drop rate (estimation)
+        nothing(slots = 620) // 62% drop rate for nothing
     }
 }
 
@@ -92,7 +86,7 @@ ids.forEach {
         slayer {
             assignment = SlayerAssignment.GREATER_DEMON
             level = 1
-            experience = 79.0
+            experience = 87.0
         }
     }
 }
