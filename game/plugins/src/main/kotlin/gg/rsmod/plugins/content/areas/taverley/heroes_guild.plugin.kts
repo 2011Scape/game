35d9bce519437643d846eb06e03d216878c0e12d
Harley <hg@harleygilpin.com>
2024-01-12 14:29:06 -0800

    feat: add heroes guild objects handling

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/taverley/heroes_guild.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/taverley/heroes_guild.plugin.kts
@@ -4,41 +4,51 @@ package gg.rsmod.plugins.content.areas.taverley
  * @author Harley <https://github.com/HarleyGilpin>
  */
 
+// Define an array of jewelry IDs that can be recharged
 val GloryOrROW = intArrayOf(1704, 1706, 1708, 1710, 1712, 10354, 10356, 10358, 10360, 10362, 2572, 20653, 20655, 20657, 20659)
 
+// Iterate over each jewelry ID
 for (jewelryId in GloryOrROW) {
+    // Define an action when an item from the array is used on the specified object
     on_item_on_obj(Objs.FOUNTAIN_OF_HEROES, jewelryId) {
-        val inventory = player.inventory
-        val def = player.world.definitions
 
-        inventory.rawItems.forEach { item ->
-            item?.let { nonNullItem ->
-                if (nonNullItem.id in GloryOrROW && !nonNullItem.getName(def).lowercase().contains("(4)")) {
+        player.queue(TaskPriority.STRONG) {
+            val inventory = player.inventory
+            val def = player.world.definitions
+            var itemNameUsed: String? = null
+
+            // Process each item in the player's inventory
+            inventory.rawItems
+                .filterNotNull()
+                .filter { it.id in GloryOrROW && !it.getName(def).lowercase().contains("(4)") }
+                .forEach { nonNullItem ->
                     val fullyChargedItemId = getChargedId(nonNullItem.id)
-                    if (fullyChargedItemId != -1) {
-                        if (inventory.remove(item = nonNullItem).hasSucceeded()) {
-                            inventory.add(fullyChargedItemId)
-                            player.animate(827)
-                            player.message("You dip the ${nonNullItem.getName(def).lowercase()} into the fountain restoring all charges.")
-                        }
+                    if (fullyChargedItemId != -1 && inventory.remove(item = nonNullItem).hasSucceeded()) {
+                        itemNameUsed = itemNameUsed ?: nonNullItem.getName(def).split("\\s+".toRegex()).firstOrNull() ?: ""
+                        inventory.add(fullyChargedItemId)
+                        player.animate(827)
                     }
                 }
-            }
+
+            itemNameUsed?.let {
+                player.message("You dip the ${it.lowercase()} into the fountain restoring all charges.")
+                wait(3)
+                itemMessageBox(message = "You feel a power emanating from the fountain as it recharges your ${it.lowercase()}." +
+                        "You can now rub the ${it.lowercase()} to teleport.", item = jewelryId)
+            } ?: player.message("No suitable item found to recharge.")
         }
     }
 }
 
+// Function to get the fully charged item ID
 fun getChargedId(itemId: Int): Int {
-    //glory
-    if ((itemId == 1704) || (itemId == 1706) || (itemId == 1708) || (itemId == 1710)) return 1712
-
-    //trimmed glory
-    if ((itemId == 10356) || (itemId == 10358) || (itemId == 10360) || (itemId == 10362)) return 10354
-
-    //row
-    if ((itemId == 2572) || (itemId == 20653) || (itemId == 20655) || (itemId == 20657)) return 20659
-
-    return -1
+    // Return the corresponding fully charged item ID
+    return when (itemId) {
+        in intArrayOf(1704, 1706, 1708, 1710) -> 1712
+        in intArrayOf(10356, 10358, 10360, 10362) -> 10354
+        in intArrayOf(2572, 20653, 20655, 20657) -> 20659
+        else -> -1
+    }
 }
 
 //Heroes' Guild Doors
