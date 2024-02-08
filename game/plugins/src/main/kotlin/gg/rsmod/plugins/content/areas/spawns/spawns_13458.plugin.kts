fb8402594a4a704d74630907f828105c2d769e54
RSDEANEO <deane1991@live.co.uk>
2024-02-08 15:33:58 +0000

    fix: spawns for the Pollnivneach Slayer Dungeon

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/spawns/spawns_13458.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/spawns/spawns_13458.plugin.kts
@@ -1,14 +1,14 @@
 package gg.rsmod.plugins.content.areas.spawns
 
-spawn_npc(npc = Npcs.MIGHTY_BANSHEE, x = 3357, z = 9376, height = 0, walkRadius = 5, direction = Direction.NORTH, static = false) //Mighty banshee
-spawn_npc(npc = Npcs.MIGHTY_BANSHEE, x = 3368, z = 9382, height = 0, walkRadius = 5, direction = Direction.NORTH, static = false) //Mighty banshee
-spawn_npc(npc = Npcs.MIGHTY_BANSHEE, x = 3361, z = 9382, height = 0, walkRadius = 5, direction = Direction.NORTH, static = false) //Mighty banshee
-spawn_npc(npc = Npcs.MIGHTY_BANSHEE, x = 3353, z = 9384, height = 0, walkRadius = 5, direction = Direction.NORTH, static = false) //Mighty banshee
-spawn_npc(npc = Npcs.MIGHTY_BANSHEE, x = 3369, z = 9383, height = 0, walkRadius = 5, direction = Direction.NORTH, static = false) //Mighty banshee
-spawn_npc(npc = Npcs.MIGHTY_BANSHEE, x = 3349, z = 9389, height = 0, walkRadius = 5, direction = Direction.NORTH, static = false) //Mighty banshee
-spawn_npc(npc = Npcs.MIGHTY_BANSHEE, x = 3345, z = 9393, height = 0, walkRadius = 5, direction = Direction.NORTH, static = false) //Mighty banshee
-spawn_npc(npc = Npcs.MIGHTY_BANSHEE, x = 3369, z = 9395, height = 0, walkRadius = 5, direction = Direction.NORTH, static = false) //Mighty banshee
-spawn_npc(npc = Npcs.MIGHTY_BANSHEE, x = 3343, z = 9392, height = 0, walkRadius = 5, direction = Direction.NORTH, static = false) //Mighty banshee
-spawn_npc(npc = Npcs.MIGHTY_BANSHEE, x = 3363, z = 9397, height = 0, walkRadius = 5, direction = Direction.NORTH, static = false) //Mighty banshee
-spawn_npc(npc = Npcs.MIGHTY_BANSHEE, x = 3355, z = 9406, height = 0, walkRadius = 5, direction = Direction.NORTH, static = false) //Mighty banshee
-spawn_npc(npc = Npcs.MIGHTY_BANSHEE, x = 3362, z = 9406, height = 0, walkRadius = 5, direction = Direction.NORTH, static = false) //Mighty banshee
+spawn_npc(npc = Npcs.CATOLAX, x = 3366, z = 9352, height = 0, walkRadius = 5, direction = Direction.NORTH, static = false) //Mighty banshee
+spawn_npc(npc = Npcs.BANSHEE_MISTRESS, x = 3359, z = 9435, height = 0, walkRadius = 5, direction = Direction.SOUTH, static = false) //Mighty banshee
+
+spawn_npc(npc = Npcs.MIGHTY_BANSHEE, x = 3352, z = 9422, height = 0, walkRadius = 5, direction = Direction.NORTH, static = false) //Mighty banshee
+spawn_npc(npc = Npcs.MIGHTY_BANSHEE, x = 3365, z = 9417, height = 0, walkRadius = 5, direction = Direction.NORTH, static = false) //Mighty banshee
+spawn_npc(npc = Npcs.MIGHTY_BANSHEE, x = 3350, z = 9410, height = 0, walkRadius = 5, direction = Direction.NORTH, static = false) //Mighty banshee
+spawn_npc(npc = Npcs.MIGHTY_BANSHEE, x = 3365, z = 9403, height = 0, walkRadius = 5, direction = Direction.NORTH, static = false) //Mighty banshee
+spawn_npc(npc = Npcs.MIGHTY_BANSHEE, x = 3367, z = 9390, height = 0, walkRadius = 5, direction = Direction.NORTH, static = false) //Mighty banshee
+spawn_npc(npc = Npcs.MIGHTY_BANSHEE, x = 3366, z = 9378, height = 0, walkRadius = 5, direction = Direction.NORTH, static = false) //Mighty banshee
+spawn_npc(npc = Npcs.MIGHTY_BANSHEE, x = 3352, z = 9377, height = 0, walkRadius = 5, direction = Direction.NORTH, static = false) //Mighty banshee
+spawn_npc(npc = Npcs.MIGHTY_BANSHEE, x = 3351, z = 9386, height = 0, walkRadius = 5, direction = Direction.NORTH, static = false) //Mighty banshee
+spawn_npc(npc = Npcs.MIGHTY_BANSHEE, x = 3351, z = 9402, height = 0, walkRadius = 5, direction = Direction.NORTH, static = false) //Mighty banshee
