7a0e02aabc1f04ffd5d8e16e7271729351faef1e
RSDEANEO <deane1991@live.co.uk>
2024-02-19 09:43:30 +0000

    Updated CreatedUnfinishedPotionAction

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/skills/herblore/mixing/CreateUnfinishedPotionAction.kt
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/skills/herblore/mixing/CreateUnfinishedPotionAction.kt
@@ -46,6 +46,5 @@ class CreateUnfinishedPotionAction {
             player.filterableMessage(message)
             return false
         }
-        return (inventory.contains(potion.clean) && inventory.contains(Items.VIAL_OF_WATER) || inventory.contains(Items.VIAL_OF_WATER_17492) || inventory.contains(Items.JUJU_VIAL_OF_WATER))
-    }
+        return inventory.contains(potion.clean) && inventory.contains(potion.requiredVial) }
 }
