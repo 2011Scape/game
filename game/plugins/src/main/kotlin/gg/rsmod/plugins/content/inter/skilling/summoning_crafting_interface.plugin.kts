87541e9f7a99aaf56ffef9f08877e1ecf7b4bb02
Ilwyd <wgray5093@gmail.com>
2024-03-01 20:23:17 -0400

    chore: init pouch creation interface

---
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/inter/skilling/summoning_crafting_interface.plugin.kts
@@ -0,0 +1,9 @@
+import gg.rsmod.plugins.content.skills.summoning.SummoningPouchData
+
+/**
+ * Add buttons to the summoning obelisk interface
+ * @author Ilwyd
+ */
+SummoningPouchData.values.forEach { pouch ->
+
+}
