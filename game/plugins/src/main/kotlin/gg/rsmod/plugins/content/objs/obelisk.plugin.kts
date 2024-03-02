ac4f57dafd6c5766533441da10afbe5176835965
Ilwyd <wgray5093@gmail.com>
2024-03-02 12:44:50 -0400

    chore: add proper capitalization to options

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/objs/obelisk.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/objs/obelisk.plugin.kts
@@ -1,4 +1,4 @@
-on_obj_options(Objs.OBELISK_28716, Objs.OBELISK_28725, Objs.OBELISK_28734, options = arrayOf("infuse-pouch")) {
+on_obj_options(Objs.OBELISK_28716, Objs.OBELISK_28725, Objs.OBELISK_28734, options = arrayOf("Infuse-pouch")) {
     openPouchInterface(player)
 }
 
