798171eae4df68bfca609017b6fe6a9f28f8a4e9
Ilwyd <wgray5093@gmail.com>
2024-03-05 18:29:14 -0400

    feat: add ability to open pikkupstix's trapdoor with a trapdoor key during wolf whistle

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/skills/summoning/trapdoors.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/skills/summoning/trapdoors.plugin.kts
@@ -1,3 +1,6 @@
+import gg.rsmod.plugins.content.quests.getCurrentStage
+import gg.rsmod.plugins.content.quests.impl.WolfWhistle
+
 // Taverley obelisk
 on_obj_option(Objs.LADDER_28714, "Climb") {
     player.handleLadder(2926, 3444, underground = true)
@@ -8,8 +11,17 @@ on_obj_option(Objs.TRAPDOOR_28676, "Climb-down") {
 }
 
 on_obj_option(Objs.TRAPDOOR_28675, "Open") {
-    val obj = player.getInteractingGameObj()
-    open(player, obj) //Broken for this specific location idk why
+    player.message("The trapdoor is locked...")
+}
+
+on_item_on_obj(Objs.TRAPDOOR_28675, Items.TRAPDOOR_KEY) {
+    val wolfWhistle = WolfWhistle
+
+    if (player.getCurrentStage(wolfWhistle) >= 5) {
+        player.setVarbit(4303, 2)
+        player.message("The trapdoor opens.")
+        player.inventory.remove(Items.TRAPDOOR_KEY)
+    }
 }
 
 on_obj_option(Objs.TRAPDOOR_28676, "Close") {
