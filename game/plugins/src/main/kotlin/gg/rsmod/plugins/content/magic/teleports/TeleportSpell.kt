e1bf3dbd68a3ffaa039e57e5ae12e84b16e10fda
RSDEANEO <deane1991@live.co.uk>
2024-03-16 11:09:17 +0000

    fix: coords issue for ardougne getting teleported into pole

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/magic/teleports/TeleportSpell.kt
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/magic/teleports/TeleportSpell.kt
@@ -13,7 +13,7 @@ enum class TeleportSpell(val spellName: String, val type: TeleportType, val endA
     LUMBRIDGE("Lumbridge Teleport", TeleportType.MODERN, Area(3221, 3218, 3222, 3219), 41.0),
     FALADOR("Falador Teleport", TeleportType.MODERN, Area(2961, 3376, 2969, 3385), 47.0),
     CAMELOT("Camelot Teleport", TeleportType.MODERN, Area(2756, 3476, 2758, 3480), 55.5),
-    ARDOUGNE("Ardougne Teleport", TeleportType.MODERN, Area(2659, 3300, 2665, 3310), 61.0),
+    ARDOUGNE("Ardougne Teleport", TeleportType.MODERN, Area(2664, 3305, 2665, 3307), 61.0),
     WATCHTOWER("Watchtower Teleport", TeleportType.MODERN, Area(2551, 3113, 2553, 3116), 68.0),
     TROLLHEIM("Trollheim Teleport", TeleportType.MODERN, Area(2888, 3675, 2890, 3678), 68.0),
     APE_ATOLL("Teleport to Ape Atoll", TeleportType.MODERN, Area(2760, 2781, 2763, 2784), 74.0),
