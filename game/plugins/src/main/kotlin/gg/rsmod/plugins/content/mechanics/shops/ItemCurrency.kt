9764214e36f93f3f605855dc265387d1bdf78e7c
Harley <hg@harleygilpin.com>
2024-06-03 09:15:33 -0700

    feat: add infinite stock type to shops.

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/mechanics/shops/ItemCurrency.kt
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/mechanics/shops/ItemCurrency.kt
@@ -6,10 +6,7 @@ import gg.rsmod.game.model.World
 import gg.rsmod.game.model.attr.LAST_VIEWED_SHOP_ITEM_FREE
 import gg.rsmod.game.model.entity.Player
 import gg.rsmod.game.model.item.Item
-import gg.rsmod.game.model.shop.PurchasePolicy
-import gg.rsmod.game.model.shop.Shop
-import gg.rsmod.game.model.shop.ShopCurrency
-import gg.rsmod.game.model.shop.ShopItem
+import gg.rsmod.game.model.shop.*
 import gg.rsmod.plugins.api.InterfaceDestination
 import gg.rsmod.plugins.api.ext.*
 import gg.rsmod.util.Misc
@@ -180,7 +177,7 @@ open class ItemCurrency(itemCurrency: Int, private val singularCurrency: String,
         if(cost <= 0) {
             return 1
         }
-        return world.definitions.get(ItemDef::class.java, item).cost
+        return world.definitions.get(ItemDef::class.java, item).cost + 1
     }
 
     override fun getBuyPrice(stock: Int, world: World, item: Int): Int {
@@ -191,6 +188,7 @@ open class ItemCurrency(itemCurrency: Int, private val singularCurrency: String,
         return (value * firstItemSellPriceFactor - sellPriceDecreasePerItem * value * min(stock, maxNumItemsBeforePriceDecrease)).toInt()
     }
 
+    // In the giveToPlayer method
     override fun giveToPlayer(p: Player, shop: Shop, slot: Int, amt: Int) {
         val shopItem = shop.sampleItems[slot] ?: return
 
@@ -213,7 +211,7 @@ open class ItemCurrency(itemCurrency: Int, private val singularCurrency: String,
             p.message("You don't have enough inventory space.")
         }
 
-        if (add.completed > 0 && shopItem.amount != Int.MAX_VALUE) {
+        if (add.completed > 0 && shop.stockType != StockType.INFINITE && shopItem.amount != Int.MAX_VALUE) {
             shop.sampleItems[slot]!!.currentAmount -= add.completed
 
             /*
@@ -227,6 +225,7 @@ open class ItemCurrency(itemCurrency: Int, private val singularCurrency: String,
         }
     }
 
+    // In the sellToPlayer method
     override fun sellToPlayer(p: Player, shop: Shop, slot: Int, amt: Int) {
         val shopItem = shop.items[slot] ?: return
 
@@ -279,7 +278,7 @@ open class ItemCurrency(itemCurrency: Int, private val singularCurrency: String,
             p.inventory.add(item = currencyItem, amount = refund)
         }
 
-        if (add.completed > 0 && shopItem.amount != Int.MAX_VALUE) {
+        if (add.completed > 0 && shop.stockType != StockType.INFINITE && shopItem.amount != Int.MAX_VALUE) {
             shop.items[slot]!!.currentAmount -= add.completed
 
             /*
@@ -292,6 +291,7 @@ open class ItemCurrency(itemCurrency: Int, private val singularCurrency: String,
             shop.refresh(p.world)
         }
     }
+
 
     override fun buyFromPlayer(p: Player, shop: Shop, slot: Int, amt: Int) {
         val item = p.inventory[slot] ?: return
