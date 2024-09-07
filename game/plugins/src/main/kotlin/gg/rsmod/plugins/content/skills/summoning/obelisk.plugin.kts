262de74e9208ffa5eeed5a1cd23736167d6e5fe9
Ilwyd <wgray5093@gmail.com>
2024-09-07 15:45:39 -0300

    feat: add Gu'Tanoth obelisk

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/skills/summoning/obelisk.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/skills/summoning/obelisk.plugin.kts
@@ -1,3 +1,11 @@
-on_obj_options(Objs.OBELISK_28716, Objs.OBELISK_28725, Objs.OBELISK_28734, options = arrayOf("Infuse-pouch")) {
+on_obj_options(
+    Objs.OBELISK_28716,
+    Objs.OBELISK_28725,
+    Objs.OBELISK_28734,
+    Objs.OBELISK_28722,
+    options =
+        arrayOf
+        ("Infuse-pouch"),
+) {
     openPouchInterface(player)
 }
