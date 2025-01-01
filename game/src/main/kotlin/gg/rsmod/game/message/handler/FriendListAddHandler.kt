4d31a3b7a9abcbe36c1d52b7bb2c87bc39678858
Ilwyd <wgray5093@gmail.com>
2025-01-01 12:14:44 -0400

    fix: format added friend's name before adding them to the list

--- game/src/main/kotlin/gg/rsmod/game/message/handler/FriendListAddHandler.kt
+++ game/src/main/kotlin/gg/rsmod/game/message/handler/FriendListAddHandler.kt
@@ -5,6 +5,7 @@ import gg.rsmod.game.message.impl.FriendListAddMessage
 import gg.rsmod.game.model.World
 import gg.rsmod.game.model.attr.ADDED_FRIEND
 import gg.rsmod.game.model.entity.Client
+import gg.rsmod.util.Misc
 
 class FriendListAddHandler : MessageHandler<FriendListAddMessage> {
     override fun handle(
@@ -12,7 +13,7 @@ class FriendListAddHandler : MessageHandler<FriendListAddMessage> {
         world: World,
         message: FriendListAddMessage,
     ) {
-        client.attr[ADDED_FRIEND] = message.username
+        client.attr[ADDED_FRIEND] = Misc.formatForDisplay(message.username)
         client.world.plugins.executeAddFriend(client)
     }
 }
