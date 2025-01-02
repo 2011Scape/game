90d955090ad546e6a0338d6bb0fc145a9479fc96
Ilwyd <wgray5093@gmail.com>
2025-01-02 19:10:31 -0400

    feat: move fally castle stairs to stair json

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/falador/falador_castle.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/falador/falador_castle.plugin.kts
@@ -7,11 +7,3 @@ on_obj_option(obj = Objs.LADDER_11727, option = "climb-up") {
 on_obj_option(obj = Objs.LADDER_11728, option = "climb-down") {
     player.handleLadder(x = player.tile.x, z = player.tile.z, height = player.tile.transform(height = -1).height)
 }
-
-on_obj_option(obj = Objs.STAIRCASE_11734, option = "climb-up") {
-    player.moveTo(Tile(x = 2984, z = 3340, height = 2))
-}
-
-on_obj_option(obj = Objs.STAIRCASE_35783, option = "climb-down") {
-    player.moveTo(Tile(x = 2984, z = 3336, height = 1))
-}
