b2146c6c185e1dfac5fcd452ca4a9b5fd93b2c1c
Ilwyd <wgray5093@gmail.com>
2025-01-03 16:04:49 -0400

    feat: add support for CameraForceAngle packet

--- game/src/main/kotlin/gg/rsmod/game/message/MessageEncoderSet.kt
+++ game/src/main/kotlin/gg/rsmod/game/message/MessageEncoderSet.kt
@@ -74,6 +74,7 @@ class MessageEncoderSet {
         put(SetPrivateChatFilterEncoder(), SetPrivateChatFilterMessage::class.java)
         put(MessagePrivateSentEncoder(), MessagePrivateSentMessage::class.java)
         put(UpdateIgnoreListEncoder(), UpdateIgnoreListMessage::class.java)
+        put(CameraForceAngleEncoder(), CameraForceAngleMessage::class.java)
     }
 
     private fun <T : Message> put(
