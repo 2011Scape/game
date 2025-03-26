3c5fd207ee3e5c9d587ee305ea913f62bbfbdaf5
lucid-soft <69691787+lucid-soft@users.noreply.github.com>
2025-03-26 12:31:02 -0400

    Clean up smuggle logic, remove from queue

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/karamja/tzhaar_entrance.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/karamja/tzhaar_entrance.plugin.kts
@@ -16,17 +16,12 @@ on_obj_option(obj = Objs.CLIMBING_ROPE_1764, option = "climb") {
 
 // TzHaar entrance from Karamja side
 on_obj_option(obj = Objs.CAVE_ENTRANCE_31284, option = "enter") {
-    player.queue {
-        if (player.inventory.contains(Items.KARAMJAN_RUM)) {
-            player.graphic(Graphic(1028, 0, 0, 0))
-            player.moveTo(x = 2480, z = 5175)
-            player.inventory.remove(Items.KARAMJAN_RUM, 28)
-            player.message("Why is the rum gone?")
-        }
-        else {
-            player.moveTo(x = 2480, z = 5175)
-        }
+    if (player.inventory.contains(Items.KARAMJAN_RUM)) {
+        player.graphic(Graphic(1028, 0, 0, 0))
+        player.inventory.remove(Items.KARAMJAN_RUM, 28)
+        player.message("Why is the rum gone?")
     }
+    player.moveTo(x = 2480, z = 5175)
 }
 
 // TzHaar exit to Karamja side
