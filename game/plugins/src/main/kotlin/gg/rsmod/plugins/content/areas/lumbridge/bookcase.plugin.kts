edcd04a1f9818c8714c6f12887b0b37ad2ec378c
lucid-soft <69691787+lucid-soft@users.noreply.github.com>
2025-03-29 10:38:45 -0400

    Add another bookcase

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/lumbridge/bookcase.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/lumbridge/bookcase.plugin.kts
@@ -10,3 +10,14 @@ on_obj_option(Objs.BOOKCASE_45246, "Search") {
         player.message("You don't find anything that you'd ever want to read.")
     }
 }
+
+on_obj_option(Objs.BOOKCASE_45247, "Search") {
+    player.queue {
+        player.lock = LockState.FULL
+        player.message("You search the books...")
+        wait(2)
+        player.unlock()
+        wait(2)
+        player.message("None of them look very interesting.")
+    }
+}
