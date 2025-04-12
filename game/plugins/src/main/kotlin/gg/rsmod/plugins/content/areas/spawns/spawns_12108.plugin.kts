96a333379a5a3c09c5e7b9554c8347b4ba77ef20
RSDEANEO <deane1991@live.co.uk>
2025-04-12 01:59:27 +0100

    feat:added spawn_12108.plugin.kts (Abyssal_Area) https://runescape.fandom.com/wiki/Abyssal_Area
    feat:added spawn_11425.plugin.kts (Mahjarrat Ritual Site Caver)
    https://runescape.fandom.com/wiki/Mahjarrat_Ritual_Site_Cavern
    
    feat:added spawn_13209.plugin.kts (Tolna's rift)
    https://runescape.fandom.com/wiki/Tolna%27s_rift
    
    feat:added spawn_10904.plugin.kts (Legends' Guild basement)
    https://runescape.fandom.com/wiki/Legends%27_Guild

---
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/spawns/spawns_12108.plugin.kts
@@ -0,0 +1,351 @@
+package gg.rsmod.plugins.content.areas.spawns
+
+spawn_npc(
+    npc = Npcs.ABYSSAL_DEMON,
+    x = 3060,
+    z = 4883,
+    height = 0,
+    walkRadius = 5,
+    direction = Direction.NORTH,
+    static = false,
+) // Abyssal demon
+spawn_npc(
+    npc = Npcs.ABYSSAL_DEMON,
+    x = 3046,
+    z = 4874,
+    height = 0,
+    walkRadius = 5,
+    direction = Direction.NORTH,
+    static = false,
+) // Abyssal demon
+spawn_npc(
+    npc = Npcs.ABYSSAL_DEMON,
+    x = 3024,
+    z = 4877,
+    height = 0,
+    walkRadius = 5,
+    direction = Direction.NORTH,
+    static = false,
+) // Abyssal demon
+spawn_npc(
+    npc = Npcs.ABYSSAL_DEMON,
+    x = 3016,
+    z = 4891,
+    height = 0,
+    walkRadius = 5,
+    direction = Direction.NORTH,
+    static = false,
+) // Abyssal demon
+spawn_npc(
+    npc = Npcs.ABYSSAL_DEMON,
+    x = 3035,
+    z = 4883,
+    height = 0,
+    walkRadius = 5,
+    direction = Direction.NORTH,
+    static = false,
+) // Abyssal demon
+spawn_npc(
+    npc = Npcs.ABYSSAL_DEMON,
+    x = 3046,
+    z = 4886,
+    height = 0,
+    walkRadius = 5,
+    direction = Direction.NORTH,
+    static = false,
+) // Abyssal demon
+spawn_npc(
+    npc = Npcs.ABYSSAL_DEMON,
+    x = 3054,
+    z = 4891,
+    height = 0,
+    walkRadius = 5,
+    direction = Direction.NORTH,
+    static = false,
+) // Abyssal demon
+spawn_npc(
+    npc = Npcs.ABYSSAL_DEMON,
+    x = 3029,
+    z = 4899,
+    height = 0,
+    walkRadius = 5,
+    direction = Direction.NORTH,
+    static = false,
+) // Abyssal demon
+spawn_npc(
+    npc = Npcs.ABYSSAL_DEMON,
+    x = 3036,
+    z = 4905,
+    height = 0,
+    walkRadius = 5,
+    direction = Direction.NORTH,
+    static = false,
+) // Abyssal demon
+spawn_npc(
+    npc = Npcs.ABYSSAL_DEMON,
+    x = 3019,
+    z = 4901,
+    height = 0,
+    walkRadius = 5,
+    direction = Direction.NORTH,
+    static = false,
+) // Abyssal demon
+spawn_npc(
+    npc = Npcs.ABYSSAL_DEMON,
+    x = 3025,
+    z = 4916,
+    height = 0,
+    walkRadius = 5,
+    direction = Direction.NORTH,
+    static = false,
+) // Abyssal demon
+spawn_npc(
+    npc = Npcs.ABYSSAL_DEMON,
+    x = 3050,
+    z = 4918,
+    height = 0,
+    walkRadius = 5,
+    direction = Direction.NORTH,
+    static = false,
+) // Abyssal demon
+spawn_npc(
+    npc = Npcs.ABYSSAL_DEMON,
+    x = 3056,
+    z = 4908,
+    height = 0,
+    walkRadius = 5,
+    direction = Direction.NORTH,
+    static = false,
+) // Abyssal demon
+spawn_npc(
+    npc = Npcs.ABYSSAL_LEECH,
+    x = 3060,
+    z = 4880,
+    walkRadius = 5,
+    direction = Direction.NORTH,
+    static = false,
+) // Abyssal leech
+spawn_npc(
+    npc = Npcs.ABYSSAL_LEECH,
+    x = 3020,
+    z = 4877,
+    walkRadius = 5,
+    direction = Direction.NORTH,
+    static = false,
+) // Abyssal leech
+spawn_npc(
+    npc = Npcs.ABYSSAL_LEECH,
+    x = 3040,
+    z = 4870,
+    height = 0,
+    walkRadius = 5,
+    direction = Direction.NORTH,
+    static = false,
+) // Abyssal leech
+spawn_npc(
+    npc = Npcs.ABYSSAL_LEECH,
+    x = 3030,
+    z = 4876,
+    height = 0,
+    walkRadius = 5,
+    direction = Direction.NORTH,
+    static = false,
+) // Abyssal leech
+spawn_npc(
+    npc = Npcs.ABYSSAL_LEECH,
+    x = 3050,
+    z = 4873,
+    height = 0,
+    walkRadius = 5,
+    direction = Direction.NORTH,
+    static = false,
+) // Abyssal leech
+spawn_npc(
+    npc = Npcs.ABYSSAL_LEECH,
+    x = 3035,
+    z = 4908,
+    height = 0,
+    walkRadius = 5,
+    direction = Direction.NORTH,
+    static = false,
+) // Abyssal leech
+spawn_npc(
+    npc = Npcs.ABYSSAL_LEECH,
+    x = 3018,
+    z = 4902,
+    height = 0,
+    walkRadius = 5,
+    direction = Direction.NORTH,
+    static = false,
+) // Abyssal leech
+spawn_npc(
+    npc = Npcs.ABYSSAL_LEECH,
+    x = 3018,
+    z = 4909,
+    height = 0,
+    walkRadius = 5,
+    direction = Direction.NORTH,
+    static = false,
+) // Abyssal leech
+spawn_npc(
+    npc = Npcs.ABYSSAL_LEECH,
+    x = 3021,
+    z = 4918,
+    height = 0,
+    walkRadius = 5,
+    direction = Direction.NORTH,
+    static = false,
+) // Abyssal leech
+spawn_npc(
+    npc = Npcs.ABYSSAL_LEECH,
+    x = 3029,
+    z = 4917,
+    height = 0,
+    walkRadius = 5,
+    direction = Direction.NORTH,
+    static = false,
+) // Abyssal leech
+spawn_npc(
+    npc = Npcs.ABYSSAL_LEECH,
+    x = 3046,
+    z = 4916,
+    height = 0,
+    walkRadius = 5,
+    direction = Direction.NORTH,
+    static = false,
+) // Abyssal leech
+spawn_npc(
+    npc = Npcs.ABYSSAL_LEECH,
+    x = 3064,
+    z = 4900,
+    height = 0,
+    walkRadius = 5,
+    direction = Direction.NORTH,
+    static = false,
+) // Abyssal leech
+spawn_npc(
+    npc = Npcs.ABYSSAL_LEECH,
+    x = 3056,
+    z = 4912,
+    height = 0,
+    walkRadius = 5,
+    direction = Direction.NORTH,
+    static = false,
+) // Abyssal leech
+spawn_npc(
+    npc = Npcs.ABYSSAL_WALKER,
+    x = 3063,
+    z = 4886,
+    height = 0,
+    walkRadius = 5,
+    direction = Direction.NORTH,
+    static = false,
+) // Abyssal walker
+spawn_npc(
+    npc = Npcs.ABYSSAL_WALKER,
+    x = 3032,
+    z = 4872,
+    height = 0,
+    walkRadius = 5,
+    direction = Direction.NORTH,
+    static = false,
+) // Abyssal walker
+spawn_npc(
+    npc = Npcs.ABYSSAL_WALKER,
+    x = 3031,
+    z = 4888,
+    height = 0,
+    walkRadius = 5,
+    direction = Direction.NORTH,
+    static = false,
+) // Abyssal walker
+spawn_npc(
+    npc = Npcs.ABYSSAL_WALKER,
+    x = 3043,
+    z = 4902,
+    height = 0,
+    walkRadius = 5,
+    direction = Direction.NORTH,
+    static = false,
+) // Abyssal walker
+spawn_npc(
+    npc = Npcs.ABYSSAL_WALKER,
+    x = 3031,
+    z = 4901,
+    height = 0,
+    walkRadius = 5,
+    direction = Direction.NORTH,
+    static = false,
+) // Abyssal walker
+spawn_npc(
+    npc = Npcs.ABYSSAL_WALKER,
+    x = 3020,
+    z = 4914,
+    height = 0,
+    walkRadius = 5,
+    direction = Direction.NORTH,
+    static = false,
+) // Abyssal walker
+spawn_npc(
+    npc = Npcs.ABYSSAL_WALKER,
+    x = 3061,
+    z = 4915,
+    height = 0,
+    walkRadius = 5,
+    direction = Direction.NORTH,
+    static = false,
+) // Abyssal walker
+spawn_npc(
+    npc = Npcs.ABYSSAL_GUARDIAN,
+    x = 3044,
+    z = 4883,
+    height = 0,
+    walkRadius = 5,
+    direction = Direction.NORTH,
+    static = false,
+) // Abyssal guardian
+spawn_npc(
+    npc = Npcs.ABYSSAL_GUARDIAN,
+    x = 3020,
+    z = 4895,
+    height = 0,
+    walkRadius = 5,
+    direction = Direction.NORTH,
+    static = false,
+) // Abyssal guardian
+spawn_npc(
+    npc = Npcs.ABYSSAL_GUARDIAN,
+    x = 3052,
+    z = 4898,
+    height = 0,
+    walkRadius = 5,
+    direction = Direction.NORTH,
+    static = false,
+) // Abyssal guardian
+spawn_npc(
+    npc = Npcs.ABYSSAL_GUARDIAN,
+    x = 3038,
+    z = 4907,
+    height = 0,
+    walkRadius = 5,
+    direction = Direction.NORTH,
+    static = false,
+) // Abyssal guardian
+spawn_npc(
+    npc = Npcs.ABYSSAL_GUARDIAN,
+    x = 3028,
+    z = 4914,
+    height = 0,
+    walkRadius = 5,
+    direction = Direction.NORTH,
+    static = false,
+) // Abyssal guardian
+spawn_npc(
+    npc = Npcs.ABYSSAL_GUARDIAN,
+    x = 3057,
+    z = 4907,
+    height = 0,
+    walkRadius = 5,
+    direction = Direction.NORTH,
+    static = false,
+) // Abyssal guardian
