b13fa080210a384dd6d026d4f60fce2f781ee0ea
lucid-soft <69691787+lucid-soft@users.noreply.github.com>
2025-04-20 23:22:45 -0400

    Add Morrigans javelin projectile type

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/api/ProjectileType.kt
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/api/ProjectileType.kt
@@ -15,6 +15,7 @@ enum class ProjectileType(
     BOLT(startHeight = 38, endHeight = 36, delay = 41, angle = 5, steepness = 11),
     ARROW(startHeight = 40, endHeight = 36, delay = 41, angle = 15, steepness = 11),
     JAVELIN(startHeight = 38, endHeight = 36, delay = 42, angle = 1, steepness = 120),
+    MORRIGANS_JAVELIN(startHeight = 40, endHeight = 36, delay = 30, angle = 0, steepness = 120),
     THROWN(startHeight = 40, endHeight = 36, delay = 32, angle = 15, steepness = 11),
     CHINCHOMPA(startHeight = 40, endHeight = 36, delay = 21, angle = 15, steepness = 11),
     TELEKINETIC_GRAB(startHeight = 38, endHeight = 0, delay = 55, angle = 0, steepness = 124),
@@ -27,7 +28,7 @@ enum class ProjectileType(
             THROWN -> distance * 5
             CHINCHOMPA -> distance * 5
             ARROW, BOLT -> max(10, distance * 5)
-            JAVELIN -> (distance * 3) + 2
+            JAVELIN, MORRIGANS_JAVELIN -> (distance * 3) + 2
         /*
          * Handled in [gg.rsmod.plugins.content.combat.Combat.getProjectileLifespan].
          */
