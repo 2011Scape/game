7d3c3c087a35c7d19a854aac5754407b2ffee34f
lucid-soft <69691787+lucid-soft@users.noreply.github.com>
2025-04-27 11:26:50 -0400

    Make all the chickens the same type like theyre supposed to be

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/spawns/spawns_12596.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/spawns/spawns_12596.plugin.kts
@@ -4,13 +4,13 @@ package gg.rsmod.plugins.content.areas.spawns
  * @author Alycia <https://github.com/alycii>
  */
 
-spawn_npc(npc = Npcs.CHICKEN, x = 3196, z = 3358, walkRadius = 5, direction = Direction.EAST)
+spawn_npc(npc = Npcs.CHICKEN_1017, x = 3196, z = 3358, walkRadius = 5, direction = Direction.EAST)
 spawn_npc(npc = Npcs.CHICKEN_1017, x = 3198, z = 3358, walkRadius = 5, direction = Direction.NORTH_WEST)
-spawn_npc(npc = Npcs.CHICKEN, x = 3195, z = 3356, walkRadius = 5, direction = Direction.EAST)
+spawn_npc(npc = Npcs.CHICKEN_1017, x = 3195, z = 3356, walkRadius = 5, direction = Direction.EAST)
 spawn_npc(npc = Npcs.CHICKEN_1017, x = 3196, z = 3353, walkRadius = 5, direction = Direction.NORTH_WEST)
-spawn_npc(npc = Npcs.CHICKEN, x = 3197, z = 3353, walkRadius = 5, direction = Direction.EAST)
+spawn_npc(npc = Npcs.CHICKEN_1017, x = 3197, z = 3353, walkRadius = 5, direction = Direction.EAST)
 spawn_npc(npc = Npcs.CHICKEN_1017, x = 3198, z = 3359, walkRadius = 5, direction = Direction.NORTH_WEST)
-spawn_npc(npc = Npcs.CHICKEN, x = 3197, z = 3356, walkRadius = 5, direction = Direction.EAST)
+spawn_npc(npc = Npcs.CHICKEN_1017, x = 3197, z = 3356, walkRadius = 5, direction = Direction.EAST)
 spawn_npc(npc = Npcs.DUCK, x = 3179, z = 3348, walkRadius = 5, direction = Direction.NORTH_WEST)
 spawn_npc(npc = Npcs.DUCK, x = 3178, z = 3353, walkRadius = 5, direction = Direction.NORTH_WEST)
 spawn_npc(npc = Npcs.GIANT_SPIDER, x = 3146, z = 3347, walkRadius = 5, direction = Direction.WEST)
